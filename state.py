0cb06380e471771c71d27c450127498dc03ffcc8
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-06-05 09:39:07 +0000

    I was bored on my flight to Iceland so I made the header line up for processtable.
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@31 9d962951-508e-48a8-94b5-974f9cfda47c

--- state.py
+++ state.py
@@ -15,8 +15,9 @@ import atoms
 
 
 ID, ENERGY, PREFACTOR, PRODUCT, PRODUCT_ENERGY, PRODUCT_PREFACTOR, BARRIER, RATE, REPEATS = range(9)
-processtable_header = "  state    saddle energy    prefactor product  product energy  product prefactor  barrier  rate  repeats\n"
-processtable_line = "%7d %16.5f % .5e %9d %16.5f % .5e %16.5f % .5e %7d\n"
+processtable_head_fmt = "%7s %16s %11s %9s %16s %17s %8s %12s %7s\n"
+processtable_header = processtable_head_fmt % ("state", "saddle energy", "prefactor", "product", "product energy", "product prefactor", "barrier", "rate", "repeats")
+processtable_line = "%7d %16.5f %11.5e %9d %16.5f %17.5e %8.5f %12.5e %7d\n"
 search_result_header = "%8s %16s %16s    %s\n" % ("id", "type", "barrier", "result")
 search_result_header += "-" * len(search_result_header) + '\n'
 
