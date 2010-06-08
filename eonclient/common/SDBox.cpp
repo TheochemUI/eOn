fd5720e2db61ba19fecbfe0d4cdfe3b7b03045a6
ryeterrell <ryeterrell@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-06-08 21:30:21 +0000

    option in client to minimize box
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@46 9d962951-508e-48a8-94b5-974f9cfda47c

--- eonclient/common/SDBox.cpp
+++ eonclient/common/SDBox.cpp
@@ -14,15 +14,18 @@
 using namespace helper_functions;
 using namespace constants;
 
-SDBox::SDBox(Matter *matter, Parameters *parameters){
-    // Note that it is the pointer that is copied.
+SDBox::SDBox(Matter *matter, Parameters *parameters)
+{
     matter_ = matter;    
     parameters_ = parameters;
-    
-    nFreeCoord_ = 3*matter->numberOfFreeAtoms();
-//    forces_ = new double[nFreeCoord_];
+    nFreeCoord_ = 3 * matter->numberOfFreeAtoms();
     tempListDouble_ = new double[nFreeCoord_];
-
+    boxforce_ = new double[3];
+    boxv_ = new double[3];
+    boxv_[0] = 0.0; boxv_[1] = 0.0; boxv_[2] = 0.0; 
+    dR = 0.0001;
+    dT = 0.01;
+    qmBox_ = new Quickmin(matter_, parameters);
 };
 
 SDBox::~SDBox(){
@@ -33,119 +36,200 @@ SDBox::~SDBox(){
     // Are pointers to objects outside the scope
     
     delete [] tempListDouble_;
+    delete [] boxforce_;
+    delete [] boxv_;
     return;
 };
 
 
-void SDBox::oneStep(){
-//    long forceCallsTemp;
-    double *freeForces;
-    freeForces = new double[nFreeCoord_];
-
-    //----- Initialize end -----
-    //std::cout<<"oneStep\n";
-
-//    forceCallsTemp = matter_->getForceCalls();  
-    matter_->getFreeForces(freeForces);
-    oneStepPart1(freeForces);
-    matter_->getFreeForces(freeForces);
-    oneStepPart2(freeForces);
-
-//    forceCallsTemp = matter_->getForceCalls()-forceCallsTemp;
-//    parameters_->addForceCalls(forceCallsTemp);
-
-    delete [] freeForces;
-    return;
-};
-
-void SDBox::oneStepPart1(double *freeForces){
-    double *positions;
-    double *velocity;
-    positions = new double[nFreeCoord_];
-
-    velocity = new double[nFreeCoord_];
-
-    //----- Initialize end -----
-    //std::cout<<"oneStepPart1\n";
-    
-    matter_->getFreeVelocities(velocity);  
-    
-    multiplyScalar(tempListDouble_, freeForces, 0.5 * getTimeStep(), nFreeCoord_);
-    add(velocity, tempListDouble_, velocity, nFreeCoord_);
-    matter_->setFreeVelocities(velocity);
-    
-    matter_->getFreePositions(positions);       
-    multiplyScalar(tempListDouble_, velocity, getTimeStep(), nFreeCoord_);
-    add(positions, tempListDouble_, positions, nFreeCoord_);
-    matter_->setFreePositions(positions);  
-
-    delete [] positions;
-    delete [] velocity;
-    return;
-};
-    
-
-void SDBox::oneStepPart2(double *freeForces)
+void SDBox::increment_velocity()
 {
-    double dotVelocityForces;
-    double dotForcesForces;
-    double *velocity;
-    velocity = new double[nFreeCoord_];
-
-    // Keep a copy of the pointer to the force array, is being used when it is 
-    // decided if the calculation is converged
-    forces_ = freeForces;
-    //----- Initialize end -----
-    //std::cout<<"oneStepPart2\n";
-    
-    matter_->getFreeVelocities(velocity); 
-    multiplyScalar(tempListDouble_, freeForces, 0.5*getTimeStep(), nFreeCoord_);
-    add(velocity, tempListDouble_, velocity, nFreeCoord_);
-    
-    dotVelocityForces = dot(velocity, freeForces, nFreeCoord_);
-    // Zeroing all velocities if they are not orthogonal to the forces
-    if(dotVelocityForces < 0)
-        multiplyScalar(velocity, velocity, 0., nFreeCoord_);
-    else{
-        dotForcesForces = dot(freeForces, freeForces, nFreeCoord_);
-        multiplyScalar(velocity, freeForces, 
-                       dotVelocityForces/dotForcesForces, nFreeCoord_);
+    double bx = matter_->getBoundary(0);
+    double by = matter_->getBoundary(1);
+    double bz = matter_->getBoundary(2);
+    double E = matter_->potentialEnergy();
+    matter_->setBoundary(0, bx + dR);
+    double dxE = matter_->potentialEnergy();
+    matter_->setBoundary(0, bx);
+    matter_->setBoundary(1, by + dR);
+    double dyE = matter_->potentialEnergy();
+    matter_->setBoundary(1, by);
+    matter_->setBoundary(2, bz + dR);
+    double dzE = matter_->potentialEnergy();
+    matter_->setBoundary(2, bz);
+    boxforce_[0] = (E - dxE) / dR;
+    boxforce_[1] = (E - dyE) / dR;
+    boxforce_[2] = (E - dzE) / dR;
+    for(int i = 0; i < 3; i++)
+    {
+        if(boxforce_[i] * boxv_[i] < 0.0)
+        {
+            boxv_[i] += boxforce_[i] * dT;
+        }
+        else
+        {
+            boxv_[i] += boxforce_[i] * dT;
+        }    
     }
-    matter_->setFreeVelocities(velocity);      
+    return;
+}
 
-    delete [] velocity;
+void SDBox::oneStep()
+{
+    increment_velocity();
+    double bx = matter_->getBoundary(0);
+    double by = matter_->getBoundary(1);
+    double bz = matter_->getBoundary(2);
+    //printf("%12.8f   %12.8f   %12.8f   %12.8f\n", bx, by, bz, matter_->potentialEnergy());
+    matter_->setBoundary(0, bx + boxv_[0] * dT);
+    matter_->setBoundary(1, by + boxv_[1] * dT);
+    matter_->setBoundary(2, bz + boxv_[2] * dT);
+    qmBox_->oneStep();
     return;
 };
 
+    
 
-void SDBox::fullRelax(){
+void SDBox::fullRelax()
+{
     bool converged = false;
     long forceCallsTemp;
-    //----- Initialize end -----
-    //std::cout<<"fullRelax\n";
-
     forceCallsTemp = matter_->getForceCalls();  
-    while(!converged){
+    while(!converged)
+    {
         oneStep();
+        printf("%12.8f   %12.8f   %12.8f   %12.8f\n", boxforce_[0], boxforce_[1], boxforce_[2], matter_->potentialEnergy());
         converged = isItConverged(parameters_->getConverged_Relax());
-        std::cout<<matter_->potentialEnergy()<<"\n";
+//        std::cout<<matter_->potentialEnergy()<<"\n";
     }
-
-    forceCallsTemp = matter_->getForceCalls()-forceCallsTemp;
+    forceCallsTemp = matter_->getForceCalls() - forceCallsTemp;
     parameters_->addForceCalls(forceCallsTemp);
     return;
 };
 
 
-bool SDBox::isItConverged(double convergeCriterion){
+bool SDBox::isItConverged(double convergeCriterion)
+{
     double diff;
-    
-    for(int i=0;i<nFreeCoord_;i++){
-        diff = fabs(forces_[i]);
-        if(convergeCriterion < diff)
-            break;
+    if(!qmBox_->isItConverged(convergeCriterion))
+    {
+        return false;
     }
-    return(diff < convergeCriterion);
+    for(int i = 0; i < 3; i++)
+    {
+        diff = fabs(boxforce_[i]);
+        if(convergeCriterion < diff)
+        {
+            return false;
+        }
+    }
+    return true;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void SDBox::oneStepPart2(double *freeForces)
+//{
+//    double dotVelocityForces;
+//    double dotForcesForces;
+//    double *velocity;
+//    velocity = new double[nFreeCoord_];
+
+//    // Keep a copy of the pointer to the force array, is being used when it is 
+//    // decided if the calculation is converged
+//    forces_ = freeForces;
+//    
+//    matter_->getFreeVelocities(velocity); 
+//    multiplyScalar(tempListDouble_, freeForces, 0.5*getTimeStep(), nFreeCoord_);
+//    add(velocity, tempListDouble_, velocity, nFreeCoord_);
+//    
+//    dotVelocityForces = dot(velocity, freeForces, nFreeCoord_);
+
+//    // Zeroing all velocities if they are not orthogonal to the forces
+//    if(dotVelocityForces < 0)
+//        multiplyScalar(velocity, velocity, 0., nFreeCoord_);
+//    else{
+//        dotForcesForces = dot(freeForces, freeForces, nFreeCoord_);
+//        multiplyScalar(velocity, freeForces, 
+//                       dotVelocityForces/dotForcesForces, nFreeCoord_);
+//    }
+//    matter_->setFreeVelocities(velocity);      
+
+//    delete [] velocity;
+//    return;
+//};
+
+//void SDBox::oneStepPart1(double *freeForces){
+//    double *positions;
+//    double *velocity;
+//    positions = new double[nFreeCoord_];
+
+//    velocity = new double[nFreeCoord_];
+
+//    matter_->getFreeVelocities(velocity);  
+//    
+//    multiplyScalar(tempListDouble_, freeForces, 0.5 * getTimeStep(), nFreeCoord_);
+//    add(velocity, tempListDouble_, velocity, nFreeCoord_);
+//    matter_->setFreeVelocities(velocity);
+//    
+//    matter_->getFreePositions(positions);       
+//    multiplyScalar(tempListDouble_, velocity, getTimeStep(), nFreeCoord_);
+//    add(positions, tempListDouble_, positions, nFreeCoord_);
+//    matter_->setFreePositions(positions);  
+
+//    delete [] positions;
+//    delete [] velocity;
+//    return;
+//};
 
 
