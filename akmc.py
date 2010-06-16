d86b65d07cefb68c5af9d6e810fdc3fc194f2ade
matt <matt@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-06-16 20:42:46 +0000

    syntax
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@75 9d962951-508e-48a8-94b5-974f9cfda47c

--- akmc.py
+++ akmc.py
@@ -214,7 +214,7 @@ def register_results(comm, current_state, states, searchdata, kdber = None):
                     for process_id in states.get_state(state_num).get_process_ids():
                         output = kdber.add_process(states.get_state(state_num), process_id)
                         logger.debug("kdbaddpr.pl: %s" % output)
-                if not config.register_extra_results:
+                if not config.debug_register_extra_results:
                     break
         else:
             states.get_state(state_num).register_bad_saddle(result_path, config.debug_keep_bad_saddles, result_data)
