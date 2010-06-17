617cee3682a004753caf16d0313b5b9a1a73ff67
Rye Terrell <wwwtyro@users.noreply.github.com>
2010-06-17 21:14:24 +0000

    moved eonclient to client

---
+++ client/potentials/SW/SW.cpp
@@ -0,0 +1,21 @@
+#include"SW.h"
+
+SW::SW(void){
+    return;
+}
+
+void SW::initialize(void){
+    return;
+}
+
+void SW::cleanMemory(void){
+    return;
+}
+
+// pointer to number of atoms, pointer to array of positions	
+// pointer to array of forces, pointer to internal energy
+// adress to supercell size
+void SW::force(long N, const double *R, const long *atomicNrs, double *F, double *U, const double *box){
+//    sw_(&N, R, F, U, &box[0], &box[1], &box[2]);    
+    return;
+}
