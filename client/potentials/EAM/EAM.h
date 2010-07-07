936955f6ded424cf23e5fa37d14c22b48801e7f1
Rye Terrell <wwwtyro@users.noreply.github.com>
2010-07-07 18:41:12 +0000

    EAM hardcoded for manual Al fit

--- client/potentials/EAM/EAM.h
+++ client/potentials/EAM/EAM.h
@@ -2,46 +2,30 @@
 #include <iostream>
 
 //#include "../../system_unit.h" // unit converters
-#include "PotentialsInterface.h"
-
-//	Variables
-	long *celllist_old;
-	long *celllist_new;
-	long *neigh_list;
-	long fcalled;
-	double Dm, alphaM, Rm, beta1,beta2;
-	double r_cut;
-	double rc[3]={5,5,5}; //2 is arbitrary number. rc represents the optimal length for each cell in cell list
-    double func_param[9]={67.2169,-253.032,392.956,-328.003,165.763,-59.8235,18.0797,-2.00292,-.0102076};
-    double u0;
-    double cuttOffR;
-    double psi;
+#include "../../PotentialsInterface.h"
     
-    double cuttOffU;
-    
-    // Functions
-	// constructor
+class EAM : public PotentialsInterface
+{    
+    public:
+        EAM(void);
+        // Variables
+	    long *celllist_old;
+	    long *celllist_new;
+	    long *neigh_list;
+	    bool initialized;
+	    double Dm, alphaM, Rm, beta1,beta2;
+	    double r_cut;
+	    double *rc;
+        double *func_param;
+        // To satify interface
+        void initialize();
+        void cleanMemory();
+        void force(long N, const double *R, const long *atomicNrs, double *F, double *U, const double *box);
+        void calc_force(long N, double *R, const long *atomicNrs, double *F, double *U, double *box);
+        void new_celllist(long N, const double *box, long *num_axis, long *cell_length, long *celllist_new, long num_cells, double *Rnew);
+        void cell_to_neighbor(long N, long num_of_cells, long *num_axis, long *cell_length, long *celllist_new, long *neigh_list);
+        int update_cell_list(long N, long num_cells,long *num_axis, long *cell_length, long *celllist_old, double *Rnew); //returns 0 if unchanged, >0 if changed - represents number of atoms that changed lists
+        double density (long N, long atom, double *R, const long *atomicNrs, const double *box); //calculates local density of single atom
+};
 	
-    // Just to satify interface
-    void initialize() {
-		fcalled=0;
-		Dm=1;
-		alphaM=3.0205380362464;
-		Rm=2.65;
-		beta1=6.6137657075868;
-		beta2= 6.0000000000000;
-		
-		
-
-		r_cut=5.5;
-		}
-    void cleanMemory(){
-		delete celllist_old;
-		delete celllist_new;
-		delete neigh_list;   
-	} 
-    
-    void force(long N, const double *R, const long *atomicNrs, double *F, double *U, const double *box);
-    void setParameters(double r0Recieved, double u0Recieved, double psiRecieved);
-
 
