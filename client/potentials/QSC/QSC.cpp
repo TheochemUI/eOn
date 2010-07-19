9b0b9f2ab6dd346632db1c167f1883f1671e29d3
Sam Chill <SamChill@users.noreply.github.com>
2010-07-19 22:31:31 +0000

    Fixed last major bug in QSC. Needs performance improvements.

--- client/potentials/QSC/QSC.cpp
+++ client/potentials/QSC/QSC.cpp
@@ -42,7 +42,7 @@ void QSC::force(long N, const double *R, const long *atomicNrs, double *F,
             double r_ij = distance(box, R, i, j);
             qsc_parameters p;
             p = get_qsc_parameters(atomicNrs[i], atomicNrs[j]); 
-            if (r_ij > 2*p.a) continue;
+            //if (r_ij > 2*p.a) continue;
             rho[i] += pair_potential(r_ij, p.a, p.m);
         }
     }
@@ -56,7 +56,7 @@ void QSC::force(long N, const double *R, const long *atomicNrs, double *F,
             qsc_parameters p;
             /* Get the mixed parameters */
             p = get_qsc_parameters(atomicNrs[i], atomicNrs[j]); 
-            if (r_ij > 2*p.a) continue;
+            //if (r_ij > 2*p.a) continue;
             pair_term += p.epsilon*pair_potential(r_ij, p.a, p.n); 
         }
         /* Get the parameters for element i */
@@ -66,6 +66,8 @@ void QSC::force(long N, const double *R, const long *atomicNrs, double *F,
         //printf("c=%f,e=%f,a=%f,m=%f,n=%f\n",p.c,p.epsilon,p.a,p.m,p.n);
         //printf("attractive=%f repulsive=%f\n", attractive, repulsive);
     }
+    //printf("dist=%.4f\n", distance(box, R,0, 1));
+    //printf("U=%.4f\n", *U);
 
     //printf("%10.4f %10.4f\n", distance(box, R, 0, 1), *U);
     //printf("\t\t%10.4f\n", *U);
@@ -83,7 +85,7 @@ void QSC::force(long N, const double *R, const long *atomicNrs, double *F,
             p = get_qsc_parameters(atomicNrs[i], atomicNrs[j]); 
 
             double r_ij = distance(box, R, i, j);
-            if (r_ij > 2*p.a) continue;
+            //if (r_ij > 2*p.a) continue;
 
             double Fij;
             Fij =       p.epsilon *
@@ -91,16 +93,13 @@ void QSC::force(long N, const double *R, const long *atomicNrs, double *F,
                          (p.c*p.m*0.5*(pow(rho[i],-0.5)+pow(rho[j],-0.5)) *
                          pair_potential(r_ij, p.a, p.m)))/(r_ij*r_ij);
 
-            //this is correct for 2 atoms.
-            //mag_force = -p.epsilon/(2.0*r_ij) *
-            //            (2.0*p.n*pair_potential(r_ij, p.a, p.n) -
-            //             p.c*p.m*sqrt(pair_potential(r_ij, p.a, p.m)));
-            //printf("mf: %12.6g\n", mag_force);
-
             double diffx,diffy,diffz, Fijx, Fijy, Fijz;
             diffx = R[3*i  ] - R[3*j  ];
             diffy = R[3*i+1] - R[3*j+1];
             diffz = R[3*i+2] - R[3*j+2];
+            diffx = diffx-box[0]*floor(diffx/box[0]+0.5); 
+            diffy = diffy-box[1]*floor(diffy/box[1]+0.5);
+            diffz = diffz-box[2]*floor(diffz/box[2]+0.5);
 
             Fijx = Fij * diffx;
             Fijy = Fij * diffy;
@@ -114,6 +113,13 @@ void QSC::force(long N, const double *R, const long *atomicNrs, double *F,
             F[3*j+2] -= Fijz;
         }
     }
+
+    //double mag_force=0.0;
+    //for (int i=0;i<3*N;i++) {
+    //    mag_force += F[i]*F[i];
+    //}
+    //mag_force = sqrt(mag_force);
+    //printf("mag_force = %.4g U = %.4g\n", mag_force, *U);
 
     delete rho;
 }
