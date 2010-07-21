83c099f9ae321dedc4940341cac31a59dcf8508a
Sam Chill <SamChill@users.noreply.github.com>
2010-07-21 17:15:59 +0000

    added an optional distance cutoff option

--- tools/dist.py
+++ tools/dist.py
@@ -9,4 +9,11 @@ p1 = io.loadcon(sys.argv[1])
 p2 = io.loadcon(sys.argv[2])
 
 diff = atoms.per_atom_norm(p2.r-p1.r, p1.box)
-print diff
+
+if len(sys.argv) > 3:
+    cutoff = float(sys.argv[3])
+    for r in diff:
+        if r >= cutoff:
+            print r
+else:
+    print diff
