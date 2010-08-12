c91951481ae5e50884898153a5e4617bfe5d262e
Matthew Welborn <mattwelborn@users.noreply.github.com>
2010-08-12 20:22:26 +0000

    Fixed mpi output

--- tools/mpi_wrapper.c
+++ tools/mpi_wrapper.c
@@ -16,9 +16,9 @@ int main (int argc, char *argv[])
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);	
     MPI_Comm_size (MPI_COMM_WORLD, &size);
     
-    int len = strlen(argv[1]) + strlen(argv[rank + 2]) + 6;
+    int len = strlen(argv[1]) + strlen(argv[rank + 2]) + 16;
     char buffer[len];
-    snprintf(buffer, sizeof(buffer), "cd %s; %s", argv[rank + 2], argv[1]);
+    snprintf(buffer, sizeof(buffer), "cd %s; %s>/dev/null", argv[rank + 2], argv[1]);
     
     t1 = mysecond();
     system(buffer);
