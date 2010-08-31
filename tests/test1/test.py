2a0edc91861f3a43ff80c9ffc99e095ff9dc99ed
Sam Chill <SamChill@users.noreply.github.com>
2010-08-31 19:05:18 +0000

    fixed problem with test.py on OS X

--- tests/test1/test.py
+++ tests/test1/test.py
@@ -6,16 +6,19 @@ import sys
 failstr = "\n\nXXXXX XXXXX FAILED TEST 1 XXXXX XXXXX\n\n"
 passstr = "\n\n+++++ +++++ PASSED TEST 1 +++++ +++++\n\n"
 
-os.system("yes | ../../akmc.py --reset")
+os.system("echo 'y' | ../../akmc.py --reset")
 for i in range(143):
-    os.system("../../akmc.py")
+    retval = os.system("../../akmc.py")
+    if retval:
+        print failstr
+        sys.exit(1)
 
 unit = open("dynamics.test", 'r').readlines()
 result = open("dynamics.txt", 'r').readlines()
 
 if len(unit) != len(result):
     print failstr
-    sys.exit()
+    sys.exit(1)
 
 for i in range(len(unit)):
     unitLine = unit[i].strip().split()
