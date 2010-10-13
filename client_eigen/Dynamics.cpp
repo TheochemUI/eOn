c8c83356252fbbdf92be8e4c9c05ce516ef4aefd
Matthew Welborn <mattwelborn@users.noreply.github.com>
2010-10-13 20:05:37 +0000

    Eigen client builds

--- client_eigen/Dynamics.cpp
+++ client_eigen/Dynamics.cpp
@@ -3,21 +3,19 @@
 
 using namespace helper_functions;
 
-Dynamics::Dynamics(Matter *matter,Parameters *parameters)
+Dynamics::Dynamics(Matter *matter_passed,Parameters *parameters_passed)
 {
-    matter_ = matter;    
-    parameters_ = parameters;
-    dtScale_ = 1.0; //in unit of 10fs
+    matter = matter_passed;    
+    parameters = parameters_passed;
+    dtScale = 1.0; //in unit of 10fs
     kb=1.0/11604.5; //Kb in unit of eV
-	nFreeCoord_ = 3*matter->numberOfFreeAtoms();
-    tempListDouble_ = new double[nFreeCoord_];
+	nAtoms = matter->numberOfAtoms();
 
 };
 
 
 Dynamics::~Dynamics()
 {
-    delete [] tempListDouble_;
     return;
 };
 
@@ -33,48 +31,31 @@ void Dynamics::oneStep()
 
 void Dynamics::VerletStep1()
 {
-     double *positions;
-     double *velocities;
-     double *accelerations;
-	
-     positions = new double[nFreeCoord_];
-     velocities = new double[nFreeCoord_];
-     accelerations= new double[nFreeCoord_];
+     Matrix<double, Eigen::Dynamic, 3> positions;
+     Matrix<double, Eigen::Dynamic, 3> velocities;
+     Matrix<double, Eigen::Dynamic, 3> accelerations;
 
-     matter_->getFreePositions(positions);
-     matter_->getFreeVelocities(velocities);
-     matter_->getFreeAccelerations(accelerations);
+     positions = matter->getPositions();
+     velocities = matter->getVelocities();
+     accelerations = matter->getAccelerations();
      
-     multiplyScalar(tempListDouble_, accelerations, 0.5 * parameters_->mdTimeStep * dtScale_, nFreeCoord_);
-     add(velocities, tempListDouble_, velocities, nFreeCoord_);
-     matter_->setFreeVelocities(velocities);  // First update velocities
+     velocities += accelerations * 0.5 * parameters->mdTimeStep * dtScale;
+     matter->setVelocities(velocities);  // First update velocities
 
-     multiplyScalar(tempListDouble_, velocities, parameters_->mdTimeStep * dtScale_, nFreeCoord_);
-     add(positions, tempListDouble_, positions, nFreeCoord_);
-     matter_->setFreePositions(positions); // Update Positions
-     delete [] positions;
-     delete [] velocities; 
-     delete [] accelerations;
-     return;
+     positions += velocities * parameters->mdTimeStep * dtScale;
+     matter->setPositions(positions); // Update Positions
 };
 
 void Dynamics::VerletStep2()
 {
-     double *velocities;
-     double *accelerations;
+     Matrix<double, Eigen::Dynamic, 3> velocities(nAtoms,3);
+     Matrix<double, Eigen::Dynamic, 3> accelerations(nAtoms,3);
 
-     velocities = new double[nFreeCoord_];
-     accelerations= new double[nFreeCoord_];
+     velocities = matter->getVelocities();
+     accelerations = matter->getAccelerations();	
 
-     matter_->getFreeVelocities(velocities);
-     matter_->getFreeAccelerations(accelerations);	
-
-     multiplyScalar(tempListDouble_, accelerations, 0.5 * parameters_->mdTimeStep * dtScale_, nFreeCoord_);
-     add(velocities, tempListDouble_, velocities, nFreeCoord_);
-     matter_->setFreeVelocities(velocities);// Second update Velocities
-     delete [] velocities;
-     delete [] accelerations;
-     return;
+     velocities += accelerations * 0.5 * parameters->mdTimeStep * dtScale;
+     matter->setVelocities(velocities);// Second update Velocities
 };
 
 void Dynamics::fullSteps()
@@ -82,107 +63,101 @@ void Dynamics::fullSteps()
      bool stoped = false;
      long forceCallsTemp;
      long nsteps=0;
-     double *freeVelocity;
+     Matrix<double, Eigen::Dynamic, 3> velocity; 
      double EKin;
      double TKin,SumT=0.0,SumT2=0.0,AvgT,VarT;
-   
-     freeVelocity = new double[nFreeCoord_];
-     forceCallsTemp = matter_->getForceCalls();  
+  
+     long nFreeCoord = matter->numberOfFreeAtoms()*3; 
+     forceCallsTemp = matter->getForceCalls();  
 
      velocityScale();
 
      while(!stoped){
         oneStep();
 	nsteps++;
-        
-	matter_->getFreeVelocities(freeVelocity);
-        EKin=matter_->kineticEnergy();
-        TKin=(2*EKin/nFreeCoord_/kb); 
+     
+	velocity = matter->getVelocities();
+    EKin=matter->getKineticEnergy();
+    TKin=(2*EKin/nFreeCoord/kb); 
 	SumT+=TKin;
 	SumT2+=TKin*TKin;
         //printf("MDsteps %ld Ekin = %lf Tkin = %lf \n",nsteps,EKin,TKin); 
 
 	if (nsteps % 100 == 0){
-	    matter_->matter2xyz("movie", true);
+	    matter->matter2xyz("movie", true);
 	}
 
-	if (nsteps >= parameters_->mdSteps ){
+	if (nsteps >= parameters->mdSteps ){
 	    stoped = true;
 	}       
      }
      
      AvgT=SumT/nsteps;
      VarT=SumT2/nsteps-AvgT*AvgT;
-     printf("Tempeture : Average = %lf ; Variance = %lf ; Factor = %lf \n", AvgT,VarT,VarT/AvgT/AvgT*nFreeCoord_/2);
-	
-
-     delete [] freeVelocity;
-     return;
+     printf("Tempeture : Average = %lf ; Variance = %lf ; Factor = %lf \n", AvgT,VarT,VarT/AvgT/AvgT*nFreeCoord/2);
 };
 
 void Dynamics::Andersen(){
     
      double temp,alpha,Tcol,Pcol;//temp,sigma,Tcol,Pcol should be got from parameter.dat.
      double irand,v1,new_v,old_v;
-     double *mass;
-     double *freeVelocity;
-     long int nFreeAtoms;
+     Matrix<double, Eigen::Dynamic, 1> mass;
+     Matrix<double, Eigen::Dynamic, 3> velocity;
 
-     temp = parameters_->mdTemperture; //unit K
-     alpha = parameters_->Andersen_Alpha; //collision strength
-     Tcol = parameters_->Andersen_Tcol; // Average time between collision, in unit of dt
+     temp = parameters->mdTemperture; //unit K
+     alpha = parameters->Andersen_Alpha; //collision strength
+     Tcol = parameters->Andersen_Tcol; // Average time between collision, in unit of dt
      Pcol = 1.0-exp(-1.0/Tcol);
-     nFreeAtoms = matter_->numberOfFreeAtoms(); 
 
-     mass = new double[nFreeAtoms];
-     freeVelocity = new double[nFreeCoord_];
-     matter_->getFreeVelocities(freeVelocity);
-     matter_->getFreeMasses(mass);
+     velocity = matter->getVelocities();
+     mass = matter->getMasses();
 
-     for (long int i = 0;i<nFreeAtoms;i++){
-	  for (int j = 0; j < 3; j++){
-	       old_v = freeVelocity[3*i+j];
-	       irand = randomDouble();
-               if( irand < Pcol){
-		   v1 = sqrt(kb*temp/mass[i])*guaRandom(0.0,1.0);
-		   new_v = sqrt(1-alpha*alpha)*old_v+alpha*v1;
-                   freeVelocity[3*i+j] = new_v;
-	       }
-	  }
+     for (long int i = 0;i<nAtoms;i++)
+     {
+        if(!matter->getFixed(i))
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                old_v = velocity(i,j);
+                irand = randomDouble();
+                if( irand < Pcol)
+                {
+                    v1 = sqrt(kb*temp/mass[i])*guaRandom(0.0,1.0);
+                    new_v = sqrt(1-alpha*alpha)*old_v+alpha*v1;
+                    velocity(i,j) = new_v;
+                }
+	        }
+        }
      }
  
-     matter_->setFreeVelocities(freeVelocity);
+     matter->setVelocities(velocity);
      
-     delete [] freeVelocity;
-     delete [] mass;
      return;
 }
 
 void Dynamics::velocityScale(){
 	
      double temp,new_v;
-     double *mass;
-     double *freeVelocity;
+     Matrix<double, Eigen::Dynamic, 1> mass;
+     Matrix<double, Eigen::Dynamic, 3> velocity;
      long int nFreeAtoms;
 
-     temp = parameters_->mdTemperture;
-     nFreeAtoms = matter_->numberOfFreeAtoms(); 
-     mass = new double[nFreeAtoms];
-     freeVelocity = new double[nFreeCoord_];
-     matter_->getFreeVelocities(freeVelocity);
-     matter_->getFreeMasses(mass);
+     temp = parameters->mdTemperture;
+     nFreeAtoms = matter->numberOfFreeAtoms(); 
+     velocity = matter->getVelocities();
+     mass = matter->getMasses();
 
-     for (long int i = 0;i<nFreeAtoms;i++){
-	  for (int j = 0; j < 3; j++){
+     for (long int i = 0;i<nFreeAtoms;i++)
+     {
+	    for (int j = 0; j < 3; j++)
+        {
 	       new_v = sqrt(kb*temp/mass[i])*guaRandom(0.0,1.0);
-	       freeVelocity[3*i+j] = new_v;
-	  }
+	       velocity(i,j) = new_v;
+	    }
      }
 
-     matter_->setFreeVelocities(freeVelocity);
+     matter->setVelocities(velocity);
      
-     delete [] freeVelocity;
-     delete [] mass;
      return;
 }
 
