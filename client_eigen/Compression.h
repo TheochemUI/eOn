f801a11b6703fd6bbb786f29d7dd64dcf056435d
Matthew Welborn <mattwelborn@users.noreply.github.com>
2010-10-14 15:24:06 +0000

    Removed client_eigen branch

--- client_eigen/Compression.h
+++
@@ -1,2 +0,0 @@
-int create_archive(char *outname, char *path, int (*d_match)(char *));
-int extract_archive(char *filename);
--- client_eigen/Job.h
+++
@@ -1,7 +0,0 @@
-#ifndef JOB_H
-#define JOB_H
-class Job { 
-    public:
-        virtual void run(int bundleNumber)=0;
-};
-#endif
