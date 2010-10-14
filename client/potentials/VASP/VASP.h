82d64c88bbce24af3674895c0178b98fe39e95e1
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-10-14 23:17:04 +0000

    vasp potential is more robust, but only works on unix like systems at the moment
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@509 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/potentials/VASP/VASP.h
+++ client/potentials/VASP/VASP.h
@@ -11,15 +11,19 @@ class VASP : public PotentialsInterface
 		~VASP();
         void initialize() {};
         void cleanMemory(void);    
-        void force(long N, const double *R, const int *atomicNrs, double *F, double *U, const double *box);
+        void force(long N, const double *R, const int *atomicNrs, 
+                   double *F, double *U, const double *box);
 
 
     private:
-        void writeNEWCAR(long N, const double *R, int const *atomicNrs, const double *box);
+        void writeNEWCAR(long N, const double *R, const int *atomicNrs,
+                         const double *box);
         void readFU(long N, double *F, double *U);
+        void spawnVASP();
+        bool vaspRunning();
+        static bool firstRun;
         static long vaspRunCount;
-		static bool vaspRunning;
-
+        static pid_t vaspPID;
 };
 
 #endif
