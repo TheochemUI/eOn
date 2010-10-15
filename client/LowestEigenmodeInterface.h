253dc4addead21c0c52ce44d3c905438dfe0dc87
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-10-15 22:14:40 +0000

    Sam happy. No more gcc warnings for MacPorts gcc 4.5.
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@529 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/LowestEigenmodeInterface.h
+++ client/LowestEigenmodeInterface.h
@@ -17,13 +17,12 @@ USING_PART_OF_NAMESPACE_EIGEN //It hurts every time I type this
 /* Define the interface for the lowest eigenvalue determination algorithm */
 class LowestEigenmodeInterface{
 public:
-    virtual ~LowestEigenmodeInterface(){};
     void virtual startNewSearchAndCompute(Matter const *matter, Matrix<double, Eigen::Dynamic, 3>) = 0; 
     void virtual moveAndCompute(Matter const *matter) = 0;  
     double virtual getEigenvalue() = 0;
     /// Return eigenvector.
-    virtual Matrix<double, Eigen::Dynamic, 3> getEigenvector() {};
+    virtual Matrix<double, Eigen::Dynamic, 3> getEigenvector()=0;
         /** Set initial direction manually.*/
-    virtual void setEigenvector(long size, Matrix<double, Eigen::Dynamic, 3> eigenvector) {}
+    virtual void setEigenvector(Matrix<double, Eigen::Dynamic, 3> const eigenvector)=0;
 };
 #endif
