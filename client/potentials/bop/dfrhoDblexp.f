75b2fb4943908a739703c458301d1359c004d98e
ryeterrell <ryeterrell@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-10-18 16:41:10 +0000

    adding bop lib potential
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@532 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ client/potentials/bop/dfrhoDblexp.f
@@ -0,0 +1,36 @@
+c  Dblexp:  June 92
+c  Generalized to arbitrary order   Feb 92
+c  This routine evaluates teh derivative of the embedding function with respect
+c  to its argument (the density).
+
+        SUBROUTINE DFRHODRHO(rho1,natm1,ipot,fprho)
+
+        implicit real*8 (a-h,o-z)
+
+        include 'commonblks/parameters.cmn'
+        include 'commonblks/comconf.cmn'    
+        include 'commonblks/compotent.cmn'
+
+        dimension fprho(MAXATOMS,3),natm1(3),rho1(MAXATOMS,3)
+
+c     ---------------------------------------------------------------
+c   Interpret parameters in potpar:
+
+c        gfact=potpar(1,ipot)
+
+c    ----------------------------------------------------------------
+
+        do i = 1,natm1(ipot)
+          sum = potpar(indf1,ipot)
+          do io=2,iorderf
+              sum=sum+potpar(indf1+io-1,ipot)*io*rho1(i,ipot)**(io-1)
+          enddo 
+
+          fprho(i,ipot) = sum + potpar(1,ipot)
+c                      potpar(1,ipot) is the g-transformation parameter.
+
+        end do
+
+        RETURN
+        END
+
