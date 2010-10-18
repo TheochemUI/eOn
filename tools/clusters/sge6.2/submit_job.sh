9db9ea02197cffb0146517d2327d183dcf1496d7
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-10-18 22:27:23 +0000

    Added option to cluster communicator to set a name for the job.
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@537 9d962951-508e-48a8-94b5-974f9cfda47c

--- tools/clusters/sge6.2/submit_job.sh
+++ tools/clusters/sge6.2/submit_job.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qsub -V -b y -j y -o ll_out -S /bin/bash -N "akmc_$1" -wd "$2" ~/bin/eonclient | awk '{print $3}'
+qsub -V -b y -j y -o ll_out -S /bin/bash -N "$1" -wd "$2" ~/bin/eonclient | awk '{print $3}'
