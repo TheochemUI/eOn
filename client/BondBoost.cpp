6babf9418f08698c14b718804aab0c94ddd7b2a0
Liang Zhang <zhangbright@tsinghua.edu.cn>
2010-10-19 16:55:52 +0000

    BondBoost Code 85% finished; also add setForces() and pdistance()  to Matter class

--- client/BondBoost.cpp
+++ client/BondBoost.cpp
@@ -64,6 +64,7 @@ void BondBoost::initial(){
 }
 
 void BondBoost::boost(){
+
     long RMDS = parameters->mdSteps-3;
     Matrix<double, Eigen::Dynamic, 1> TABL_tmp(nTABs,1);
     bool flag = 0;
@@ -110,47 +111,153 @@ void BondBoost::boost(){
 }
 
 void BondBoost::Booststeps(){
+     long i,j,Mi;
      long AtomI_1,AtomI_2;
-     double QRR, PRR, Epsr_MAX, A_EPS_M;
+     double QRR, PRR, Epsr_MAX, A_EPS_M, Sum_V, Boost_Fact, DVMAX;
+     double Dforce, Fact_1, Fact_2,Mforce;
+     double Ri[3] = {0.0} , R = 0.0;
+
+     Matrix<double, Eigen::Dynamic, 3> OriForce(nAtoms,3);
+     Matrix<double, Eigen::Dynamic, 3> Free(nAtoms,3);
+     Matrix<double, Eigen::Dynamic, 3> AddForce(nBBs,3);
+     Matrix<double, Eigen::Dynamic, 3> TADF(nAtoms,3);
+     Matrix<double, Eigen::Dynamic, 3> NewForce(nAtoms,3);
+ 
+     OriForce.setZero();
+     TADF.setZero();
+     Free.setZero();
+     AddForce.setZero();
+     NewForce.setZero();
+
      QRR = 0.3;
      PRR = 0.95;
+     Epsr_MAX = 0.0;
+     A_EPS_M = 0.0;
+     Boost_Fact = 0.0;
+     Sum_V = 0.0;
+     DVMAX = 1.0;
+     Dforce = 0.0;
+     Mforce = 0.0;
+     Fact_1 = 0.0;
+     Fact_2 = 0.0;
+
      printf("haha::BoostSteps\n");
 
-     for(long i=0;i<nBBs;i++){
+     for(i=0;i<nBBs;i++){
          AtomI_1 = BBAList[2*i];
          AtomI_2 = BBAList[2*i+1];
          CBBLList(i,0) = matter->distance(AtomI_1,AtomI_2);
      }
 
-//     for (long i=0;i<nBBs;i++){
-//         printf("Boost::Distance between Atoms %ld and %ld is %lf\n",BBAList[2*i],BBAList[2*i+1],CBBLList(i,0));
-//     }
-
-     Epsr_MAX = 0.0;
-
-     for (long i=0;i<nBBs;i++){
+     for (i=0;i<nBBs;i++){
          Epsr_Q[i]=(CBBLList(i,0)-EBBLList(i,0))/EBBLList(i,0)/QRR;
-         printf("test EPSR_Q[%ld]=%lf\n",i,Epsr_Q[i]);
          if (abs(Epsr_Q[i]) >= Epsr_MAX ){
              Epsr_MAX = abs(Epsr_Q[i]);
          }
      }
      
      A_EPS_M=(1.0-Epsr_MAX*Epsr_MAX)*(1.0-Epsr_MAX*Epsr_MAX)/(1.0-PRR*PRR*Epsr_MAX*Epsr_MAX);
-
-     for (long i=0;i<nBBs;i++){
+/*
+     for (i=0;i<nBBs;i++){
          printf("Boost::Distance between Atoms %ld and %ld is %lf; EPSR_Q=%lf\n",BBAList[2*i],BBAList[2*i+1],CBBLList(i,0),Epsr_Q[i]);
      }
 
      printf("Boost::Epsr_MAX= %lf\n",Epsr_MAX);
      printf("Boost::A_EPS_M= %lf\n",A_EPS_M);
+*/   
+     if(Epsr_MAX < 1.0){
+        for(i=0;i<nBBs;i++){
+	    Sum_V +=DVMAX*(1.0-Epsr_Q[i]*Epsr_Q[i])/double(nBBs);
+	}
+	Boost_Fact = A_EPS_M*Sum_V;
+	//SPtime
+ 	//SDtime
+	//Ave_Boost_Fact
+   
+        for(i=0;i<nBBs;i++){
+	    if( abs(Epsr_Q[i]) < Epsr_MAX ){
+		Fact_1 = 2.0*A_EPS_M*DVMAX*Epsr_Q[i]/QRR/EBBLList(i,0)/double(nBBs);
+		Dforce = Fact_1;
+             
+		AtomI_1 = BBAList[2*i];
+		AtomI_2 = BBAList[2*i+1];
+		
+ 		R=matter->distance(AtomI_1,AtomI_2);
 
-     
-     
+		for(j=0;j<3;j++){
+                    Ri[j] = matter->pdistance(AtomI_1,AtomI_2,j);   
+	            AddForce(i,j)=Ri[j]/R*Dforce;
+                    TADF(AtomI_1,j)=TADF(AtomI_1,j)+AddForce(i,j);
+                    TADF(AtomI_2,j)=TADF(AtomI_2,j)-AddForce(i,j);
+		}
+              //  printf("%ld Bond:: Rx=%lf;Ry=%lf;Rz=%lf;Rsum=%lf;R=%lf\n",i,Ri[0],Ri[1],Ri[2],sqrt(Ri[0]*Ri[0]+Ri[1]*Ri[1]+Ri[2]*Ri[2]),R);
 
-        
+	    }	
+            else if( abs(Epsr_Q[i]) == Epsr_MAX ){
+  		Fact_1 = 2.0*A_EPS_M*DVMAX*Epsr_Q[i]/QRR/EBBLList(i,0)/double(nBBs);
+ 		Fact_2 = 2.0*(1.0-(Epsr_Q[i])*Epsr_Q[i])*Epsr_Q[i]*(2.0*(1.0-PRR*PRR*Epsr_Q[i]*Epsr_Q[i])-PRR*PRR*(1.0-(Epsr_Q[i])*Epsr_Q[i]))/QRR/EBBLList(i,0)/(1.0-PRR*PRR*Epsr_Q[i]*Epsr_Q[i])/(1.0-PRR*PRR*Epsr_Q[i]*Epsr_Q[i]); 
+        	Dforce=Fact_1+Sum_V*Fact_2;
 
-  }
+		AtomI_1 = BBAList[2*i];
+                AtomI_2 = BBAList[2*i+1];
+
+                Mi=i;
+		Mforce=Dforce;
+                R=matter->distance(AtomI_1,AtomI_2);
+
+                for(j=0;j<3;j++){
+                    Ri[j] = matter->pdistance(AtomI_1,AtomI_2,j);
+                    AddForce(i,j)=Ri[j]/R*Dforce;
+                    TADF(AtomI_1,j)=TADF(AtomI_1,j)+AddForce(i,j);
+                    TADF(AtomI_2,j)=TADF(AtomI_2,j)-AddForce(i,j);
+		}
+	    }
+/*
+            if(i != Mi){
+	        printf("DeltaF(Epsr_Q = %lf ), %lf  %lf  %lf; Atom %ld and Atom %ld\n", Epsr_Q[i],AddForce(i,0),AddForce(i,1),AddForce(i,2),BBAList[2*i],BBAList[2*i+1]);
+	    }else{
+		printf("DeltaF(Epsr_MAX = %lf) = %lf, %lf  %lf  %lf; Atom %ld and Atom %ld\n", Epsr_MAX,Mforce,AddForce(i,0),AddForce(i,1),AddForce(i,2),BBAList[2*i],BBAList[2*i+1]);
+	   }
+*/
+	}
+         OriForce = matter->getForces();
+        NewForce = OriForce + TADF;
+        Free = matter->getFree();
+        NewForce = NewForce.cwise() *  Free;
+	matter->setForces(NewForce);
+       
+        printf("TADF::\n");
+	for(i=0;i<nAtoms;i++){
+		printf("%lf   %lf   %lf\n",TADF(i,0),TADF(i,1),TADF(i,2));
+        }
+ 
+        printf("OriForces::\n");
+        for(i=0;i<nAtoms;i++){
+                printf("%lf   %lf   %lf\n",OriForce(i,0),OriForce(i,1),OriForce(i,2));
+        }
+  
+        printf("NewForces::\n");
+        for(i=0;i<nAtoms;i++){
+                printf("%lf   %lf   %lf\n",NewForce(i,0),NewForce(i,1),NewForce(i,2));
+        }
+        OriForce = matter->getForces();
+        printf("NewSettedForces::\n");
+        for(i=0;i<nAtoms;i++){
+                printf("%lf   %lf   %lf\n",OriForce(i,0),OriForce(i,1),OriForce(i,2));
+        }
+
+       
+
+
+
+     }
+     else if(Epsr_MAX >= 1.0){
+   	Boost_Fact = 0.0;
+	//SPtime = 
+	//Ave_Boost_Fact =
+	//
+     }
+}
 
 
 Matrix<double, Eigen::Dynamic,1> BondBoost::Rmdsteps() {
