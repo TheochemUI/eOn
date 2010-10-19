bbf7caee81b0e3ecb40246ecde938f7d7d7b85ac
Rye Terrell <wwwtyro@users.noreply.github.com>
2010-10-19 16:59:44 +0000

    bop lib potential

--- client/potentials/bop/bop.h
+++ client/potentials/bop/bop.h
@@ -1,9 +1,9 @@
-#ifndef ALUMINUM_POTENTIAL
-#define ALUMINUM_POTENTIAL
+#ifndef BOP_POTENTIAL
+#define BOP_POTENTIAL
 
 #include "../../PotentialsInterface.h"
 
-    /** External function implemented in Fortran. Calculate interactions between molecules of water using forcefield Aluminum.
+    /** External function implemented in Fortran. Calculate interactions between molecules of water using forcefield bop.
     @param[in]	N           Number of atoms.
     @param[in]	R           Array to positions of the atoms in Angstrom.
     @param[out]	F           Array used to return the forces resulting from interactions between molecules. Forces are in eV/Angstrom.
@@ -13,23 +13,28 @@
     
 extern "C" 
 {
-    void force_(const long int *N, const double *R, double *F, double *U, const double* bx, const double* by, const double* bz);
+    //void boplib_example_eam_(void);
+    void bopini_(void);
+    void boplib_calc_ef_(long *N, const double *R, double *box, double *U, double *F);    
+    //void force_(const long int *N, const double *R, double *F, double *U, const double* bx, const double* by, const double* bz);
 }    
-extern "C" 
-{
-    void potinit_();
-}    
+//extern "C" 
+//{
+//    void potinit_();
+//}    
 
-/** Aluminum potential.*/
-class Aluminum : public PotentialsInterface
+/** bop potential.*/
+class bop : public PotentialsInterface
 {     
 public:
-    Aluminum(void);
+    bop(void);
 	
     // To satify interface
     void initialize(void);    
     void cleanMemory(void);    
     void force(long N, const double *R, const int *atomicNrs, double *F, double *U, const double *box);
+    void writeFOX(long N, const double *R, int const *atomicNrs, const double *box);
+    static bool initialized;
 };
 #endif
 
