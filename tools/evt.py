d5f32a637232987d1dacae80c191249111ee9286
matt <matt@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-10-27 01:49:43 +0000

    Added energy vs. time plotter which attempts to use eOn functions to read data
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@581 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ tools/evt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+import sys
+
+import pathfix
+import io, atoms
+import config
+config.init(sys.argv[1])
+
+import akmc
+
+states = akmc.get_statelist(config.akmc_temperature/11604.5)
+
+e = []
+for i in range(states.get_num_states()):
+    e.append(states.get_state(i).get_energy())
+
+
+f = open('evt.plt', 'w')
+print >> f, "set title \"Energy of Minima versus Time\""
+print >> f, "set xlabel \"time(s)\""
+print >> f, "set ylabel \"Energy (eV)\""
+print >> f, "set nokey"
+print >> f, "plot \"-\" with lines" 
+
+dyn = io.Dynamics('dynamics.txt')
+for i in dyn.get():
+    print >> f, i['totaltime'], e[i['reactant']]
+
