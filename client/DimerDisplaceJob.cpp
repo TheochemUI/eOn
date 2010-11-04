648214d2864439be9e074b9ffd7a38b3782dd941
ryeterrell <ryeterrell@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-11-04 20:05:01 +0000

    displace test job
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@600 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/DimerDisplaceJob.cpp
+++ client/DimerDisplaceJob.cpp
@@ -4,6 +4,7 @@
 #include "Constants.h"
 #include "HelperFunctions.h"
 #include "EpiCenters.h"
+#include "Dimer.h"
 
 using namespace helper_functions;
 
@@ -19,28 +20,108 @@ void DimerDisplaceJob::run(int bundleNumber)
     // No bundling for this job, so bundleNumber is ignored.
     
     // Load the displacement con file and get the position.
-    Matter *reactant = new Matter(parameters);
-    reactant->con2matter("reactant_passed.con");
+    
+    FILE *results = fopen("results.dat", "w");
+    fprintf(results, "%15s   %15s   %15s   %15s   %15s\n", "cutoff", "magnitude", "curvature", "dE", "result");
 
-    long epicenter = EpiCenters::minimalCoordinatedEpiCenter(reactant);
-    printf("\nepicenter: %d\n\n", epicenter);
+    int nSteps = 1024; //XXX Parameterize.
+    double cutoff = 3.3; //XXX Parameterize.
+    double cutoffStep = 0.25;//XXX Parameterize.
+    double magnitude = 1.0; //XXX Parameterize.
+    double magnitudeStep = 0.25;//XXX Parameterize.
+    double iterMax = 32; //XXX Parameterize.
+    double torqueConvergence = 0.01; //XXX Parameterize.
+    double maxCurvature = -0.1;
+    double max_dE = 10.0;
 
-    // Create a random displacement.
-    Matrix<double, Eigen::Dynamic, 3> displacement;        
-    displacement.resize(reactant->numberOfAtoms(), 3);
-    displacement.setZero();
-    for(int i = 0; i < reactant->numberOfAtoms(); i++)
+    for(int k = 0; k < nSteps; k++)
     {
-        for(int j = 0; j < 3; j++)
+
+        double newCutoff = guaRandom(cutoff, cutoffStep);
+        double newMagnitude = guaRandom(magnitude, magnitudeStep);
+
+        Matter *reactant = new Matter(parameters);
+        reactant->con2matter("reactant_passed.con");
+        double e0 = reactant->getPotentialEnergy();
+
+        long epicenter = EpiCenters::minimalCoordinatedEpiCenter(reactant);
+        
+        
+        // Create a random displacement.
+        Matrix<double, Eigen::Dynamic, 3> displacement;        
+        displacement.resize(reactant->numberOfAtoms(), 3);
+        displacement.setZero();
+        for(int i = 0; i < reactant->numberOfAtoms(); i++)
         {
-            if(!reactant->getFixed(i))
+            if(reactant->distance(epicenter, i) < newCutoff)
             {
-                displacement(i, j) = randomDouble(1.0);
+                for(int j = 0; j < 3; j++)
+                {
+                    if(!reactant->getFixed(i))
+                    {
+                        displacement(i, j) = randomDouble(1.0);
+                    }
+                }
             }
         }
+        displacement.normalize(); 
+        displacement *= guaRandom(0.0, newMagnitude);
+        
+        reactant->setPositions(reactant->getPositions() + displacement);
+        
+        Dimer *d = new Dimer(reactant, parameters);
+        
+        d->startNewSearchAndCompute(reactant, displacement);
+        d->moveAndCompute(reactant);
+        int iterCount = 0;
+        while(d->stats[0] > torqueConvergence && iterCount < iterMax)
+        {
+            d->moveAndCompute(reactant);
+            iterCount++;
+        }
+        double e1 = reactant->getPotentialEnergy();
+        if(e1 - e0 < max_dE && d->stats[1] < maxCurvature)
+        {
+            cutoff = newCutoff;
+            magnitude = newMagnitude;
+            fprintf(results, "%15f   %15f   %15f   %15f   %15s\n", newCutoff, newMagnitude, d->stats[1], e1 - e0, "good");
+        }
+        else
+        {
+            fprintf(results, "%15f   %15f   %15f   %15f   %15s\n", newCutoff, newMagnitude, d->stats[1], e1 - e0, "bad");
+        }
+
+        delete d;
+        delete reactant;
+        
     }
-    displacement.normalize(); 
-
-
     
+    fclose(results);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
