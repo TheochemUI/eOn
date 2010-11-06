1afd91ea32b555fd1dfe0e259550d8cd241a92ba
ryeterrell <ryeterrell@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-11-06 00:48:34 +0000

    displace sample job now gridded, not random
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@609 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/DisplacementSamplingJob.cpp
+++ client/DisplacementSamplingJob.cpp
@@ -5,6 +5,7 @@
 #include "HelperFunctions.h"
 #include "EpiCenters.h"
 #include "Dimer.h"
+#include <math.h>
 
 using namespace helper_functions;
 
@@ -19,85 +20,96 @@ void DisplacementSamplingJob::run(int bundleNumber)
 {
     // No bundling for this job, so bundleNumber is ignored.
     
-   
     FILE *results = fopen("results.dat", "w");
-    fprintf(results, "%15s   %15s   %15s   %15s   %15s\n", "cutoff", "magnitude", "curvature", "dE", "result");
+    fprintf(results, "%15s   %15s   %15s\n", "cutoff", "magnitude", "fraction good");
 
-    long   nSteps = parameters->displaceNSteps;
-    double cutoff = parameters->displaceCutoff;
-    double cutoffStep = parameters->displaceCutoffStep;
-    double magnitude = parameters->displaceMagnitude;
-    double magnitudeStep = parameters->displaceMagnitudeStep;
     long   iterMax = parameters->displaceIterMax;
     double torqueConvergence = parameters->displaceTorqueConvergence;
     double maxCurvature = parameters->displaceMaxCurvature;
     double max_dE = parameters->displaceMaxDE;
+    int nSamples = parameters->displaceNSamples;;
 
     parameters->dimerRotationsHigh = 1;
     parameters->dimerRotationsLow = 1;
     parameters->dimerWindowHigh = 0.0;
     parameters->dimerWindowLow = 0.0;
 
-    for(int k = 0; k < nSteps; k++)
+    std::stringstream cutoffStream(parameters->displaceCutoffs);
+    while(true)
     {
-
-        double newCutoff = guaRandom(cutoff, cutoffStep);
-        double newMagnitude = guaRandom(magnitude, magnitudeStep);
-
-        Matter *reactant = new Matter(parameters);
-        reactant->con2matter("reactant_passed.con");
-        double e0 = reactant->getPotentialEnergy();
-
-        long epicenter = EpiCenters::minimalCoordinatedEpiCenter(reactant);
-        
-        
-        // Create a random displacement.
-        Matrix<double, Eigen::Dynamic, 3> displacement;        
-        displacement.resize(reactant->numberOfAtoms(), 3);
-        displacement.setZero();
-        for(int i = 0; i < reactant->numberOfAtoms(); i++)
+        double cutoff;
+        cutoffStream >> cutoff;
+        if(!cutoffStream)
         {
-            if(reactant->distance(epicenter, i) < newCutoff)
+            break;
+        }
+        std::stringstream magnitudeStream(parameters->displaceMagnitudes);
+        while(true)
+        {
+            double magnitude;
+            magnitudeStream >> magnitude;
+            if(!magnitudeStream)
             {
-                for(int j = 0; j < 3; j++)
+                break;
+            }
+    
+            double nGood = 0;
+
+            for(int k = 0; k < nSamples; k++)
+            {
+                Matter *reactant = new Matter(parameters);
+                reactant->con2matter("reactant_passed.con");
+                double e0 = reactant->getPotentialEnergy();
+
+                long epicenter = EpiCenters::minimalCoordinatedEpiCenter(reactant);
+                
+                // Create a random displacement.
+                Matrix<double, Eigen::Dynamic, 3> displacement;        
+                displacement.resize(reactant->numberOfAtoms(), 3);
+                displacement.setZero();
+                for(int i = 0; i < reactant->numberOfAtoms(); i++)
                 {
-                    if(!reactant->getFixed(i))
+                    if(reactant->distance(epicenter, i) < cutoff)
                     {
-                        displacement(i, j) = randomDouble(1.0);
+                        for(int j = 0; j < 3; j++)
+                        {
+                            if(!reactant->getFixed(i))
+                            {
+                                displacement(i, j) = randomDouble(1.0);
+                            }
+                        }
                     }
                 }
-            }
-        }
-        displacement.normalize(); 
-        displacement *= guaRandom(0.0, newMagnitude);
-        
-        reactant->setPositions(reactant->getPositions() + displacement);
-        
-        Dimer *d = new Dimer(reactant, parameters);
-        
-        d->startNewSearchAndCompute(reactant, displacement);
-        d->moveAndCompute(reactant);
-        int iterCount = 0;
-        while(d->stats[0] > torqueConvergence && iterCount < iterMax)
-        {
-            d->moveAndCompute(reactant);
-            iterCount++;
-        }
-        double e1 = reactant->getPotentialEnergy();
-        if(e1 - e0 < max_dE && d->stats[1] < maxCurvature)
-        {
-            cutoff = newCutoff;
-            magnitude = newMagnitude;
-            fprintf(results, "%15f   %15f   %15f   %15f   %15s\n", newCutoff, newMagnitude, d->stats[1], e1 - e0, "good");
-        }
-        else
-        {
-            fprintf(results, "%15f   %15f   %15f   %15f   %15s\n", newCutoff, newMagnitude, d->stats[1], e1 - e0, "bad");
-        }
+                
+                displacement.normalize(); 
+                displacement *= guaRandom(0.0, magnitude);
+                
+                reactant->setPositions(reactant->getPositions() + displacement);
+                
+                Dimer *d = new Dimer(reactant, parameters);
+                
+                d->startNewSearchAndCompute(reactant, displacement);
+                d->moveAndCompute(reactant);
+                int iterCount = 0;
+                while(d->stats[0] > torqueConvergence && iterCount < iterMax)
+                {
+                    d->moveAndCompute(reactant);
+                    iterCount++;
+                }
+                double e1 = reactant->getPotentialEnergy();
+                if(e1 - e0 < max_dE && d->stats[1] < maxCurvature)
+                {
+                    nGood++;
+                }
 
-        delete d;
-        delete reactant;
-        
+                delete d;
+                delete reactant;
+                
+            }
+
+            fprintf(results, "%15f   %15f   %15f\n", cutoff, magnitude, nGood / nSamples);
+
+        }
     }
     
     fclose(results);
