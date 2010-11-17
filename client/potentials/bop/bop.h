540565f6bc9ededab44d107af4a0f1ec6085778a
andreas <andreas@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-11-17 11:27:14 +0000

    Added GPL lincense header to most files, still missing Lanczos, EAM, QSC, platinum-water, Eigen
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@629 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/potentials/bop/bop.h
+++ client/potentials/bop/bop.h
@@ -1,4 +1,13 @@
-#ifndef BOP_POTENTIAL
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//
+//-----------------------------------------------------------------------------------#ifndef BOP_POTENTIAL
 #define BOP_POTENTIAL
 
 #include "../../PotentialsInterface.h"
--- client/potentials/bopfox/bopfox.cpp
+++ client/potentials/bopfox/bopfox.cpp
@@ -1,4 +1,13 @@
-#include <iostream>
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//
+//-----------------------------------------------------------------------------------#include <iostream>
 #include <cstdio>
 #include <stdlib.h>
 #include <unistd.h>
