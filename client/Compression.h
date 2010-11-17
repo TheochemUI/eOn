38fdfbc0c103ff78f0840ee101f288fee320412c
Andreas Pedersen <andped10@users.noreply.github.com>
2010-11-17 11:27:14 +0000

    Added GPL lincense header to most files, still missing Lanczos, EAM, QSC, platinum-water, Eigen

--- client/Compression.h
+++ client/Compression.h
@@ -1,2 +1,12 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//
+//-----------------------------------------------------------------------------------
 int create_archive(char *outname, char *path, int (*d_match)(char *));
 int extract_archive(char *filename);
--- client/potentials/bop/bop.cpp
+++ client/potentials/bop/bop.cpp
@@ -1,4 +1,13 @@
-#include "bop.h"
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//
+//-----------------------------------------------------------------------------------#include "bop.h"
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
