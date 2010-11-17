6b7d57e3e5b7e8997022b3b3468fb6210bd90705
andreas <andreas@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-11-17 11:27:14 +0000

    Added GPL lincense header to most files, still missing Lanczos, EAM, QSC, platinum-water, Eigen
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@629 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/Hessian.h
+++ client/Hessian.h
@@ -1,3 +1,13 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//
+//-----------------------------------------------------------------------------------
 #ifndef HESSIAN_H
 #define HESSIAN_H
 
