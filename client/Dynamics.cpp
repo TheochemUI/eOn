782997865bd7fc6d3c47c985f04b66893c7a9533
brightzhang <brightzhang@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-12-06 17:29:13 +0000

    Rearranged andersenVerlet() in Dynamics.cpp
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@690 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/Dynamics.cpp
+++ client/Dynamics.cpp
@@ -38,11 +38,8 @@ Dynamics::~Dynamics()
 
 void Dynamics::oneStep(double temperature)
 {
-    andersen(temperature);
     if(parameters->thermoType == ANDERSEN){
-       andersen(temperature);
-       verletStep1();
-       verletStep2();
+       andersenVerlet(temperature);
     }
     else if(parameters->thermoType == NOSE_HOVER){
        noseHoverVerlet(temperature);
@@ -50,144 +47,29 @@ void Dynamics::oneStep(double temperature)
     return;
 }
 
-void Dynamics::verletStep1()
+void Dynamics::andersenVerlet(double temperature)
 {
      Matrix<double, Eigen::Dynamic, 3> positions;
      Matrix<double, Eigen::Dynamic, 3> velocities;
      Matrix<double, Eigen::Dynamic, 3> accelerations;
-
+     
      positions = matter->getPositions();
      velocities = matter->getVelocities();
      accelerations = matter->getAccelerations();
+     
+     andersen(temperature);//andersen Thermostat;
 
      velocities += accelerations * 0.5 * parameters->mdTimeStep * dtScale;
      matter->setVelocities(velocities);  // first update velocities
 
      positions += velocities * parameters->mdTimeStep * dtScale;
-
-     //positions = matter->pbc(positions);
      matter->setPositions(positions); // update positions
+
+     accelerations = matter->getAccelerations();
+     velocities += accelerations * 0.5 * parameters->mdTimeStep * dtScale;
+     matter->setVelocities(velocities); // second update velocities
 }
 
-void Dynamics::verletStep2()
-{
-    Matrix<double, Eigen::Dynamic, 3> velocities(nAtoms,3);
-    Matrix<double, Eigen::Dynamic, 3> accelerations(nAtoms,3);
-
-    velocities = matter->getVelocities();
-    accelerations = matter->getAccelerations();
-
-    velocities += accelerations * 0.5 * parameters->mdTimeStep * dtScale;
-    matter->setVelocities(velocities); // second update velocities
-}
-
-void Dynamics::fullSteps(double temperature)
-{
-    bool stoped = false;
-    long forceCallsTemp;
-    long nsteps = 0;
-    Matrix<double, Eigen::Dynamic, 3> velocity; 
-    double kinE, kinT;
-    double sumT=0.0, sumT2=0.0, avgT, varT;
-    long nFreeCoord = matter->numberOfFreeAtoms()*3; 
-    forceCallsTemp = matter->getForceCalls();  
-
-    velocityScale(temperature);
-
-    while(!stoped)
-    {
-        oneStep(temperature);
-        nsteps++;
-
-        velocity = matter->getVelocities();
-        kinE = matter->getKineticEnergy();
-        kinT = (2*kinE/nFreeCoord/kb); 
-        sumT += kinT;
-        sumT2 += kinT*kinT;
-        //printf("MDsteps %ld kinE = %lf Tkin = %lf \n",nsteps,kinE,kinT); 
-/*
-        if (nsteps % 100 == 0){
-            matter->matter2xyz("movie", true);
-        }
-*/
-        if (nsteps >= parameters->mdSteps){
-            stoped = true;
-        }
-    }
-
-    avgT=sumT/nsteps;
-    varT=sumT2/nsteps-avgT*avgT;
-    printf("Temperature : Average = %lf ; Variance = %lf ; Factor = %lf \n", avgT,varT,varT/avgT/avgT*nFreeCoord/2);
-}
-
-void Dynamics::andersen(double temperature)
-{
-    double alpha, tCol, pCol; // sigma, tCol, pCol should be obtained from parameter.dat
-    double irand, v1, vNew, vOld;
-    Matrix<double, Eigen::Dynamic, 1> mass;
-    Matrix<double, Eigen::Dynamic, 3> velocity;
-
-    alpha = parameters->thermoAndersenAlpha; // collision strength
-    tCol = parameters->thermoAndersenTcol; // average time between collision, in unit of dt
-    pCol = 1.0-exp(-1.0/tCol);
-
-    velocity = matter->getVelocities();
-    mass = matter->getMasses();
-
-    for (long int i = 0;i<nAtoms;i++)
-    {
-        if(!matter->getFixed(i))
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                vOld = velocity(i,j);
-                irand = randomDouble();
-                if( irand < pCol)
-                {
-                    v1 = sqrt(kb*temperature/mass[i])*gaussRandom(0.0,1.0);
-                    vNew = sqrt(1-alpha*alpha)*vOld+alpha*v1;
-                    velocity(i,j) = vNew;
-                }
-            }
-        }
-    }
-    matter->setVelocities(velocity);
-    return;
-}
-
-void Dynamics::velocityScale(double temperature)
-{
-    double vNew, kinT, kinE;
-    Matrix<double, Eigen::Dynamic, 1> mass;
-    Matrix<double, Eigen::Dynamic, 3> velocity;
-    long nFreeCoord = matter->numberOfFreeAtoms()*3;
-
-    velocity = matter->getVelocities();
-    mass = matter->getMasses();
-
-    for (long int i = 0;i<nAtoms;i++)
-    {
-        if(!matter->getFixed(i))
-        for (int j = 0; j < 3; j++)
-        {
-           // printf("mass[%ld] = %lf\n",i,mass(i));
-           vNew = sqrt(kb*temperature/mass[i])*gaussRandom(0.0,1.0);
-           velocity(i,j) = vNew;
-        }
-    }
-
-    matter->setVelocities(velocity);  
-    kinE = matter->getKineticEnergy();
-    kinT = (2*kinE/nFreeCoord/kb);
-//    printf("Tkin_1 = %lf\n",kinT);
-    velocity = velocity*sqrt(temperature/kinT);
-    matter->setVelocities(velocity);
-//    kinE = matter->getKineticEnergy();
-//    kinT = (2*EKin/nFreeCoord/kb);
-//    printf("Tkin_2 = %lf\n",kinT);
-
-    return;
-}
 
 void Dynamics::noseHoverVerlet(double temperature){
     double smass = parameters->thermoNoseMass;
@@ -286,3 +168,115 @@ void Dynamics::noseHoverVerlet(double temperature){
     matter->setPositions(p);
     return;
 }
+
+
+void Dynamics::fullSteps(double temperature)
+{
+    bool stoped = false;
+    long forceCallsTemp;
+    long nsteps = 0;
+    Matrix<double, Eigen::Dynamic, 3> velocity; 
+    double kinE, kinT;
+    double sumT=0.0, sumT2=0.0, avgT, varT;
+    long nFreeCoord = matter->numberOfFreeAtoms()*3; 
+    forceCallsTemp = matter->getForceCalls();  
+
+    velocityScale(temperature);
+
+    while(!stoped)
+    {
+        oneStep(temperature);
+        nsteps++;
+
+        velocity = matter->getVelocities();
+        kinE = matter->getKineticEnergy();
+        kinT = (2*kinE/nFreeCoord/kb); 
+        sumT += kinT;
+        sumT2 += kinT*kinT;
+        //printf("MDsteps %ld kinE = %lf Tkin = %lf \n",nsteps,kinE,kinT); 
+/*
+        if (nsteps % 100 == 0){
+            matter->matter2xyz("movie", true);
+        }
+*/
+        if (nsteps >= parameters->mdSteps){
+            stoped = true;
+        }
+    }
+
+    avgT=sumT/nsteps;
+    varT=sumT2/nsteps-avgT*avgT;
+    printf("Temperature : Average = %lf ; Variance = %lf ; Factor = %lf \n", avgT,varT,varT/avgT/avgT*nFreeCoord/2);
+}
+
+void Dynamics::velocityScale(double temperature)
+{
+    double vNew, kinT, kinE;
+    Matrix<double, Eigen::Dynamic, 1> mass;
+    Matrix<double, Eigen::Dynamic, 3> velocity;
+    long nFreeCoord = matter->numberOfFreeAtoms()*3;
+
+    velocity = matter->getVelocities();
+    mass = matter->getMasses();
+
+    for (long int i = 0;i<nAtoms;i++)
+    {
+        if(!matter->getFixed(i))
+        for (int j = 0; j < 3; j++)
+        {
+           // printf("mass[%ld] = %lf\n",i,mass(i));
+           vNew = sqrt(kb*temperature/mass[i])*gaussRandom(0.0,1.0);
+           velocity(i,j) = vNew;
+        }
+    }
+
+    matter->setVelocities(velocity);  
+    kinE = matter->getKineticEnergy();
+    kinT = (2*kinE/nFreeCoord/kb);
+//    printf("Tkin_1 = %lf\n",kinT);
+    velocity = velocity*sqrt(temperature/kinT);
+    matter->setVelocities(velocity);
+//    kinE = matter->getKineticEnergy();
+//    kinT = (2*EKin/nFreeCoord/kb);
+//    printf("Tkin_2 = %lf\n",kinT);
+
+    return;
+}
+
+void Dynamics::andersen(double temperature)
+{
+    double alpha, tCol, pCol; // sigma, tCol, pCol should be obtained from parameter.dat
+    double irand, v1, vNew, vOld;
+    Matrix<double, Eigen::Dynamic, 1> mass;
+    Matrix<double, Eigen::Dynamic, 3> velocity;
+
+    alpha = parameters->thermoAndersenAlpha; // collision strength
+    tCol = parameters->thermoAndersenTcol; // average time between collision, in unit of dt
+    pCol = 1.0-exp(-1.0/tCol);
+
+    velocity = matter->getVelocities();
+    mass = matter->getMasses();
+
+    for (long int i = 0;i<nAtoms;i++)
+    {
+        if(!matter->getFixed(i))
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                vOld = velocity(i,j);
+                irand = randomDouble();
+                if( irand < pCol)
+                {
+                    v1 = sqrt(kb*temperature/mass[i])*gaussRandom(0.0,1.0);
+                    vNew = sqrt(1-alpha*alpha)*vOld+alpha*v1;
+                    velocity(i,j) = vNew;
+                }
+            }
+        }
+    }
+    matter->setVelocities(velocity);
+    return;
+}
+
+
+  
