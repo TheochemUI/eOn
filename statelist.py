8f4a8b4b9e9e5dc1be8db9cb1064948cd4ebcf31
Graeme Henkelman <iz1ng@users.noreply.github.com>
2010-12-09 07:24:47 +0000

    degubbing names from our docs changes

--- statelist.py
+++ statelist.py
@@ -6,10 +6,9 @@
 ##
 ## A copy of the GNU General Public License is available at
 ## http://www.gnu.org/licenses/
-##
 ##-----------------------------------------------------------------------------------
-""" The statelist module. """
 
+""" The statelist module. """
 
 import logging
 logger = logging.getLogger('statelist')
