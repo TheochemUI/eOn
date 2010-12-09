140d473d30c0681872b8e706a2b540e944ffdc6c
graeme <graeme@9d962951-508e-48a8-94b5-974f9cfda47c>
2010-12-09 07:24:47 +0000

    degubbing names from our docs changes
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@783 9d962951-508e-48a8-94b5-974f9cfda47c

--- recycling.py
+++ recycling.py
@@ -6,12 +6,10 @@
 ##
 ## A copy of the GNU General Public License is available at
 ## http://www.gnu.org/licenses/
-##
 ##-----------------------------------------------------------------------------------
+
 import os
-
 import numpy
-
 import atoms
 import io
 
