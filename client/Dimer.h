d264616acb602121b903b98902cc85e490eff7a2
Graeme Henkelman <iz1ng@users.noreply.github.com>
2010-12-17 06:56:08 +0000

    improved dimer is an option; more variable name changes in the dimer method

--- client/Dimer.h
+++ client/Dimer.h
@@ -24,14 +24,10 @@ USING_PART_OF_NAMESPACE_EIGEN
 #include "Parameters.h"
 #include "LowestEigenmodeInterface.h"
 
-#define PI 3.141592653589793
-
-// The dimer method to find the lowest curvature mode
+// dimer method to find the lowest curvature mode
 class Dimer : public LowestEigenmodeInterface{
 
 public:
-
-    long totalForceCalls;
 
     Dimer(Matter const *matter, Parameters *parameters);
     ~Dimer();
@@ -42,9 +38,16 @@ public:
     void setEigenvector(Matrix<double, Eigen::Dynamic, 3> const eigenvector); // set the dimer direction (eigenvector)
     Matrix<double, Eigen::Dynamic, 3>  getEigenvector();  // return the current eigenvector
 
+    // stats information
+    long totalForceCalls;
+    double statsTorque;
+    double statsCurvature;
+    double statsAngle;
+    double statsRotations;
+
 private:
 
-    Matter *matterInitial; // center of the dimer
+    Matter *matterCenter; // center of the dimer
     Matter *matterDimer; //one configuration of the dimer
     Matrix<double, Eigen::Dynamic, 3> directionNorm; // direction along the dimer
     Matrix<double, Eigen::Dynamic, 3> rotationalPlaneNorm; // direction normal to the plane of dimer rotation 
@@ -52,7 +55,7 @@ private:
     int nAtoms;
     Parameters *parameters;
 
-    // The rotational plane that is going to be used is determined with the Conjugate Gradient method
+    // The rotational plane that is going to be used is determined with the conjugate gradient method
     void determineRotationalPlane(Matrix<double, Eigen::Dynamic, 3> rotationalForce, 
                                   Matrix<double, Eigen::Dynamic, 3> &rotationalForceOld, 
                                   Matrix<double, Eigen::Dynamic, 3> rotationalPlaneNormOld,
