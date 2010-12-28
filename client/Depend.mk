43fd4ee00707fa9d86f7217df3fac8ad2e3ad826
Rye Terrell <wwwtyro@users.noreply.github.com>
2010-12-28 17:27:51 +0000

    removed QMBox

--- client/Depend.mk
+++ client/Depend.mk
@@ -522,7 +522,7 @@ Dynamics.o: Dynamics.cpp Dynamics.h MinimizersInterface.h Matter.h \
  Eigen/src/Sparse/SparseDiagonalProduct.h \
  Eigen/src/Sparse/TriangularSolver.h Eigen/src/Sparse/SparseLLT.h \
  Eigen/src/Sparse/SparseLDLT.h Eigen/src/Sparse/SparseLU.h \
- HelperFunctions.h
+ HelperFunctions.h ConjugateGradients.h
 EpiCenters.o: EpiCenters.cpp EpiCenters.h Matter.h Parameters.h \
  Constants.h Potentials.h PotentialsInterface.h Eigen/Eigen Eigen/Dense \
  Eigen/Core Eigen/src/Core/util/DisableMSVCWarnings.h \
@@ -635,9 +635,8 @@ FiniteDifferenceJob.o: FiniteDifferenceJob.cpp FiniteDifferenceJob.h \
  Eigen/src/Sparse/SparseProduct.h \
  Eigen/src/Sparse/SparseDiagonalProduct.h \
  Eigen/src/Sparse/TriangularSolver.h Eigen/src/Sparse/SparseLLT.h \
- Eigen/src/Sparse/SparseLDLT.h Eigen/src/Sparse/SparseLU.h \
- ConjugateGradients.h Matter.h Potentials.h PotentialsInterface.h \
- MinimizersInterface.h
+ Eigen/src/Sparse/SparseLDLT.h Eigen/src/Sparse/SparseLU.h Matter.h \
+ Potentials.h PotentialsInterface.h EpiCenters.h HelperFunctions.h
 HelperFunctions.o: HelperFunctions.cpp HelperFunctions.h Eigen/Eigen \
  Eigen/Dense Eigen/Core Eigen/src/Core/util/DisableMSVCWarnings.h \
  Eigen/src/Core/util/Macros.h Eigen/src/Core/util/Constants.h \
@@ -806,6 +805,63 @@ HessianJob.o: HessianJob.cpp HessianJob.h Job.h Parameters.h Constants.h \
  Eigen/src/Sparse/TriangularSolver.h Eigen/src/Sparse/SparseLLT.h \
  Eigen/src/Sparse/SparseLDLT.h Eigen/src/Sparse/SparseLU.h Hessian.h
 INIFile.o: INIFile.cpp INIFile.h
+ImprovedDimer.o: ImprovedDimer.cpp ImprovedDimer.h debug.h Eigen/Eigen \
+ Eigen/Dense Eigen/Core Eigen/src/Core/util/DisableMSVCWarnings.h \
+ Eigen/src/Core/util/Macros.h Eigen/src/Core/util/Constants.h \
+ Eigen/src/Core/util/ForwardDeclarations.h Eigen/src/Core/util/Meta.h \
+ Eigen/src/Core/util/XprHelper.h Eigen/src/Core/util/StaticAssert.h \
+ Eigen/src/Core/util/Memory.h Eigen/src/Core/NumTraits.h \
+ Eigen/src/Core/MathFunctions.h Eigen/src/Core/GenericPacketMath.h \
+ Eigen/src/Core/arch/SSE/PacketMath.h Eigen/src/Core/Functors.h \
+ Eigen/src/Core/MatrixBase.h Eigen/src/Core/Coeffs.h \
+ Eigen/src/Core/Assign.h Eigen/src/Core/MatrixStorage.h \
+ Eigen/src/Core/NestByValue.h Eigen/src/Core/Flagged.h \
+ Eigen/src/Core/Matrix.h Eigen/src/Core/Cwise.h \
+ Eigen/src/Core/CwiseBinaryOp.h Eigen/src/Core/CwiseUnaryOp.h \
+ Eigen/src/Core/CwiseNullaryOp.h Eigen/src/Core/Dot.h \
+ Eigen/src/Core/Product.h Eigen/src/Core/DiagonalProduct.h \
+ Eigen/src/Core/SolveTriangular.h Eigen/src/Core/MapBase.h \
+ Eigen/src/Core/Map.h Eigen/src/Core/Block.h Eigen/src/Core/Minor.h \
+ Eigen/src/Core/Transpose.h Eigen/src/Core/DiagonalMatrix.h \
+ Eigen/src/Core/DiagonalCoeffs.h Eigen/src/Core/Sum.h \
+ Eigen/src/Core/Redux.h Eigen/src/Core/Visitor.h Eigen/src/Core/Fuzzy.h \
+ Eigen/src/Core/IO.h Eigen/src/Core/Swap.h \
+ Eigen/src/Core/CommaInitializer.h Eigen/src/Core/Part.h \
+ Eigen/src/Core/CacheFriendlyProduct.h \
+ Eigen/src/Core/util/EnableMSVCWarnings.h Eigen/Array \
+ Eigen/src/Array/CwiseOperators.h Eigen/src/Array/Functors.h \
+ Eigen/src/Array/BooleanRedux.h Eigen/src/Array/Select.h \
+ Eigen/src/Array/PartialRedux.h Eigen/src/Array/Random.h \
+ Eigen/src/Array/Norms.h Eigen/LU Eigen/src/LU/LU.h \
+ Eigen/src/LU/Determinant.h Eigen/src/LU/Inverse.h Eigen/Cholesky \
+ Eigen/src/Cholesky/LLT.h Eigen/src/Cholesky/LDLT.h Eigen/QR \
+ Eigen/src/QR/QR.h Eigen/src/QR/Tridiagonalization.h \
+ Eigen/src/QR/EigenSolver.h Eigen/src/QR/SelfAdjointEigenSolver.h \
+ Eigen/src/QR/HessenbergDecomposition.h Eigen/SVD Eigen/src/SVD/SVD.h \
+ Eigen/Geometry Eigen/src/Geometry/OrthoMethods.h \
+ Eigen/src/Geometry/RotationBase.h Eigen/src/Geometry/Rotation2D.h \
+ Eigen/src/Geometry/Quaternion.h Eigen/src/Geometry/AngleAxis.h \
+ Eigen/src/Geometry/EulerAngles.h Eigen/src/Geometry/Transform.h \
+ Eigen/src/Geometry/Translation.h Eigen/src/Geometry/Scaling.h \
+ Eigen/src/Geometry/Hyperplane.h Eigen/src/Geometry/ParametrizedLine.h \
+ Eigen/src/Geometry/AlignedBox.h Eigen/LeastSquares \
+ Eigen/src/LeastSquares/LeastSquares.h Eigen/Sparse \
+ Eigen/src/Sparse/SparseUtil.h Eigen/src/Sparse/SparseMatrixBase.h \
+ Eigen/src/Sparse/CompressedStorage.h Eigen/src/Sparse/AmbiVector.h \
+ Eigen/src/Sparse/RandomSetter.h Eigen/src/Sparse/SparseBlock.h \
+ Eigen/src/Sparse/SparseMatrix.h Eigen/src/Sparse/DynamicSparseMatrix.h \
+ Eigen/src/Sparse/MappedSparseMatrix.h Eigen/src/Sparse/SparseVector.h \
+ Eigen/src/Sparse/CoreIterators.h Eigen/src/Sparse/SparseTranspose.h \
+ Eigen/src/Sparse/SparseCwise.h Eigen/src/Sparse/SparseCwiseUnaryOp.h \
+ Eigen/src/Sparse/SparseCwiseBinaryOp.h Eigen/src/Sparse/SparseDot.h \
+ Eigen/src/Sparse/SparseAssign.h Eigen/src/Sparse/SparseRedux.h \
+ Eigen/src/Sparse/SparseFuzzy.h Eigen/src/Sparse/SparseFlagged.h \
+ Eigen/src/Sparse/SparseProduct.h \
+ Eigen/src/Sparse/SparseDiagonalProduct.h \
+ Eigen/src/Sparse/TriangularSolver.h Eigen/src/Sparse/SparseLLT.h \
+ Eigen/src/Sparse/SparseLDLT.h Eigen/src/Sparse/SparseLU.h Matter.h \
+ Parameters.h Constants.h Potentials.h PotentialsInterface.h \
+ HelperFunctions.h LowestEigenmodeInterface.h
 Matter.o: Matter.cpp Matter.h Parameters.h Constants.h Potentials.h \
  PotentialsInterface.h Eigen/Eigen Eigen/Dense Eigen/Core \
  Eigen/src/Core/util/DisableMSVCWarnings.h Eigen/src/Core/util/Macros.h \
@@ -1180,63 +1236,6 @@ ProcessSearchJob.o: ProcessSearchJob.cpp ProcessSearchJob.h Matter.h \
  Eigen/src/Sparse/SparseLDLT.h Eigen/src/Sparse/SparseLU.h SaddleSearch.h \
  LowestEigenmodeInterface.h Hessian.h Job.h ConjugateGradients.h \
  MinimizersInterface.h false_boinc.h
-QMBox.o: QMBox.cpp QMBox.h Matter.h Parameters.h Constants.h Potentials.h \
- PotentialsInterface.h Eigen/Eigen Eigen/Dense Eigen/Core \
- Eigen/src/Core/util/DisableMSVCWarnings.h Eigen/src/Core/util/Macros.h \
- Eigen/src/Core/util/Constants.h \
- Eigen/src/Core/util/ForwardDeclarations.h Eigen/src/Core/util/Meta.h \
- Eigen/src/Core/util/XprHelper.h Eigen/src/Core/util/StaticAssert.h \
- Eigen/src/Core/util/Memory.h Eigen/src/Core/NumTraits.h \
- Eigen/src/Core/MathFunctions.h Eigen/src/Core/GenericPacketMath.h \
- Eigen/src/Core/arch/SSE/PacketMath.h Eigen/src/Core/Functors.h \
- Eigen/src/Core/MatrixBase.h Eigen/src/Core/Coeffs.h \
- Eigen/src/Core/Assign.h Eigen/src/Core/MatrixStorage.h \
- Eigen/src/Core/NestByValue.h Eigen/src/Core/Flagged.h \
- Eigen/src/Core/Matrix.h Eigen/src/Core/Cwise.h \
- Eigen/src/Core/CwiseBinaryOp.h Eigen/src/Core/CwiseUnaryOp.h \
- Eigen/src/Core/CwiseNullaryOp.h Eigen/src/Core/Dot.h \
- Eigen/src/Core/Product.h Eigen/src/Core/DiagonalProduct.h \
- Eigen/src/Core/SolveTriangular.h Eigen/src/Core/MapBase.h \
- Eigen/src/Core/Map.h Eigen/src/Core/Block.h Eigen/src/Core/Minor.h \
- Eigen/src/Core/Transpose.h Eigen/src/Core/DiagonalMatrix.h \
- Eigen/src/Core/DiagonalCoeffs.h Eigen/src/Core/Sum.h \
- Eigen/src/Core/Redux.h Eigen/src/Core/Visitor.h Eigen/src/Core/Fuzzy.h \
- Eigen/src/Core/IO.h Eigen/src/Core/Swap.h \
- Eigen/src/Core/CommaInitializer.h Eigen/src/Core/Part.h \
- Eigen/src/Core/CacheFriendlyProduct.h \
- Eigen/src/Core/util/EnableMSVCWarnings.h Eigen/Array \
- Eigen/src/Array/CwiseOperators.h Eigen/src/Array/Functors.h \
- Eigen/src/Array/BooleanRedux.h Eigen/src/Array/Select.h \
- Eigen/src/Array/PartialRedux.h Eigen/src/Array/Random.h \
- Eigen/src/Array/Norms.h Eigen/LU Eigen/src/LU/LU.h \
- Eigen/src/LU/Determinant.h Eigen/src/LU/Inverse.h Eigen/Cholesky \
- Eigen/src/Cholesky/LLT.h Eigen/src/Cholesky/LDLT.h Eigen/QR \
- Eigen/src/QR/QR.h Eigen/src/QR/Tridiagonalization.h \
- Eigen/src/QR/EigenSolver.h Eigen/src/QR/SelfAdjointEigenSolver.h \
- Eigen/src/QR/HessenbergDecomposition.h Eigen/SVD Eigen/src/SVD/SVD.h \
- Eigen/Geometry Eigen/src/Geometry/OrthoMethods.h \
- Eigen/src/Geometry/RotationBase.h Eigen/src/Geometry/Rotation2D.h \
- Eigen/src/Geometry/Quaternion.h Eigen/src/Geometry/AngleAxis.h \
- Eigen/src/Geometry/EulerAngles.h Eigen/src/Geometry/Transform.h \
- Eigen/src/Geometry/Translation.h Eigen/src/Geometry/Scaling.h \
- Eigen/src/Geometry/Hyperplane.h Eigen/src/Geometry/ParametrizedLine.h \
- Eigen/src/Geometry/AlignedBox.h Eigen/LeastSquares \
- Eigen/src/LeastSquares/LeastSquares.h Eigen/Sparse \
- Eigen/src/Sparse/SparseUtil.h Eigen/src/Sparse/SparseMatrixBase.h \
- Eigen/src/Sparse/CompressedStorage.h Eigen/src/Sparse/AmbiVector.h \
- Eigen/src/Sparse/RandomSetter.h Eigen/src/Sparse/SparseBlock.h \
- Eigen/src/Sparse/SparseMatrix.h Eigen/src/Sparse/DynamicSparseMatrix.h \
- Eigen/src/Sparse/MappedSparseMatrix.h Eigen/src/Sparse/SparseVector.h \
- Eigen/src/Sparse/CoreIterators.h Eigen/src/Sparse/SparseTranspose.h \
- Eigen/src/Sparse/SparseCwise.h Eigen/src/Sparse/SparseCwiseUnaryOp.h \
- Eigen/src/Sparse/SparseCwiseBinaryOp.h Eigen/src/Sparse/SparseDot.h \
- Eigen/src/Sparse/SparseAssign.h Eigen/src/Sparse/SparseRedux.h \
- Eigen/src/Sparse/SparseFuzzy.h Eigen/src/Sparse/SparseFlagged.h \
- Eigen/src/Sparse/SparseProduct.h \
- Eigen/src/Sparse/SparseDiagonalProduct.h \
- Eigen/src/Sparse/TriangularSolver.h Eigen/src/Sparse/SparseLLT.h \
- Eigen/src/Sparse/SparseLDLT.h Eigen/src/Sparse/SparseLU.h Quickmin.h \
- MinimizersInterface.h HelperFunctions.h
 Quickmin.o: Quickmin.cpp Quickmin.h MinimizersInterface.h Matter.h \
  Parameters.h Constants.h Potentials.h PotentialsInterface.h Eigen/Eigen \
  Eigen/Dense Eigen/Core Eigen/src/Core/util/DisableMSVCWarnings.h \
@@ -1351,7 +1350,7 @@ SaddleSearch.o: SaddleSearch.cpp SaddleSearch.h Matter.h Parameters.h \
  Eigen/src/Sparse/TriangularSolver.h Eigen/src/Sparse/SparseLLT.h \
  Eigen/src/Sparse/SparseLDLT.h Eigen/src/Sparse/SparseLU.h \
  LowestEigenmodeInterface.h ConjugateGradients.h MinimizersInterface.h \
- HelperFunctions.h Dimer.h debug.h EpiCenters.h
+ HelperFunctions.h Dimer.h debug.h ImprovedDimer.h EpiCenters.h
 SaddleSearchJob.o: SaddleSearchJob.cpp SaddleSearchJob.h Matter.h \
  Parameters.h Constants.h Potentials.h PotentialsInterface.h Eigen/Eigen \
  Eigen/Dense Eigen/Core Eigen/src/Core/util/DisableMSVCWarnings.h \
