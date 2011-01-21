b849af8717e64033e03b1d4c5fa813417b007b93
graeme <graeme@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-01-21 17:03:39 +0000

    added lanczos files
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@881 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ client/Lanczos.h
@@ -0,0 +1,61 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//-----------------------------------------------------------------------------------
+
+#ifndef LANCZOS_H
+#define LANCZOS_H
+
+#include <math.h>
+#include <cmath>
+#include <cassert>
+#include "debug.h"
+
+#include "Eigen/Eigen"
+USING_PART_OF_NAMESPACE_EIGEN
+
+#include "Matter.h"
+#include "HelperFunctions.h"
+#include "Parameters.h"
+#include "LowestEigenmodeInterface.h"
+
+// dimer method to find the lowest curvature mode
+class Lanczos : public LowestEigenmodeInterface
+{
+
+    public:
+
+    Lanczos(Matter const *matter, Parameters *parameters);
+    ~Lanczos();
+
+    void initialize(Matter const *matter, Matrix<double, Eigen::Dynamic, 3>);
+    void compute(Matter const *matter); 
+    double getEigenvalue();
+    void setEigenvector(Matrix<double, Eigen::Dynamic, 3> const eigenvector);
+    Matrix<double, Eigen::Dynamic, 3>  getEigenvector();
+
+    Parameters *parameters;
+
+    Matter *x0;                                 // Center Image
+    Matter *x1;                                 // Forward image.
+    Matrix<double, Eigen::Dynamic, 3> tau;      // Lanczos direction.
+    double C_tau;                               // Curvature along tau.
+
+    private:
+
+    Matrix<double, Eigen::Dynamic, Eigen::Dynamic> fullMatrix;
+    Matrix<double, Eigen::Dynamic, 3> w, qq, qqold, z;
+    Matrix<double, Eigen::Dynamic, 3> *PP;
+    VectorXf d, e, aa, bb;
+
+};
+
+#endif
+
+
+
