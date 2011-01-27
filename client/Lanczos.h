5750c9ccc9a5bba2dc6ec9452c5bf7877ba6850d
Sam Chill <SamChill@users.noreply.github.com>
2011-01-27 17:07:58 +0000

    Initial commit of new Lanczos. It doesn't quite work but it's all hooked up to the build process at least.

--- client/Lanczos.h
+++ client/Lanczos.h
@@ -17,7 +17,6 @@
 #include "debug.h"
 
 #include "Eigen.h"
-
 #include "Matter.h"
 #include "HelperFunctions.h"
 #include "Parameters.h"
@@ -32,26 +31,19 @@ class Lanczos : public LowestEigenmodeInterface
     Lanczos(Matter const *matter, Parameters *parameters);
     ~Lanczos();
 
-    void initialize(Matter const *matter, Matrix<double, Eigen::Dynamic, 3>);
+    void initialize(Matter const *matter, AtomMatrix displacement);
     void compute(Matter const *matter); 
+
     double getEigenvalue();
-    void setEigenvector(Matrix<double, Eigen::Dynamic, 3> const eigenvector);
-    Matrix<double, Eigen::Dynamic, 3>  getEigenvector();
+    AtomMatrix getEigenvector();
+    void setEigenvector(AtomMatrix const eigenvector);
 
     Parameters *parameters;
 
-    Matter *x0;                                 // Center Image
-    Matter *x1;                                 // Forward image.
-    Matrix<double, Eigen::Dynamic, 3> tau;      // Lanczos direction.
-    double C_tau;                               // Curvature along tau.
-
     private:
-
-    Matrix<double, Eigen::Dynamic, Eigen::Dynamic> fullMatrix;
-    Matrix<double, Eigen::Dynamic, 3> w, qq, qqold, z;
-    Matrix<double, Eigen::Dynamic, 3> *PP;
-    VectorXf d, e, aa, bb;
-
+    AtomMatrix lowestEv;
+    double lowestEw;
+    VectorXd r;
 };
 
 #endif
