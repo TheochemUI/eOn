965297db3e19c5482632be2685ab1e780e960170
ryeterrell <ryeterrell@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-01-29 03:18:47 +0000

    Moved MinimizersInterface.h to Minimizer.h
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@892 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ client/Minimizer.h
@@ -0,0 +1,28 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//
+//-----------------------------------------------------------------------------------
+
+#ifndef MINIMIZER_H
+#define MINIMIZER_H
+
+class Minimizer
+{
+    
+public:
+    virtual ~Minimizer(){};
+    void virtual setOutput(int level) = 0;
+    void virtual oneStep() = 0;
+    void virtual fullRelax() = 0;
+    bool virtual isItConverged(double convergeCriterion) = 0; 
+    long totalForceCalls;
+
+};
+
+#endif
