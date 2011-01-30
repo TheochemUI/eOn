fc96d8f60f80a5cf0b6ca85a86471476082f4cef
ryeterrell <ryeterrell@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-01-30 21:44:20 +0000

    Fixed minimization job to use quickmin or cg.
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@903 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/Quickmin.h
+++ client/Quickmin.h
@@ -13,39 +13,34 @@
 
 #include "Minimizer.h"
 #include "Matter.h"
-//#include "HelperFunctions.h"
-//#include "Constants.h"
-//#include "Parameters.h"
 
-/** Functionality relying on the conjugate gradients algorithm. The object is capable of minimizing an Matter object or modified forces being passed in.*/
-class Quickmin : public Minimizer{
+class Quickmin : public Minimizer
+{
 
-public:
-    /** Constructor to be used when a structure is minimized.
-    @param[in]   *matter        Pointer to the Matter object to be relaxed.
-    @param[in]   *parameters    Pointer to the Parameter object containing the runtime parameters.*/
-    Quickmin(Matter *matter, Parameters *parameters);
+    public:
+        Quickmin(Matter *matter, Parameters *parameters);
 
-    ~Quickmin();///< Destructor.
+        ~Quickmin();
 
-    void oneStep();///< Do one iteration.
-    void oneStepPart1(Matrix<double, Eigen::Dynamic, 3> forces);
-    void oneStepPart2(Matrix<double, Eigen::Dynamic, 3> forces);
-    
-    void fullRelax();///< Relax the Matter object corresponding to the pointer that was passed with the constructor.
-    bool isItConverged(double convergeCriterion);///< Determine if the norm of the force vector is bellow the \a convergeCriterion.
-    void setOutput(int level);
-    
-private:
-    long nAtoms;///< Number of free coordinates.
+        void oneStep();
+        void oneStepPart1(Matrix<double, Eigen::Dynamic, 3> forces);
+        void oneStepPart2(Matrix<double, Eigen::Dynamic, 3> forces);
+        
+        void fullRelax();
+        bool isItConverged(double convergeCriterion);
+        void setOutput(int level);
+        
+    private:
+        long nAtoms;
+        int outputLevel;
+        Matter *matter;
+        Parameters *parameters;
 
-    Matter *matter;///< Pointer to atom object \b outside the scope of the class.    
-    Parameters *parameters;///< Pointer to a structure outside the scope of the class containing runtime parameters. 
+        Matrix<double, Eigen::Dynamic, 3> forces;
+        double dtScale;
 
-    Matrix<double, Eigen::Dynamic, 3> forces;///< Double array, its size equals the number of \b free atoms times 3.
-    double dtScale;
-
-    Matrix<double, Eigen::Dynamic, 3> velocity; 
+        Matrix<double, Eigen::Dynamic, 3> velocity; 
 };
 
 #endif
+
