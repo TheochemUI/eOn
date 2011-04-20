b48f3d62ebbeab7ac77d0949e6e7a1e217fe7e2c
matt <matt@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-04-20 21:43:45 +0000

    Diagonal moves + corrected energy
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1053 9d962951-508e-48a8-94b5-974f9cfda47c

--- tools/toykmc/state.py
+++ tools/toykmc/state.py
@@ -6,13 +6,14 @@ from math import exp
 dEsaddle = .1
 
 #these are the directions that atoms are allowed to move
-move_neighbors = [(1,0), (-1,0), (0,1), (0,-1)]
+move_neighbors = [(1,0), (-1,0), (0,1), (0,-1),
+        (1,1), (-1,-1), (1,-1), (-1,1)]
 
 #these are the first nearest neighbors for energy purposes
 energy_neighbors = [(1,0), (-1,0), (0,1), (0,-1)]
 
 #these are the second nearest neighbors for energy purposes
-energy_neighbors_2 = [(1,1), (-1,1), (-1,1), (-1,-1)]
+energy_neighbors_2 = [(1,1), (1,-1), (-1,1), (-1,-1)]
 energy_2 = 0.5 #relative strength of 2NN bonds
 
 class State:
