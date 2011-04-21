ddb6727e8d3251ce667e57546d82a104c4b3acca
Matthew Welborn <mattwelborn@users.noreply.github.com>
2011-04-21 17:42:48 +0000

    xfer

--- tools/toykmc/kmc.py
+++ tools/toykmc/kmc.py
@@ -4,15 +4,19 @@ from state import State
 
 def kmc(s):
     ratesum = 0.0
-    for proc in s.get_rate_table():
-        ratesum += proc['rate']
-
+    
+    if s in superbasin:
+        rate_table, dt = #superbasin that s is in /step() 
+    else:
+        rate_table = s.get_rate_table()
+        for proc in rate_table:
+            ratesum += proc['rate']
+        dt = -log(random.random())/ratesum
     u = random.random()
     p = 0.0
-    for proc in s.get_rate_table():
+    for proc in rate_table:
         p += proc['rate']/ratesum
         if p>u:
-            dt = -log(random.random())/ratesum
             return State.get_state(proc['product'].grid), dt
             break
     else:
