120eaadcbd38ea1632d7afee4d4b6682e48ab1d5
Andreas Pedersen <andped10@users.noreply.github.com>
2011-05-18 13:54:00 +0000

    enabled usage of spce water potential

--- client/potentials/platinum-water/zhu_philpott_for_eon.cpp
+++ client/potentials/platinum-water/zhu_philpott_for_eon.cpp
@@ -30,3 +30,18 @@ void Tip4p::force(long N, const double *R, const int *atomicNrs, double *F, doub
     diagbox[2]=box[8];
     computeHH_O_(N, R, F, *U, diagbox, 0);
 }
+
+
+SpceCcl::SpceCcl() :
+Potential(),
+forcefields::SpceCcl(8.5, 1.0)
+{}
+
+void SpceCcl::force(long N, const double *R, const int *atomicNrs, double *F, double *U, const double *box)
+{
+    double diagbox[3];
+    diagbox[0]=box[0];
+    diagbox[1]=box[4];
+    diagbox[2]=box[8];
+    computeHH_O_(N, R, F, *U, diagbox, 0);
+}
