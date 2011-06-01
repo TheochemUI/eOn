c1c2d7140ed9b7c6897dddcd2b57469d654992da
Graeme Henkelman <iz1ng@users.noreply.github.com>
2011-06-01 22:29:48 +0000

    neb job progress

--- client/NudgedElasticBandJob.cpp
+++ client/NudgedElasticBandJob.cpp
@@ -19,122 +19,101 @@
 
 using namespace std;
 
-SaddleSearchJob::SaddleSearchJob(Parameters *params)
+NudgedElasticBandJob::NudgedElasticBandJob(Parameters *parametersPassed)
 {
-    parameters = params;
-    fCallsSaddle = 0;
+    parameters = parametersPassed;
+    fCallsNEB = 0;
 }
 
-SaddleSearchJob::~SaddleSearchJob()
+NudgedElasticBandJob::~NudgedElasticBandJob()
 {}
 
-std::vector<std::string> SaddleSearchJob::run(void)
+std::vector<std::string> NudgedElasticBandJob::run(void)
 {
     string reactant_passed("reactant_passed.con");
-    string displacement_passed("displacement_passed.con");
-    string mode_passed("mode_passed.dat");
+    string product_passed("product_passed.con");
 
     initial = new Matter(parameters);
-    displacement = new Matter(parameters);
-    saddle = new Matter(parameters);
+    final = new Matter(parameters);
 
     initial->con2matter(reactant_passed);
+    final->con2matter(product_passed);
 
-    if (parameters->saddleDisplaceType == SaddlePoint::DISP_LOAD) {
-        // displacement was passed from the server        
-        saddle->con2matter(displacement_passed);
-    }
-    else {
-        // displacement and mode will be made on the client
-        // in saddlePoint->initialize(...) 
-        *saddle = *initial;
-    }
-    saddlePoint = new SaddlePoint();
-    saddlePoint->initialize(initial, saddle, parameters);
-    if (parameters->saddleDisplaceType == SaddlePoint::DISP_LOAD) {
-        // mode was passed from the server        
-        saddlePoint->loadMode(mode_passed);
-    }
+    neb = new NudgedElasticBand(initial, final, parameters);
 
-    int status = doSaddleSearch();
+    int status = findMinimumEnergyPath();
 
     printEndState(status);
     saveData(status);
 
-    delete saddlePoint;
+    delete neb;
     delete initial;
-    delete displacement;
-    delete saddle; 
+    delete final;
 
     return returnFiles;
 }
 
-int SaddleSearchJob::doSaddleSearch()
+int NudgedElasticBandJob::findMinimumEnergyPath()
 {
     Matter matterTemp(parameters);
     long status;
     int f1;
     f1 = Potential::fcalls;
-    status = saddlePoint->locate();
-    fCallsSaddle += Potential::fcalls - f1;
+    status = neb->compute();
+    fCallsNEB += Potential::fcalls - f1;
 
-    if (status == SaddlePoint::STATUS_INIT) {
-        status = SaddlePoint::STATUS_GOOD;
+    if (status == NudgedElasticBand::STATUS_INIT) {
+        status = NudgedElasticBand::STATUS_GOOD;
     }
 
     return status;
 }
 
-void SaddleSearchJob::saveData(int status){
-    FILE *fileResults, *fileSaddle, *fileMode;
+void NudgedElasticBandJob::saveData(int status){
+    FILE *fileResults, *fileNEB, *fileMode;
 
     std::string resultsFilename("results.dat");
     returnFiles.push_back(resultsFilename);
     fileResults = fopen(resultsFilename.c_str(), "wb");
     ///XXX: min_fcalls isn't quite right it should get them from
     //      the minimizer. But right now the minimizers are in
-    //      the SaddlePoint object. They will be taken out eventually.
+    //      the NudgedElasticBand object. They will be taken out eventually.
     
     fprintf(fileResults, "%d termination_reason\n", status);
-    fprintf(fileResults, "%ld random_seed\n", parameters->randomSeed);
-    fprintf(fileResults, "%s potential_tyep\n", parameters->potential.c_str());
+    fprintf(fileResults, "%s potential_type\n", parameters->potential.c_str());
     fprintf(fileResults, "%d total_force_calls\n", Potential::fcalls);
-    fprintf(fileResults, "%d force_calls_saddle\n", fCallsSaddle);
-    fprintf(fileResults, "%f potential_energy_saddle\n", saddle->getPotentialEnergy());
+    fprintf(fileResults, "%d force_calls_neb\n", fCallsNEB);
+//    fprintf(fileResults, "%f potential_energy_saddle\n", neb->getPotentialEnergy());
     fclose(fileResults);
 
-    std::string modeFilename("mode.dat");
-    returnFiles.push_back(modeFilename);
-    fileMode = fopen(modeFilename.c_str(), "wb");
-    saddlePoint->saveMode(fileMode);
-    fclose(fileMode);
-
+/*
     std::string saddleFilename("saddle.con");
     returnFiles.push_back(saddleFilename);
     fileSaddle = fopen(saddleFilename.c_str(), "wb");
     saddle->matter2con(fileSaddle);
     fclose(fileSaddle);
+*/
 }
 
-void SaddleSearchJob::printEndState(int status) {
-    fprintf(stdout, "Final state: ");
-    if(status == SaddlePoint::STATUS_GOOD)
+void NudgedElasticBandJob::printEndState(int status) {
+/*    fprintf(stdout, "Final state: ");
+    if(status == NudgedElasticBand::STATUS_GOOD)
         fprintf(stdout, "Successful.\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_NO_CONVEX)
+    else if(status == NudgedElasticBand::STATUS_BAD_NO_CONVEX)
         fprintf(stdout, "Initial displacement, not able to reach convex region.\n");
    
-    else if(status == SaddlePoint::STATUS_BAD_HIGH_ENERGY)
+    else if(status == NudgedElasticBand::STATUS_BAD_HIGH_ENERGY)
         fprintf(stdout, "Saddle search, barrier too high.\n");
         
-    else if(status == SaddlePoint::STATUS_BAD_MAX_CONCAVE_ITERATIONS) 
+    else if(status == NudgedElasticBand::STATUS_BAD_MAX_CONCAVE_ITERATIONS) 
         fprintf(stdout, "Saddle search, too many iterations in concave region.\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_MAX_ITERATIONS)
+    else if(status == NudgedElasticBand::STATUS_BAD_MAX_ITERATIONS)
         fprintf(stdout, "Saddle search, too many iterations in saddle point search.\n");
     else
         fprintf(stdout, "Unknown status: %i!\n", status);
-
+*/
     return;
 }
 
