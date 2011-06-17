0ae722917a3020c07b54d7a984a2017af790d607
itj3 <itj3@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-06-17 15:49:41 +0000

    akmcgui and automated config update
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1126 9d962951-508e-48a8-94b5-974f9cfda47c

--- tools/akmcgui.py
+++ tools/akmcgui.py
@@ -29,119 +29,163 @@ class akmcgui(atomview.atomview):
         self.stateRB = gladetree.get_widget("stateRB")
         self.processesRB = gladetree.get_widget("processesRB")
         self.stateScale = gladetree.get_widget("stateScale")
-        self.processesScale = gladetree.get_widget("processesScale")
+        self.processesSB = gladetree.get_widget("processesSB")
         self.interpolationCB = gladetree.get_widget("interpulationCB")
         self.interpolationSB = gladetree.get_widget("interpolationSB")
+        self.stateEnergy = gladetree.get_widget("stateEnergy")
+        self.statePlayTB = gladetree.get_widget("statePlayTB")
+        self.state_fpsSB = gladetree.get_widget("state_fpsSB")
         #defaulting options
-        self.processesScale.set_sensitive(False)
+        self.processesSB.set_sensitive(False)
         self.interpolationCB.set_sensitive(False)
         self.interpolationSB.set_sensitive(False)
-        self.state_scale_changed()
-        interpolation = []
-        #event handling
+        self.changeImage()
+        self.energy_changed()
+        self.state_fpsSB.set_value(1)
+        #event handeling
+        self.stateRB.connect("clicked", self.changeImage)
+        self.processesRB.connect("clicked", self.changeImage)
+        self.interpolationCB.connect("toggled", self.changeImage)
+        self.interpolationSB.connect("value-changed", self.changeImage)
+        self.stateScale.connect("value-changed", self.changeImage)
+        self.processesSB.connect("value-changed", self.changeImage)
         self.stateRB.connect("clicked", self.RB_changed)
         self.processesRB.connect("clicked", self.RB_changed)
-        self.processesRB.connect("clicked", self.interpolation) 
-        self.processesRB.connect("clicked", self.processes_scale_changed)
-        self.interpolationCB.connect("toggled", self.interpolation)
-        self.interpolationSB.connect("value-changed", self.interpolation)
-        self.interpolationCB.connect("toggled", self.interpolationCB_changed)
+        self.interpolationCB.connect("clicked", self.interpolationCB_changed)
+        self.stateScale.connect("value-changed", self.energy_changed)
+        self.statePlayTB.connect("toggled", self.state_play)
+        self.state_fpsSB.connect("value-changed", self.state_play)
         
-#
-# StateScale--------------------------------------------------        
-#
-        states = glob.glob("./states/*")
-        
-        i = 0
-        while ("./states/%d" %i) in states:
-            i+=1    
-        numStates = i-1
-        
-        self.stateScale.set_range(0, numStates)       
-        self.stateScale.connect("value-changed", self.state_scale_changed)
-
-#
-# ProcessesScale---------------------------------------------- 
-#
-        processes = glob.glob("./states/%d/procdata/*" % self.stateScale.get_value())
-        
-        j = 0
-        while ("./states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), j)) in processes:
-            j+=1
-        numProcesses = j-1
-        
-        self.processesScale.set_range(0, numProcesses)
-        self.processesScale.connect("value-changed", self.processes_scale_changed)
-        self.processesScale.connect("value-changed", self.interpolation)
+        self.playImage = gtk.Image()
+        self.playImage.set_from_stock(gtk.STOCK_MEDIA_PLAY, gtk.ICON_SIZE_BUTTON)
+        self.pauseImage = gtk.Image()
+        self.pauseImage.set_from_stock(gtk.STOCK_MEDIA_PAUSE, gtk.ICON_SIZE_BUTTON)
+        self.statePlayTB.set_image(self.playImage)
         
 
-#     
-# Events------------------------------------------------------        
 #
-    
+# Events-----------------------------------------------------
+#
+
+    def changeImage(self, *args):
+        if self.stateRB.get_active() == True:
+            states = glob.glob("./states/*")
+            i = 0
+            while ("./states/%d" %i) in states:
+                i+=1    
+            numStates = i-1
+            self.stateScale.set_range(0, numStates)
+        
+            datapass = io.loadcons("states/%d/reactant.con" % self.stateScale.get_value())
+        else: 
+            if self.interpolationCB.get_active() == False:
+                processes = glob.glob("./states/%d/procdata/*" % self.stateScale.get_value())
+                j = 0
+                while ("./states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), j)) in processes:
+                    j+=1
+                numProcesses = j-1
+                if numProcesses == 0:
+                    self.processesSB.set_value(0)
+                    self.processesSB.set_sensitive(False)
+                else:
+                    self.processesSB.set_sensitive(True)            
+                    self.processesSB.set_range(0, numProcesses)
+                
+                
+                saddle = io.loadcon("states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
+                reactant = io.loadcon("states/%d/procdata/reactant_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
+                product = io.loadcon("states/%d/procdata/product_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
+                datapass = [reactant, saddle, product]
+            else:
+                processes = glob.glob("./states/%d/procdata/*" % self.stateScale.get_value())
+                j = 0
+                while ("./states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), j)) in processes:
+                    j+=1
+                numProcesses = j-1        
+                if numProcesses == 0:
+                    self.processesSB.set_value(0)
+                    self.processesSB.set_sensitive(False)
+                else:
+                    self.processesSB.set_sensitive(True)            
+                    self.processesSB.set_range(0, numProcesses)
+            
+                saddle = io.loadcon("states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
+                reactant = io.loadcon("states/%d/procdata/reactant_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
+                product = io.loadcon("states/%d/procdata/product_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
+                datapass = [reactant, saddle, product]
+                N = int (self.interpolationSB.get_text())
+                p = [reactant, saddle, product]
+                q = []
+                for k in range(len(p) -1):
+                    v = atoms.pbc(p[k+1].r - p[k].r, p[k].box)
+                    d = np.linalg.norm(v)
+                    v /=d
+                    q.append(p[k].copy())
+                    for l in range(1,N):
+                        temp = p[k].copy()
+                        temp.r += l * (v * (d/(N)))
+                        q.append(temp)
+                q.append(p[2].copy())
+                datapass = q
+        self.data_set(datapass)
+        
     def RB_changed(self, widget, data=None):
         if self.stateRB.get_active() == True:
-            self.processesScale.set_value(0)
-            self.processesScale.set_sensitive(False)
+            self.processesSB.set_value(0)
+            self.processesSB.set_sensitive(False)
             self.interpolationCB.set_sensitive(False)
             self.interpolationSB.set_sensitive(False)
-            self.interpolationCB.set_active(False)
-            state = io.loadcons("states/%d/reactant.con" % self.stateScale.get_value())
-            self.data_set(state)
+            self.statePlayTB.set_sensitive(True)
+            self.state_fpsSB.set_sensitive(True)
             return True
         else:
-            self.processesScale.set_sensitive(True)
+            self.processesSB.set_sensitive(True)
             self.interpolationCB.set_sensitive(True)
+            self.statePlayTB.set_sensitive(False)
+            self.state_fpsSB.set_sensitive(False)
+            self.statePlayTB.set_active(False)
+            if self.interpolationCB.get_active() == True:
+                self.interpolationSB.set_sensitive(True)
             return True
             
-     
-    def state_scale_changed(self, *args):
-        state = io.loadcons("states/%d/reactant.con" % self.stateScale.get_value())
-        self.data_set(state)
-        return True
-        
-    def processes_scale_changed(self, *args):
-        saddle = io.loadcon("states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), self.processesScale.get_value()))
-        reactant = io.loadcon("states/%d/procdata/reactant_%d.con" %(self.stateScale.get_value(), self.processesScale.get_value()))
-        product = io.loadcon("states/%d/procdata/product_%d.con" %(self.stateScale.get_value(), self.processesScale.get_value()))
-        movie = [reactant, saddle, product]
-        self.data_set(movie)
-        return True
-        
     def interpolationCB_changed(self, widget, data=None):
         if self.interpolationCB.get_active() == True:
             self.interpolationSB.set_sensitive(True)
         else:  
             self.interpolationSB.set_sensitive(False)
         
-    def interpolation(self, widget, data=None):
-        saddle = io.loadcon("states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), self.processesScale.get_value()))
-        reactant = io.loadcon("states/%d/procdata/reactant_%d.con" %(self.stateScale.get_value(), self.processesScale.get_value()))
-        product = io.loadcon("states/%d/procdata/product_%d.con" %(self.stateScale.get_value(), self.processesScale.get_value()))
-        movie = [reactant, saddle, product]
-        if self.interpolationCB.get_active() == True:
-            N = int (self.interpolationSB.get_text())
-            p = [reactant, saddle, product]
-            q = []
-            c = 1
-            for k in range(len(p) -1):
-                v = atoms.pbc(p[k+1].r - p[k].r, p[k].box)
-                d = np.linalg.norm(v)
-                v /= d
-                q.append(p[k].copy())
-                for l in range(1,N):
-                    temp = p[k].copy()
-                    temp.r += l * (v * (d/(N)))
-                    q.append(temp)
-            q.append(p[2].copy())
-            self.data_set(q)
-        else: 
-            self.data_set(movie)              
-    
+    def energy_changed(self, *args):
+        energy = open("states/state_table", "r")
+        energyNumber = energy.readlines()
+        a = energyNumber[int (self.stateScale.get_value())].split()[1]
+        self.stateEnergy.set_markup("%f<b>eV</b>" % float (a))
         
- #       
- # Main-------------------------------------------------------       
- #   
+    def state_play(self, *args): 
+        try:
+            gobject.source_remove(self.timer_id)
+        except:
+            pass
+            
+        if self.statePlayTB.get_active() == False:
+            self.statePlayTB.set_image(self.playImage)
+        else:
+            self.statePlayTB.set_image(self.pauseImage)
+            states = glob.glob("./states/*")
+            i = 0
+            while ("./states/%d" %i) in states:
+                i+=1    
+            numStates = i-1     
+            def loop():    
+                self.stateScale.set_value(int (self.stateScale.get_value())+1)
+                if self.stateScale.get_value() >= numStates:
+                    self.stateScale.set_value(0)
+                return True 
+            self.timer_id = gobject.timeout_add(1000/(int (self.state_fpsSB.get_value())), loop)
+            
+
+#       
+# Main-------------------------------------------------------       
+#   
 
      
 if __name__ == "__main__":
@@ -158,7 +202,4 @@ if __name__ == "__main__":
             data = io.loadcons(sys.argv[1])
         q.data_set(data)
     gtk.main()
-
         
-        
-
