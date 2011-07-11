9f190573cd5723c1e45381f51ccf99d74221c123
itj3 <itj3@NODATA>
2011-07-11 20:19:32 +0000

    automated docs ftw

--- tools/akmcgui.py
+++ tools/akmcgui.py
@@ -33,60 +33,52 @@ class akmcgui(atomview.atomview):
         gladetree = gtk.glade.XML(os.path.join(pathfix.path, "tools/akmcgui.glade"))
         gui = gladetree.get_widget("akmcgui")
         atomview.atomview.__init__(self, gui)
-        self.stateRB = gladetree.get_widget("stateRB")
-        self.processesRB = gladetree.get_widget("processesRB")
         self.stateScale = gladetree.get_widget("stateScale")
         self.processesSB = gladetree.get_widget("processesSB")
         self.interpolationCB = gladetree.get_widget("interpulationCB")
         self.interpolationSB = gladetree.get_widget("interpolationSB")
         self.stateEnergy = gladetree.get_widget("stateEnergy")
         self.statePlayTB = gladetree.get_widget("statePlayTB")
-        self.state_fpsSB = gladetree.get_widget("state_fpsSB")
         self.plotWindow = gladetree.get_widget("plotWindow")
         self.energy_plotButton = gladetree.get_widget("energy_plotButton")
-        self.tempLabel = gladetree.get_widget("tempLabel")
-        self.rateLabel = gladetree.get_widget("rateLabel")
-        self.barrierLabel = gladetree.get_widget("barrierLabel")
-        self.prefactorLabel = gladetree.get_widget("prefactorLabel")
+        self.hbox = gladetree.get_widget("hbox5")
+        self.tempLabel = gladetree.get_widget("simtemp")
+        self.fileButton = gladetree.get_widget("fileButton")
+        self.fileEntry = gladetree.get_widget("locationEntry")
         # defaults
-        self.config = ConfigParser.SafeConfigParser()
-        self.config.read(os.path.join(pathfix.path, "default_config.ini"))
-        try:
-            self.config.read("./config.ini")
-        except:
-            print "No config.ini found in local directory, using default values."
-        self.processesSB.set_sensitive(False)
-        self.interpolationCB.set_sensitive(False)
+        button_size = (20, 20)
+        self.open = gtk.Image()
+        self.open.set_from_stock(gtk.STOCK_FILE, gtk.ICON_SIZE_BUTTON)
+        self.fileButton.set_image(self.open)
+        self.stateScale.set_size_request(400,-1)
         self.interpolationSB.set_sensitive(False)
-        self.changeImage()
-        self.energy_changed()
-        self.state_fpsSB.set_value(1)
         self.playImage = gtk.Image()
         self.playImage.set_from_stock(gtk.STOCK_MEDIA_PLAY, gtk.ICON_SIZE_BUTTON)
         self.pauseImage = gtk.Image()
         self.pauseImage.set_from_stock(gtk.STOCK_MEDIA_PAUSE, gtk.ICON_SIZE_BUTTON)
         self.statePlayTB.set_image(self.playImage)
-        self.tempLabel.set_text(self.config.get("Main", "temperature"))
-        self.processtable()
         self.plotImage = gtk.Image()
-        self.plotImage.set_from_file("plot_icon.png")
+        image = gdk.pixbuf_new_from_file_at_size("../../tools/plot_icon.png", 15, 15)
+        self.plotImage.set_from_pixbuf(image)
         self.energy_plotButton.set_image(self.plotImage)
+        self.directory = "./config.ini"
+        self.fileEntry.set_text(self.directory)
+        self.fileButton.set_sensitive(False)
+        self.fileEntry.set_sensitive(False)
+        self.startup()
         # event handeling
-        self.stateRB.connect("clicked", self.changeImage)
-        self.processesRB.connect("clicked", self.changeImage)
         self.interpolationCB.connect("toggled", self.changeImage)
         self.interpolationSB.connect("value-changed", self.changeImage)
         self.stateScale.connect("value-changed", self.changeImage)
         self.processesSB.connect("value-changed", self.changeImage)
-        self.stateRB.connect("clicked", self.RB_changed)
-        self.processesRB.connect("clicked", self.RB_changed)
         self.interpolationCB.connect("clicked", self.interpolationCB_changed)
         self.stateScale.connect("value-changed", self.energy_changed)
+        self.stateScale.connect("value-changed", self.state_play)
         self.statePlayTB.connect("toggled", self.state_play)
         self.stateScale.connect("value-changed", self.processtable)
-        self.state_fpsSB.connect("value-changed", self.state_play)
         self.energy_plotButton.connect("clicked", self.energy_plot)
-        self.processesSB.connect("value-changed", self.processtable)
+        self.stateScale.connect("value-changed", self.state_changed)
+        self.fileButton.connect("clicked", self.filechanged)
         
         
         
@@ -95,84 +87,91 @@ class akmcgui(atomview.atomview):
 # Events-----------------------------------------------------
 #
 
+    
+    def startup(self, *args):
+        reload(config)
+        config.init(self.directory)
+        config.path_root = os.path.dirname(self.directory)
+        #changing path_states could cause errors
+        config.path_states = "%s/states/" % config.path_root
+        self.tempLabel.set_text(str(config.main_temperature))
+        self.changeImage()
+        self.energy_changed()
+        self.processtable()
+        
+        
+
     def changeImage(self, *args):
-        if self.stateRB.get_active() == True:
-            states = glob.glob("./states/*")
+        if self.interpolationCB.get_active() == False:
+            states = glob.glob("%s*" %config.path_states)
             i = 0
-            while ("./states/%d" %i) in states:
+            while ("%s%d" %(config.path_states,i)) in states:
                 i+=1    
             numStates = i-1
             self.stateScale.set_range(0, numStates)
-            datapass = io.loadcons("states/%d/reactant.con" % self.stateScale.get_value())
-        else: 
-            if self.interpolationCB.get_active() == False:
-                processes = glob.glob("./states/%d/procdata/*" % self.stateScale.get_value())
-                j = 0
-                while ("./states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), j)) in processes:
-                    j+=1
-                numProcesses = j-1
-                if numProcesses == 0:
-                    self.processesSB.set_value(0)
-                    self.processesSB.set_sensitive(False)
-                else:
-                    self.processesSB.set_sensitive(True)            
-                    self.processesSB.set_range(0, numProcesses)
-                saddle = io.loadcon("states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
-                reactant = io.loadcon("states/%d/procdata/reactant_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
-                product = io.loadcon("states/%d/procdata/product_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
-                datapass = [reactant, saddle, product]
+            processes = glob.glob("%s%d/procdata/*" %(config.path_states, self.stateScale.get_value()))
+            j = 0
+            while ("%s%d/procdata/saddle_%d.con" %(config.path_states,self.stateScale.get_value(), j)) in processes:
+                j+=1
+            numProcesses = j-1
+            if numProcesses == 0:
+                self.processesSB.set_value(0)
+                self.processesSB.set_sensitive(False)
             else:
-                processes = glob.glob("./states/%d/procdata/*" % self.stateScale.get_value())
-                j = 0
-                while ("./states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), j)) in processes:
-                    j+=1
-                numProcesses = j-1        
-                if numProcesses == 0:
-                    self.processesSB.set_value(0)
-                    self.processesSB.set_sensitive(False)
-                else:
-                    self.processesSB.set_sensitive(True)            
-                    self.processesSB.set_range(0, numProcesses)
-                saddle = io.loadcon("states/%d/procdata/saddle_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
-                reactant = io.loadcon("states/%d/procdata/reactant_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
-                product = io.loadcon("states/%d/procdata/product_%d.con" %(self.stateScale.get_value(), self.processesSB.get_value()))
-                datapass = [reactant, saddle, product]
-                N = int (self.interpolationSB.get_text())+1
-                p = [reactant, saddle, product]
-                q = []
-                for k in range(len(p)-1):
-                    v = atoms.pbc(p[k+1].r - p[k].r, p[k].box)
-                    d = np.linalg.norm(v)
-                    v /=d
-                    q.append(p[k].copy())
-                    for l in range(1,N):
-                        temp = p[k].copy()
-                        temp.r += l * (v * (d/(N)))
-                        q.append(temp)
-                q.append(p[2].copy())
-                datapass = q
+                self.processesSB.set_sensitive(True)            
+                self.processesSB.set_range(0, numProcesses)
+            saddle = io.loadcon("%s%d/procdata/saddle_%d.con" %(config.path_states,self.stateScale.get_value(), self.processesSB.get_value()))
+            reactant = io.loadcon("%s%d/procdata/reactant_%d.con" %(config.path_states,self.stateScale.get_value(), self.processesSB.get_value()))
+            product = io.loadcon("%s%d/procdata/product_%d.con" %(config.path_states,self.stateScale.get_value(), self.processesSB.get_value()))
+            datapass = [reactant, saddle, product]
+        else:
+            processes = glob.glob("%s%d/procdata/*" %(config.path_states, self.stateScale.get_value()))
+            j = 0
+            while ("%s%d/procdata/saddle_%d.con" %(config.path_states,self.stateScale.get_value(), j)) in processes:
+                j+=1
+            numProcesses = j-1        
+            if numProcesses == 0:
+                self.processesSB.set_value(0)
+                self.processesSB.set_sensitive(False)
+            else:
+                self.processesSB.set_sensitive(True)            
+                self.processesSB.set_range(0, numProcesses)
+            saddle = io.loadcon("%s%d/procdata/saddle_%d.con" %(config.path_states,self.stateScale.get_value(), self.processesSB.get_value()))
+            reactant = io.loadcon("%s%d/procdata/reactant_%d.con" %(config.path_states,self.stateScale.get_value(), self.processesSB.get_value()))
+            product = io.loadcon("%s%d/procdata/product_%d.con" %(config.path_states,self.stateScale.get_value(), self.processesSB.get_value()))
+            datapass = [reactant, saddle, product]
+            N = int (self.interpolationSB.get_text())+1
+            p = [reactant, saddle, product]
+            q = []
+            for k in range(len(p)-1):
+                v = atoms.pbc(p[k+1].r - p[k].r, p[k].box)
+                d = np.linalg.norm(v)
+                v /=d
+                q.append(p[k].copy())
+                for l in range(1,N):
+                    temp = p[k].copy()
+                    temp.r += l * (v * (d/(N)))
+                    q.append(temp)
+            q.append(p[2].copy())
+            datapass = q
         self.data_set(datapass)
         
-        
-    def RB_changed(self, widget, data=None):
-        if self.stateRB.get_active() == True:
-            self.processesSB.set_value(0)
-            self.processesSB.set_sensitive(False)
-            self.interpolationCB.set_sensitive(False)
-            self.interpolationSB.set_sensitive(False)
-            self.statePlayTB.set_sensitive(True)
-            self.state_fpsSB.set_sensitive(True)
-            return True
-        else:
-            self.processesSB.set_sensitive(True)
-            self.interpolationCB.set_sensitive(True)
-            self.statePlayTB.set_sensitive(False)
-            self.state_fpsSB.set_sensitive(False)
-            self.statePlayTB.set_active(False)
-            if self.interpolationCB.get_active() == True:
-                self.interpolationSB.set_sensitive(True)
-            return True
+    def filechanged(self, *args):
+        dialog = gtk.FileChooserDialog(action=gtk.FILE_CHOOSER_ACTION_OPEN, buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
+        response = dialog.run()
+        if response == gtk.RESPONSE_OK:
+            self.fileEntry.set_text(dialog.get_filename())
+            self.directory = dialog.get_filename()
+            dialog.destroy()
+        elif response == gtk.RESPONSE_CANCEL:
+            dialog.destroy()
+        self.startup()
            
+    
+    def state_changed(self, *args):
+        self.playing = False
+        self.moviescale.set_value(0)
+      
             
     def interpolationCB_changed(self, widget, data=None):
         if self.interpolationCB.get_active() == True:
@@ -182,32 +181,140 @@ class akmcgui(atomview.atomview):
         
         
     def energy_changed(self, *args):
-        energy = open("states/state_table", "r")
+        energy = open("%sstate_table" %config.path_states, "r")
         energyNumber = energy.readlines()
         a = energyNumber[int (self.stateScale.get_value())].split()[1]
-        self.stateEnergy.set_markup("%f<b>eV</b>" % float (a))
+        self.stateEnergy.set_markup("%.3f<b>eV</b>" % float (a))
+        
+        
+    def rowactivated(self, *args):
+        selection = self.view.get_selection().get_selected()
+        proc = self.store.get(selection[1], 0)
+        self.processesSB.set_value(proc[0])
         
     def processtable(self, *args):
-        a = open("states/%s/processtable" % int(self.stateScale.get_value()) , "r" )
-        a = a.readlines()
-        b = a[int (self.processesSB.get_value()) +1].split()
-        self.rateLabel.set_text(b[7])
-        self.barrierLabel.set_text(b[6])
-        self.prefactorLabel.set_text(b[2])
+    
+        def sortable(a, b, c, d):
+            x = store.get_value(b, d)
+            y = store.get_value(c, d)
+            if x>y:
+                return 1
+            if x==y:
+                return 0
+            if x<y:
+                return -1
+              
+        
+        try:
+            self.scrollview.destroy()
+            self.view.destroy()
+        except:
+            pass
+        self.store = gtk.ListStore(int, float, str, int, float, str, float, str, int)
+        self.view = gtk.TreeView(model=self.store)
+        self.store.set_sort_func(2, sortable, 2)
+        self.store.set_sort_func(5, sortable, 5)
+        self.store.set_sort_func(7, sortable, 7)
+        
+        self.proc_column = gtk.TreeViewColumn('proc #')
+        self.proc_column.set_sort_column_id(0)
+        self.saddleenergy_column = gtk.TreeViewColumn('saddle energy')
+        self.saddleenergy_column.set_sort_column_id(1)
+        self.prefactor_column = gtk.TreeViewColumn('prefactor')
+        self.prefactor_column.set_sort_column_id(2)
+        self.product_column = gtk.TreeViewColumn('product')
+        self.product_column.set_sort_column_id(3)
+        self.productenergy_column = gtk.TreeViewColumn('product energy')
+        self.productenergy_column.set_sort_column_id(4)
+        self.productprefactor_column = gtk.TreeViewColumn('product prefactor')
+        self.productprefactor_column.set_sort_column_id(5)
+        self.barrier_column = gtk.TreeViewColumn('barrier')
+        self.barrier_column.set_sort_column_id(6)
+        self.rate_column = gtk.TreeViewColumn('rate')
+        self.rate_column.set_sort_column_id(7)
+        self.repeat_column = gtk.TreeViewColumn('repeats')
+        self.repeat_column.set_sort_column_id(8)
+        
+        self.view.append_column(self.proc_column)
+        self.view.append_column(self.saddleenergy_column)
+        self.view.append_column(self.prefactor_column)
+        self.view.append_column(self.product_column)
+        self.view.append_column(self.productenergy_column)
+        self.view.append_column(self.productprefactor_column)
+        self.view.append_column(self.barrier_column)
+        self.view.append_column(self.rate_column)
+        self.view.append_column(self.repeat_column)
+        
+        proc_cell = gtk.CellRendererText()
+        saddleenergy_cell = gtk.CellRendererText()
+        prefactor_cell = gtk.CellRendererText()
+        product_cell = gtk.CellRendererText()
+        productenergy_cell = gtk.CellRendererText()
+        productprefactor_cell = gtk.CellRendererText()
+        barrier_cell = gtk.CellRendererText()
+        rate_cell = gtk.CellRendererText()
+        repeat_cell = gtk.CellRendererText()
+        
+        self.proc_column.pack_start(proc_cell)
+        self.saddleenergy_column.pack_start(saddleenergy_cell)
+        self.prefactor_column.pack_start(prefactor_cell)
+        self.product_column.pack_start(product_cell)
+        self.productenergy_column.pack_start(productenergy_cell)
+        self.productprefactor_column.pack_start(productprefactor_cell)
+        self.barrier_column.pack_start(barrier_cell)
+        self.rate_column.pack_start(rate_cell)
+        self.repeat_column.pack_start(repeat_cell)
+        
+        self.proc_column.add_attribute(proc_cell, 'text', 0)
+        self.saddleenergy_column.add_attribute(saddleenergy_cell, 'text', 1)
+        self.prefactor_column.add_attribute(prefactor_cell, 'text', 2)
+        self.product_column.add_attribute(product_cell, 'text', 3)
+        self.productenergy_column.add_attribute(productenergy_cell, 'text', 4)
+        self.productprefactor_column.add_attribute(productprefactor_cell, 'text', 5)
+        self.barrier_column.add_attribute(barrier_cell, 'text', 6)
+        self.rate_column.add_attribute(rate_cell, 'text', 7)
+        self.repeat_column.add_attribute(repeat_cell, 'text', 8)
+        
+        a = open("%s%s/processtable" %(config.path_states, int(self.stateScale.get_value())) , "r")
+        lines = a.readlines()
+        rows = {}
+
+        for i in range(1,len(lines)):
+            rows[i] = lines[i].split()
+            rows[i] = [float(j) for j in rows[i]]
+            self.store.append(rows[i])
+            
+        self.view.connect("row-activated", self.rowactivated)
+        self.scrollview = gtk.ScrolledWindow()
+        self.scrollview.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+        self.scrollview.set_size_request(-1, 150)
+        self.scrollview.add(self.view)
+        self.view.set_headers_clickable(True)
+        self.hbox.pack_start(self.scrollview)
+        self.scrollview.show_all()
+        self.view.show_all()
       
         
     def state_play(self, *args): 
+    # IF statement asking if moviescale playbutton is active
+    #SET MOVIESCALE PLAY BUTTON TRUE
+    # THEN SET IT FALSE
+    #
+    
         try:
             gobject.source_remove(self.timer_id)
         except:
             pass 
         if self.statePlayTB.get_active() == False:
             self.statePlayTB.set_image(self.playImage)
+            self.interpolationCB.set_sensitive(True)
         else:
             self.statePlayTB.set_image(self.pauseImage)
-            states = glob.glob("./states/*")
+            self.interpolationCB.set_active(False)
+            self.interpolationCB.set_sensitive(False)
+            states = glob.glob("%s*" %config.path_states)
             i = 0
-            while ("./states/%d" %i) in states:
+            while ("%s%d" %(config.path_states,i)) in states:
                 i+=1    
             numStates = i-1     
             def loop():    
@@ -215,11 +322,11 @@ class akmcgui(atomview.atomview):
                 if self.stateScale.get_value() >= numStates:
                     self.stateScale.set_value(0)
                 return True 
-            self.timer_id = gobject.timeout_add(1000/(int (self.state_fpsSB.get_value())), loop)
+            self.timer_id = gobject.timeout_add(1000/(int (self.fps.get_value())), loop)
            
             
     def energy_plot(self, *args):
-        a = open("states/state_table", "r")
+        a = open("%sstate_table" %config.path_states, "r")
         b = a.readlines()
         x = []
         y = []
