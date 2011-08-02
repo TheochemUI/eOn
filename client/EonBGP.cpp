a09203f15f68dab7f906752f52d4873402cf3a69
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-08-02 23:10:15 +0000

    New MPMD wrapper guy
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1259 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/EonBGP.cpp
+++ client/EonBGP.cpp
@@ -5,12 +5,14 @@
 #include <stdlib.h>
 #include <libgen.h>
 
+#warning "Building Combined server/client binary for use with MPMD"
+
 void usage(void);
 int client_main(int, char**);
 
 void wrapper_usage()
 {
-    fprintf(stderr, "wrapper <eon.py> <# eonclients> <# GPAWs>\n");
+    fprintf(stderr, "wrapper <eon.py> <# eonclients>\n");
 }
 
 int main(int argc, char **argv)
@@ -21,27 +23,26 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &isize);
     printf("eonbgp: rank: %i\n", irank);
 
-    if (argc != 4) {
+    if (argc != 2) {
         fprintf(stderr,"incorrect number of arguments: %i\n", argc);
         wrapper_usage();
         return 1;
     }
 
     int   client_number = atoi(argv[2]);
-    int   gpaw_number   = atoi(argv[3]);
     char *server_path   = argv[1];
 
-    if (irank < gpaw_number) {
-        //shouldn't happen
-    }else if (irank < gpaw_number+client_number) {
-        printf("rank: %i becoming client\n", irank);
-        client_main(0, NULL);
-    }else if (irank == gpaw_number+client_number) {
+    if (irank == 0) {
+        printf("rank: %i becoming server\n", irank);
         Py_Initialize();
         Py_Main(2, argv);
         Py_Finalize();
+        MPI_Finalize();
+    }else{
+        printf("rank: %i becoming client\n", irank);
+        client_main(0, NULL);
+        //client MPI_Finalizes its self
     }
 
-    MPI_Finalize();
     return 0;
 }
