f721ff8873f07279fe20e915bfabc05091f207a8
Sam Chill <SamChill@users.noreply.github.com>
2011-08-19 15:20:44 +0000

    Updated minimization job to write out a results.dat file.

--- client/MinimizationJob.cpp
+++ client/MinimizationJob.cpp
@@ -36,35 +36,64 @@ std::vector<std::string> MinimizationJob::run(void)
 
     printf("\nBeginning minimization of %s\n", reactant_passed.c_str());
 
+    int status;
+
     Minimizer* mizer=NULL;
-    if(parameters->optMethod == "cg")
-    {
-        mizer = new ConjugateGradients(reactant, parameters);
-    }
-    else if(parameters->optMethod == "qm")
-    {
-        mizer = new Quickmin(reactant, parameters);
-    }
-    else if(parameters->optMethod == "box")
-    {
-        mizer = new QuickminBox(reactant, parameters);
-    }else{
-        printf("Unknown optMethod: %s\n", parameters->optMethod.c_str());
+    try {
+        if(parameters->optMethod == "cg")
+        {
+            mizer = new ConjugateGradients(reactant, parameters);
+        }
+        else if(parameters->optMethod == "qm")
+        {
+            mizer = new Quickmin(reactant, parameters);
+        }
+        else if(parameters->optMethod == "box")
+        {
+            mizer = new QuickminBox(reactant, parameters);
+        }else{
+            printf("Unknown optMethod: %s\n", parameters->optMethod.c_str());
+        }
+    }catch (int e) {
+        if (e == 100) {
+            status = MinimizationJob::STATUS_POTENTIAL_FAILED;
+        }else{
+            printf("unknown exception: %i\n", e);
+            throw e;
+        }
     }
 
     mizer->setOutput(1);
     mizer->fullRelax();
-    
     if (mizer->isItConverged(parameters->optConvergedForce)) {
         printf("Minimization converged within tolerence\n");
+        status = MinimizationJob::STATUS_GOOD;
     }else{
         printf("Minimization did not converge to tolerence!\n"
                "Maybe try to increase maximum_iterations?\n");
+        status = MinimizationJob::STATUS_MAX_ITERATIONS;
     }
 
     printf("Saving result to %s\n", reactant_output.c_str());
     reactant->matter2con(reactant_output);
-    printf("Final Energy: %f\n", reactant->getPotentialEnergy());
+    if (status != MinimizationJob::STATUS_POTENTIAL_FAILED) {
+        printf("Final Energy: %f\n", reactant->getPotentialEnergy());
+    }
+
+    FILE *fileResults;
+
+    std::string resultsFilename("results.dat");
+    returnFiles.push_back(resultsFilename);
+    fileResults = fopen(resultsFilename.c_str(), "wb");
+
+    fprintf(fileResults, "%d termination_reason\n", status);
+    fprintf(fileResults, "minimization job_type\n");
+    fprintf(fileResults, "%s potential_type\n", parameters->potential.c_str());
+    fprintf(fileResults, "%d total_force_calls\n", Potential::fcalls);
+    if (status != MinimizationJob::STATUS_POTENTIAL_FAILED) {
+        fprintf(fileResults, "%f potential_energy\n", reactant->getPotentialEnergy());
+    }
+    fclose(fileResults);
 
     return returnFiles;
 }
