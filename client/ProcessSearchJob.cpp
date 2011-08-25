33dbc6761ee496ee6d5747e5ef592011b9cd517e
Rye Terrell <wwwtyro@users.noreply.github.com>
2011-08-25 18:52:25 +0000

    'SaddlePoint' class inside 'SaddleSearch.cpp' renamed 'SaddleSearch'

--- client/ProcessSearchJob.cpp
+++ client/ProcessSearchJob.cpp
@@ -55,7 +55,7 @@ std::vector<std::string> ProcessSearchJob::run(void)
     barriersValues[0] = barriersValues[1] = 0;
     prefactorsValues[0] = prefactorsValues[1] = 0;
 
-    if (parameters->saddleDisplaceType == SaddlePoint::DISP_LOAD) {
+    if (parameters->saddleDisplaceType == SaddleSearch::DISP_LOAD) {
         // displacement was passed from the server
         if(!saddle->con2matter(displacement_passed)) {
             printf("Stop\n");
@@ -64,25 +64,25 @@ std::vector<std::string> ProcessSearchJob::run(void)
         *min1 = *min2 = *initial;
     }else{
         // displacement and mode will be made on the client
-        // in saddlePoint->initialize(...)
+        // in SaddleSearch->initialize(...)
         *saddle = *min1 = *min2 = *initial;
     }
 
-    saddlePoint = new SaddlePoint();
-    saddlePoint->initialize(initial, saddle, parameters);
-    if (parameters->saddleDisplaceType == SaddlePoint::DISP_LOAD) {
+    saddleSearch = new SaddleSearch();
+    saddleSearch->initialize(initial, saddle, parameters);
+    if (parameters->saddleDisplaceType == SaddleSearch::DISP_LOAD) {
         // mode was passed from the server
-        saddlePoint->loadMode(mode_passed);
+        saddleSearch->loadMode(mode_passed);
     }
     // displacement and mode where made on the client
-    // in saddlePoint->initialize(...)
+    // in SaddleSearch->initialize(...)
 
     int status = doProcessSearch();
 
     printEndState(status);
     saveData(status);
 
-    delete saddlePoint;
+    delete saddleSearch;
     delete initial;
     delete displacement;
     delete saddle;
@@ -98,28 +98,28 @@ int ProcessSearchJob::doProcessSearch(void)
     long status;
     int f1;
     f1 = Potential::fcalls;
-    status = saddlePoint->locate();//(min1, min2);
+    status = saddleSearch->locate();//(min1, min2);
     fCallsSaddle += Potential::fcalls - f1;
 
-    if (status != SaddlePoint::STATUS_INIT) {
+    if (status != SaddleSearch::STATUS_INIT) {
         return status;
     }
 
     // relax from the saddle point located
 
-    AtomMatrix posSaddle = saddlePoint->getSaddlePositions();
+    AtomMatrix posSaddle = saddleSearch->getSaddlePositions();
     AtomMatrix displacedPos;
 
     *min1 = *saddle;
     //XXX: the distance displaced from the saddle should be a parameter
-    displacedPos = posSaddle - saddlePoint->getEigenMode() * parameters->processSearchMinimizationOffset;
+    displacedPos = posSaddle - saddleSearch->getEigenMode() * parameters->processSearchMinimizationOffset;
     min1->setPositions(displacedPos);
     ConjugateGradients cgMin1(min1, parameters);
     cgMin1.fullRelax();
     fCallsMin += cgMin1.totalForceCalls;
 
     *min2 = *saddle;
-    displacedPos = posSaddle + saddlePoint->getEigenMode() * parameters->processSearchMinimizationOffset;
+    displacedPos = posSaddle + saddleSearch->getEigenMode() * parameters->processSearchMinimizationOffset;
     min2->setPositions(displacedPos);
     ConjugateGradients cgMin2(min2, parameters);
     cgMin2.fullRelax();
@@ -139,7 +139,7 @@ int ProcessSearchJob::doProcessSearch(void)
             // the isItConverged in Matter doesn't work so this needs to be fixed.
             // return statusBadMinima;
         }
-        return SaddlePoint::STATUS_BAD_NOT_CONNECTED;
+        return SaddleSearch::STATUS_BAD_NOT_CONNECTED;
     }
 
     //if((!min1->isItConverged(parameters->convergedRelax)))
@@ -154,7 +154,7 @@ int ProcessSearchJob::doProcessSearch(void)
     if (*initial==*min2) {
         /* both minima are the initial state */
         printf("both minima are the initial state");
-        return SaddlePoint::STATUS_BAD_NOT_CONNECTED;
+        return SaddleSearch::STATUS_BAD_NOT_CONNECTED;
     }
 
     /* Calculate the barriers */
@@ -163,7 +163,7 @@ int ProcessSearchJob::doProcessSearch(void)
 
     if((parameters->saddleMaxEnergy < barriersValues[0]) || 
        (parameters->saddleMaxEnergy < barriersValues[1])) {
-        return SaddlePoint::STATUS_BAD_HIGH_BARRIER;
+        return SaddleSearch::STATUS_BAD_HIGH_BARRIER;
     }
 
     if(!parameters->processSearchDefaultPrefactor)
@@ -179,7 +179,7 @@ int ProcessSearchJob::doProcessSearch(void)
             {
                 printf("Reactant bad hessian.\n");
             }
-            return SaddlePoint::STATUS_FAILED_PREFACTOR;
+            return SaddleSearch::STATUS_FAILED_PREFACTOR;
         }
         saddleModes = hessian.getModes(Hessian::SADDLE);
         if(saddleModes.size() == 0)
@@ -188,7 +188,7 @@ int ProcessSearchJob::doProcessSearch(void)
             {
                 printf("Saddle bad hessian.\n");
             }
-            return SaddlePoint::STATUS_FAILED_PREFACTOR;
+            return SaddleSearch::STATUS_FAILED_PREFACTOR;
         }
         prodModes = hessian.getModes(Hessian::PRODUCT);
         if(prodModes.size() == 0)
@@ -197,7 +197,7 @@ int ProcessSearchJob::doProcessSearch(void)
             {
                 printf("Product bad hessian.\n");
             }
-            return SaddlePoint::STATUS_FAILED_PREFACTOR;
+            return SaddleSearch::STATUS_FAILED_PREFACTOR;
         }
         fCallsPrefactors += Potential::fcalls - f1;
 
@@ -224,18 +224,18 @@ int ProcessSearchJob::doProcessSearch(void)
         if((prefactorsValues[0]>parameters->processSearchPrefactorMax) ||
            (prefactorsValues[0]<parameters->processSearchPrefactorMin)){
             cout<<"Bad reactant-to-saddle prefactor:"<<prefactorsValues[0]<<endl;
-            return SaddlePoint::STATUS_BAD_PREFACTOR;
+            return SaddleSearch::STATUS_BAD_PREFACTOR;
         }
         if((prefactorsValues[1]>parameters->processSearchPrefactorMax) ||
            (prefactorsValues[1]<parameters->processSearchPrefactorMin)){
             cout<<"Bad product-to-saddle prefactor:"<<prefactorsValues[1]<<endl;
-            return SaddlePoint::STATUS_BAD_PREFACTOR;
+            return SaddleSearch::STATUS_BAD_PREFACTOR;
         }
     }else{ // use the default prefactor value specified
         prefactorsValues[0]=parameters->processSearchDefaultPrefactor;
         prefactorsValues[1]=parameters->processSearchDefaultPrefactor;
     }
-    return SaddlePoint::STATUS_GOOD;
+    return SaddleSearch::STATUS_GOOD;
 }
 
 void ProcessSearchJob::saveData(int status)
@@ -247,14 +247,14 @@ void ProcessSearchJob::saveData(int status)
     fileResults = fopen(resultsFilename.c_str(), "wb");
     ///XXX: min_fcalls isn't quite right it should get them from
     //      the minimizer. But right now the minimizers are in
-    //      the SaddlePoint object. They will be taken out eventually.
+    //      the SaddleSearch object. They will be taken out eventually.
 
     fprintf(fileResults, "%d termination_reason\n", status);
     fprintf(fileResults, "%ld random_seed\n", parameters->randomSeed);
     fprintf(fileResults, "%s potential_type\n", parameters->potential.c_str());
     fprintf(fileResults, "%d total_force_calls\n", Potential::fcalls);
     fprintf(fileResults, "%ld force_calls_minimization\n", fCallsMin);
-//    fprintf(fileResults, "%ld force_calls_minimization\n", saddlePoint->forceCallsMinimization + fCallsMin);
+//    fprintf(fileResults, "%ld force_calls_minimization\n", SaddleSearch->forceCallsMinimization + fCallsMin);
     fprintf(fileResults, "%d force_calls_saddle\n", fCallsSaddle);
     fprintf(fileResults, "%f potential_energy_saddle\n", saddle->getPotentialEnergy());
     fprintf(fileResults, "%f potential_energy_reactant\n", min1->getPotentialEnergy());
@@ -276,7 +276,7 @@ void ProcessSearchJob::saveData(int status)
     std::string modeFilename("mode.dat");
     returnFiles.push_back(modeFilename);
     fileMode = fopen(modeFilename.c_str(), "wb");
-    saddlePoint->saveMode(fileMode);
+    saddleSearch->saveMode(fileMode);
     fclose(fileMode);
     fclose(fileReactant);
 
@@ -298,34 +298,34 @@ void ProcessSearchJob::saveData(int status)
 void ProcessSearchJob::printEndState(int status)
 {
     fprintf(stdout, "Final state: ");
-    if(status == SaddlePoint::STATUS_GOOD)
+    if(status == SaddleSearch::STATUS_GOOD)
         fprintf(stdout, "Successful.\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_NO_CONVEX)
+    else if(status == SaddleSearch::STATUS_BAD_NO_CONVEX)
         fprintf(stdout, "Initial displacement, not able to reach convex region.\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_HIGH_ENERGY)
+    else if(status == SaddleSearch::STATUS_BAD_HIGH_ENERGY)
         fprintf(stdout, "Saddle search, barrier too high.\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_MAX_CONCAVE_ITERATIONS)
+    else if(status == SaddleSearch::STATUS_BAD_MAX_CONCAVE_ITERATIONS)
         fprintf(stdout, "Saddle search, too many iterations in concave region.\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_MAX_ITERATIONS)
+    else if(status == SaddleSearch::STATUS_BAD_MAX_ITERATIONS)
         fprintf(stdout, "Saddle search, too many iterations in saddle point search.\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_NOT_CONNECTED)
+    else if(status == SaddleSearch::STATUS_BAD_NOT_CONNECTED)
         fprintf(stdout, "Minima, saddle is not connected to initial state.\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_PREFACTOR)
+    else if(status == SaddleSearch::STATUS_BAD_PREFACTOR)
             fprintf(stdout, "Prefactors, not within window\n");
 
-    else if(status == SaddlePoint::STATUS_FAILED_PREFACTOR)
+    else if(status == SaddleSearch::STATUS_FAILED_PREFACTOR)
             fprintf(stdout, "Prefactors, hessian calculation failed\n");
 
-    else if(status == SaddlePoint::STATUS_BAD_HIGH_BARRIER)
+    else if(status == SaddleSearch::STATUS_BAD_HIGH_BARRIER)
         fprintf(stdout, "Energy barriers, not within window\n");
 
-    else if (status == SaddlePoint::STATUS_BAD_MINIMA)
+    else if (status == SaddleSearch::STATUS_BAD_MINIMA)
         fprintf(stdout, "Minima, from saddle did not converge\n");
     else
         fprintf(stdout, "Unknown status: %i!\n", status);
