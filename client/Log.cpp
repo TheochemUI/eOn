1dd6dce64f640dce6657feb9ee38ed866044e6e1
Rye Terrell <wwwtyro@users.noreply.github.com>
2011-09-13 20:52:26 +0000

    introduced logging infrastructure

--- client/Log.cpp
+++ client/Log.cpp
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "Log.h"
 
 static FILE *logfile=NULL;
+static Parameters *params = NULL;
+
+void log_init(Parameters *p, char *filename) {
+    params = p;
+    if (logfile == NULL) {
+        logfile = fopen(filename, "w");
+        setvbuf(logfile, NULL, _IOLBF, 0);
+    }
+}
+
+void log_close() {
+    if (logfile != NULL) {
+        fclose(logfile);
+        logfile = NULL;
+    }
+}
 
 void log(const char* format, ...) {
     va_list args;
-
-    if (logfile == NULL) {
-        logfile = fopen("client.log", "w");
-        setvbuf(logfile, NULL, _IOLBF, 0);
-    }
-
     va_start(args, format);
-    vfprintf(logfile, format, args);
     vfprintf(stdout,  format, args);
     va_end(args);
+    va_start(args, format);
+    vfprintf(logfile, format, args);
+    va_end(args);
+}
 
-    fprintf(stdout, "\n");
+void log_file(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(logfile, format, args);
+    va_end(args);
 }
