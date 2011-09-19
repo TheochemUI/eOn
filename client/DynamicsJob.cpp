c14aa7583fcbaaaef8b6a8b979d7a0170b74f17a
brightzhang <brightzhang@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-09-19 15:54:40 +0000

    Write final config after MD jobs; add ptisland_test sample for md
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1346 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/DynamicsJob.cpp
+++ client/DynamicsJob.cpp
@@ -46,12 +46,25 @@ DynamicsJob::~DynamicsJob()
 std::vector<std::string> DynamicsJob::run(void)
 {
     Matter *R = new Matter(parameters);
+    Matter *F = new Matter(parameters);
     R->con2matter("reactant_passed.con");
-
+    *F = *R;
+    
+    printf("Now running Regular Dynamics at %8.2lf K with %10ld steps\n", parameters->temperature, parameters->mdSteps);
     Dynamics *d = new Dynamics(R, parameters);
     d->fullSteps(parameters->temperature);
 
+    *F = * R;
+    FILE *fileProduct;
+    std::string productFilename("product.con");
+    returnFiles.push_back(productFilename);
+    
+    fileProduct = fopen(productFilename.c_str(), "wb");
+    F->matter2con(fileProduct);
+    fclose(fileProduct);
+
     delete R;
+    delete F;
     delete d;
 
     std::vector<std::string> returnFiles;  
