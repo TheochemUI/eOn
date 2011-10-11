e45f56603831efb4a02522ad13580d97df4481ba
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-10-11 21:00:38 +0000

    added sample mpmd run and submit scripts
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1440 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ tools/mpmd/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+export EON_NUMBER_OF_CLIENTS=3
+cores_per_pot=4
+
+cores_per_node=8
+
+eon_path=../eon
+pot_path=vasp_mpmd
+
+export EON_SERVER_PATH=${eon_path}/akmc.py
+client_path=${eon_path}/client/client_mpi
+
+client_ranks=$(expr $EON_NUMBER_OF_CLIENTS + 1)
+pot_ranks=$(expr $cores_per_pot \* $EON_NUMBER_OF_CLIENTS)
+
+command="mpirun -n $pot_ranks -wdir vasp $pot_path : -n $client_ranks $client_path"
+
+#if no arguments
+if [ -z $1 ]; then
+    ${eon_path}/tools/mkvasp.py $EON_NUMBER_OF_CLIENTS vasp
+    $command
+else
+    total_ranks=$(expr $pot_ranks + $client_ranks)
+    if [ "$1" = "-n" ]; then
+        echo $(expr $cores_per_node \* \( \( $total_ranks - 1 \) / $cores_per_node \) + $cores_per_node)
+    fi
+
+    if [ "$1" = "--dry-run" ]; then
+        echo mpirun command: $command
+        echo total nodes: $(expr $total_ranks / $cores_per_node)
+        echo total ranks: $total_ranks
+    fi
+fi
