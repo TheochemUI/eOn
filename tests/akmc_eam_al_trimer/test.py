7e486fcbba2442c7760ba7b76ff2c5b4a6d61263
Sam Chill <SamChill@users.noreply.github.com>
2011-10-24 21:32:18 +0000

    Fixed akmc test by explicitly setting [Dimer] opt_method to the old default (sd).

--- tests/akmc_eam_al_trimer/test.py
+++ tests/akmc_eam_al_trimer/test.py
@@ -8,17 +8,17 @@ from ndiff import ndiff
 test_path = os.path.split(os.path.realpath(__file__))[0]
 test_name = os.path.basename(test_path)
 
-os.system("../../eon.py --reset --force --quiet")
+os.system("../../eon --reset --force --quiet")
 for i in range(15):
-    retval = os.system("../../eon.py --quiet")
+    retval = os.system("../../eon --quiet")
     if retval:
-        print "%s: problem running eon.py" % test_name
+        print "%s: problem running eon" % test_name
         sys.exit(1)
 
 same, max_rel_err, reason = ndiff("dynamics.test", "dynamics.txt", 0.01)
 if same:
     print "%s: passed maximum relative error of %.3e"%(test_name,max_rel_err)
-    os.system("../../eon.py --reset --force --quiet")
+    os.system("../../eon --reset --force --quiet")
     sys.exit(0)
 else:
     print "%s: failed %s"%(test_name,reason)
