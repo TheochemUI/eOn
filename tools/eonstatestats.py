4e4d2f1884fe7aa121fcb3a8578a1a615688c513
Rye Terrell <wwwtyro@users.noreply.github.com>
2011-10-25 00:05:18 +0000

    nonnegative displacment abort option implemented, defaults to false

--- tools/eonstatestats.py
+++ tools/eonstatestats.py
@@ -33,6 +33,9 @@ for state in states:
     num_good = 0
     num_unique = 0
     fc_sad = 0
+    fc_sad_tot = 0
+    fc_sad_abort = 0
+    fc_sad_bad = 0
     fc_min = 0
     fc_dyn = 0
     e_sad = 0
@@ -42,6 +45,9 @@ for state in states:
         num_search += 1
         line_item = line.split()
         search_status = line_item[7]
+        fc_sad_tot += int(line_item[4])
+        if "Nonnegative Displacement Abort" in line:
+            fc_sad_abort += int(line_item[4])
         if (re.search("good",search_status) or re.search("repeat",search_status)):
 #            print " ".join(line_item)
             num_good += 1
@@ -51,6 +57,8 @@ for state in states:
             fc_sad += float(line_item[4])
             fc_min += float(line_item[5])
             fc_dyn += float(line_item[6])
+        else:
+            fc_sad_bad += int(line_item[4])
 
     # print stats
     print " num search :",num_search
@@ -62,5 +70,8 @@ for state in states:
         print " fcalls sad : {0:.2f}".format(float(fc_sad)/float(num_good))
         print " fcalls min : {0:.2f}".format(fc_min/num_good)
         print " fcalls dyn : {0:.2f}".format(fc_dyn/num_good)
+        print " fcalls sad total : %d" % fc_sad_tot
+        print " fcalls sad abort : %d" % fc_sad_abort
+        print " fcalls sad bad : %d" % fc_sad_bad
 
 print ""
