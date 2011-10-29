6f804358d96cdd5bdddedb6cf0e05f1873553c67
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-10-29 03:54:02 +0000

    tests update
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1549 9d962951-508e-48a8-94b5-974f9cfda47c

--- tests/mpi_test/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-../../akmc.py --reset -f
-mpirun -n 8 ../../tools/emt_sp.py : \
-       -n 8 ../../client/client : \
-       -n 1 ../../akmc.py 
--- tests/mpi_test/run_simple.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-../../akmc.py --reset -f
-mpirun -n 1 ../../akmc.py : \
-       -n 2 ../../client/client
