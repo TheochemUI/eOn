6ef4e31733a53f14ed7358f777551ee6a572b2d2
Rye Terrell <wwwtyro@users.noreply.github.com>
2011-11-11 10:02:13 +0000

    dist.py sorts by atom norm

--- tools/dist.py
+++ tools/dist.py
@@ -10,11 +10,19 @@ import atoms
 p1 = io.loadcon(sys.argv[1])
 p2 = io.loadcon(sys.argv[2])
 
-print     "\n%5s    %10s    %10s    %10s    %10s" % ("atom", "x", "y", "z", "norm")
-print "-------------------------------------------------------------"
+distances = []
 
 for i in range(len(p1)):
     diff = atoms.pbc(p2.r[i] - p1.r[i], p1.box)
-    print "%5d    %10f    %10f    %10f    %10f" % (i, diff[0], diff[1], diff[2], numpy.linalg.norm(diff))
+    distances.append((i, diff[0], diff[1], diff[2], numpy.linalg.norm(diff)))
+
+distances.sort(key=lambda d: d[4])
+
+
+print "\n%5s    %10s    %10s    %10s    %10s" % ("atom", "x", "y", "z", "norm")
+print "-------------------------------------------------------------"
+
+for d in distances:
+    print "%5d    %10f    %10f    %10f    %10f" % (d[0], d[1], d[2], d[3], d[4])
 
 print "\n total norm:", numpy.linalg.norm(atoms.pbc(p2.r - p1.r, p1.box)), "\n"
