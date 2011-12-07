e2dd6c66dbd735a1444bece790257de92b856e88
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-12-07 20:13:54 +0000

    server side processes search incorrectly used 'indistinguisable' comparison when comparing the minimization from a saddle to the reactant for that state.
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1605 9d962951-508e-48a8-94b5-974f9cfda47c

--- tools/identical.py
+++ tools/identical.py
@@ -2,8 +2,11 @@
 import sys
 
 import pathfix
-import io, atoms
+import fileio as io
+import atoms
+import config
+config.comp_eps_r=float(sys.argv[3])
 
 p1 = io.loadcon(sys.argv[1])
 p2 = io.loadcon(sys.argv[2])
-print atoms.identical(p1,p2,float(sys.argv[3]))
+print atoms.identical(p1,p2)
