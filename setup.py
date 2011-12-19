2ea9ad41d5f2279bbb17a924d685de3064527e1c
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2011-12-19 06:11:58 +0000

    this is the second part of the previous commit. svn won't let me do all the renaming in one step.
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1635 9d962951-508e-48a8-94b5-974f9cfda47c

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-#try:
-#    from setuptools import setup
-#except ImportError:
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-packages = ['eonpy']
-package_dir = {'eonpy':'eonpy'}
-package_data = {'eonpy': ['config.yaml']}
+packages = ['eon']
+package_dir = {'eon':'eon'}
+package_data = {'eon': ['config.yaml']}
 
-setup(name='eonpy',
+setup(name='eon',
       packages=packages,
       package_dir=package_dir,
       package_data=package_data,
