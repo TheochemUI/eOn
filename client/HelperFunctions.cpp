aefe74f964d524db3dbcf15f66e86b45b250b38b
Sam Chill <SamChill@users.noreply.github.com>
2012-08-22 18:41:13 +0000

    Fix string copy in HelperFunctions string split

--- client/HelperFunctions.cpp
+++ client/HelperFunctions.cpp
@@ -438,16 +438,20 @@ std::vector<int> helper_functions::split_string_int(std::string s, std::string d
 {
     char *pch;
     char *str;
-    str = (char*) malloc(sizeof(char)*s.size());
-    strcpy(str, s.c_str());
+    str = (char*) malloc(sizeof(char)*(s.length()+1));
+    s.copy(str, s.length(), 0);
+    str[s.length()] = '\0';
+    cout << "COrrect: " << s << endl;
+    printf("HI: %s\n", str);
     pch = strtok(str, delim.c_str());
 
     std::vector<int> list;
 
     while (pch != NULL) {
-        char *end;
-        int value = (int)strtol(pch, &end, 10); 
-        if (*end) {
+        char *endptr;
+        int value = (int)strtol(pch, &endptr, 10); 
+        if (strcmp(pch, endptr) == 0) {
+            //Error reading
             std::vector<int> emptylist;
             free(str);
             return emptylist;
