69e6ae0569e64af9ac2b38abd515a194d1e44494
S. Alireza Ghasemi <alirezagh76@gmail.com>
2012-08-29 18:19:34 +0000

    First version of generic global optimization job, it is incomplete in several ways

--- client/MinimaHoppingJob.h
+++ client/MinimaHoppingJob.h
@@ -13,14 +13,42 @@
 
 #include "Job.h"
 #include "Parameters.h"
+#include "Matter.h"
 
 class MinimaHoppingJob: public Job {
     public:
         MinimaHoppingJob(Parameters *params);
         ~MinimaHoppingJob(void);
+        void move_step(Matter *);
+        void accept_reject_step(Matter *,Matter *);
+        void accept_reject_minhopp(Matter *,Matter *);
+        void examine_escape(Matter *,Matter *);
+        void apply_move_feedback_p1(Matter *);
+        void apply_move_feedback_p2(Matter *);
+        void mdescape(Matter *);
+        void insert(Matter *);
+        size_t hunt(double);
+        void velopt(Matter *);
         std::vector<std::string> run(void);
+		double beta1;
+		double beta2;
+		double beta3;
+		double alpha1;
+		double alpha2;
+		size_t mdmin;
     private:
         Parameters *parameters;
+		size_t nlmin;
+		double etoler;
+		double ediff;
+		double ekin;
+		std::vector<double> earr;
+		string escaped;
+		string move_type;
+		string move_feedback;
+		string acc_rej_type;
+		string acc_rej_decision;
+		string trial_minimum;
 };
 
 #endif
