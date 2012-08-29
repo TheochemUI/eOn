0f1a08046880d16fb7cdb9ef3f6acfb5f24a5ce8
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2012-08-29 15:34:19 +0000

    Fix refinition of arguments in _force definition in FeHe.h. This fixes bug #197.
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1819 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/potentials/FeHe/FeHe.h
+++ client/potentials/FeHe/FeHe.h
@@ -28,8 +28,10 @@
     
 extern "C" 
 {
-    void force_(const long int *N, const double *RX, const double *RX, const double *RX, const long int *ISPEC,
-                double *FX, double *FY, double *FZ, double *U, const double* bx, const double* by, const double* bz);
+    void force_(const long int *N, const double *RX, const double *RY, 
+                const double *RZ, const long int *ISPEC,
+                double *FX, double *FY, double *FZ, double *U, 
+                const double* bx, const double* by, const double* bz);
 }
 /* No potential initialization    
 extern "C" 
