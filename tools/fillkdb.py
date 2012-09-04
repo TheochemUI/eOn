b2f74f7636efbeef27c42a3d7cb5ce91ad54fe2b
rye <rye@9d962951-508e-48a8-94b5-974f9cfda47c>
2012-09-04 18:52:20 +0000

    added tool to extract a KDB from an existing eon akmc simulation
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1828 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ tools/fillkdb.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# Assumes kdbinsert.py is in the path, that the user is inside an eon akmc 
+# directory, and that the states directory is named 'states'.
+
+import os
+
+n = 0
+while os.path.exists(os.path.join('states', '%d' % n)):
+	lines = open(os.path.join('states', '%d' % n, 'processtable'), 'r').readlines()[1:]
+	procs = [line.split()[0] for line in lines]
+	for proc in procs:
+		path = os.path.join('states', '%d' % n, 'procdata')
+		reactant_path = os.path.join('states', '%d' % n, 'procdata', 'reactant_%s.con' % proc)
+		saddle_path = os.path.join('states', '%d' % n, 'procdata', 'saddle_%s.con' % proc)
+		product_path = os.path.join('states', '%d' % n, 'procdata', 'product_%s.con' % proc)
+		mode_path = os.path.join('states', '%d' % n, 'procdata', 'mode_%s.dat' % proc)
+		os.system('kdbinsert.py %s %s %s -o %s' % (reactant_path, saddle_path, product_path, mode_path))
+	n += 1
+
