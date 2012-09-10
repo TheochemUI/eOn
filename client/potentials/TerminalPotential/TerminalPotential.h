0ef7846d92a441bfc6b9a06baae0acec79bfb6f5
andreas <andreas@9d962951-508e-48a8-94b5-974f9cfda47c>
2012-09-10 13:50:06 +0000

    added new potential which makes a system call and communicates through files on the hard drive
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1856 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ client/potentials/TerminalPotential/TerminalPotential.h
@@ -0,0 +1,33 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//-----------------------------------------------------------------------------------
+
+#ifndef ITERMINAL_POTENTIAL
+#define ITERMINAL_POTENTIAL
+
+#include "../../Potential.h"
+
+class TerminalPotential : public Potential
+{
+
+    public:
+        TerminalPotential(Parameters *p);
+		~TerminalPotential();
+        void initialize() {};
+        void cleanMemory(void);    
+        void force(long N, const double *R, const int *atomicNrs, 
+                   double *F, double *U, const double *box);
+
+    private:
+        void passToSystem(long N, const double *R, const int *atomicNrs, const double *box);
+        void recieveFromSystem(long N, double *F, double *U);
+
+};
+
+#endif
