8f24e8a4241d7ebbd483aa902d5231c4e5ccc54b
rye <rye@9d962951-508e-48a8-94b5-974f9cfda47c>
2012-09-19 20:57:14 +0000

    script to perform cna analysis for an akmc run
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1884 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ tools/akmccna.py
@@ -0,0 +1,33 @@
+
+import os
+from collections import Counter
+
+import eon
+
+i = 0
+while os.path.isdir(os.path.join('.', 'states', str(i))):
+	p = eon.fileio.loadcon(os.path.join('.', 'states', str(i), 'reactant.con'))
+	cna = eon.atoms.cnat(p,4.0)
+	count = Counter()
+	for c in cna:
+		count[int(c)] += 1
+	tally = []
+	for k, v in count.iteritems():
+		tally.append((k, v))
+	tally.sort(key = lambda k: k[0])
+	for t in tally:
+		print "%3d" % t[1],
+	print
+
+	for j in range(len(p)):
+		p.names[j] = eon.atoms.elements[int(cna[j]) + 1]['symbol']
+	eon.fileio.savecon('movie.con', p, w='a')
+
+	i += 1
+
+
+
+
+
+
+
