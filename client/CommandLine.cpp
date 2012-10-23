43cc73c0aad5124e495241c88b7d5fc101750d67
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2012-10-23 21:29:21 +0000

    added lennard jones cluster potential (lj_cluster)
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@1941 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/CommandLine.cpp
+++ client/CommandLine.cpp
@@ -6,6 +6,7 @@
 
 #include <cstdlib>
 #include <string>
+#include <unistd.h>
 
 using namespace std;
 
---
+++ client/potentials/LennardJonesCluster/LJCluster.cpp
@@ -0,0 +1,84 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//-----------------------------------------------------------------------------------
+
+#include"LJCluster.h"
+
+LJCluster::LJCluster(){
+    this->setParameters(1.0, 15.0, 1.0);
+}
+
+LJCluster::LJCluster(double u0Recieved, double cuttOffRRecieved, double psiRecieved){
+    this->setParameters(u0Recieved, cuttOffRRecieved, psiRecieved);
+    return;
+}
+
+void LJCluster::cleanMemory(void){
+    return;
+}
+
+// General Functions
+void LJCluster::setParameters(double u0Recieved, double cuttOffRRecieved, double psiRecieved){
+    u0 = u0Recieved;
+    psi = psiRecieved;
+    
+    cuttOffR = cuttOffRRecieved;
+    cuttOffU = 4*u0*(pow(psi/cuttOffR,12)-pow(psi/cuttOffR,6));
+    return;
+}
+
+// pointer to number of atoms, pointer to array of positions	
+// pointer to array of forces, pointer to internal energy
+// adress to supercell size
+void LJCluster::force(long N, const double *R, const int *atomicNrs, double *F, double *U, const double *box){
+    double diffR=0, diffRX, diffRY, diffRZ, dU, a, b;
+    *U = 0;    
+    for(int i=0;i<N;i++){
+        F[ 3*i ] = 0;
+        F[3*i+1] = 0;
+        F[3*i+2] = 0;
+    }
+    // Initializing end
+    
+    for(int i=0; i<N-1; i++){
+        for(int j=i+1; j<N ;j++){
+            diffRX = R[ 3*i ]-R[ 3*j ];
+            diffRY = R[3*i+1]-R[3*j+1];
+            diffRZ = R[3*i+2]-R[3*j+2];
+
+            //diffRX = diffRX-box[0]*floor(diffRX/box[0]+0.5); // floor = largest integer value less than argument 
+            //diffRY = diffRY-box[4]*floor(diffRY/box[4]+0.5);
+            //diffRZ = diffRZ-box[8]*floor(diffRZ/box[8]+0.5);
+            
+            diffR = sqrt(diffRX*diffRX+diffRY*diffRY+diffRZ*diffRZ);
+                
+            // 4u0((psi/r0)^12-(psi/r0)^6)
+            a = pow(psi/diffR,6);
+            b = 4*u0*a;
+            
+            *U = *U+b*(a-1);
+
+            dU=-6*b/diffR*(2*a-1);
+            // F is the negative derivative
+            F[ 3*i ]=F[ 3*i ] - dU*diffRX/diffR;
+            F[3*i+1]=F[3*i+1] - dU*diffRY/diffR;
+            F[3*i+2]=F[3*i+2] - dU*diffRZ/diffR;
+            
+            F[ 3*j ]=F[ 3*j ] + dU*diffRX/diffR;
+            F[3*j+1]=F[3*j+1] + dU*diffRY/diffR;
+            F[3*j+2]=F[3*j+2] + dU*diffRZ/diffR;
+        }
+    }
+    return;
+}
+
+LJCluster::~LJCluster()
+{
+    cleanMemory();
+}
