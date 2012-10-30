2e16a2cee555ef440b1ee95cd108fce11fef6f05
Rye Terrell <wwwtyro@users.noreply.github.com>
2012-10-30 18:55:32 +0000

    eonini.py prints to stdout

--- tools/eonini.py
+++ tools/eonini.py
@@ -6,23 +6,15 @@ import ConfigParser
 import pathfix
 import yaml
 
-if os.path.isfile('config.ini'):
-	print 'Error: config.ini file already exists. Aborting.'
-	import sys
-	sys.exit()
-
 yaml_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'eon', 'config.yaml')
 yaml_file = open(yaml_path)
 y = yaml.load(yaml_file)
 yaml_file.close()
 
-f = open('config.ini', 'w')
-
 for section in y:
-	f.write('\n') 
-	f.write("[%s]\n" % section)
-	for option in y[section]['options']:
-		f.write("%s = %s\n" % (option, str(y[section]['options'][option]['default'])))
+    print
+    print "[%s]" % section
+    for option in y[section]['options']:
+	    print "%s = %s" % (option, str(y[section]['options'][option]['default']))
 
-f.close()
 
