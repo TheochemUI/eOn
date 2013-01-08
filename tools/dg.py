a475669d3528d549f299a903b1570685f8dff5c5
rye <rye@9d962951-508e-48a8-94b5-974f9cfda47c>
2013-01-08 21:26:08 +0000

    disconnectivity graph plotter for akmc simulations
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2001 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ tools/dg.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import sys
+
+import numpy
+
+import pygmin.storage
+from pygmin.landscape import Graph
+from pygmin.utils import disconnectivity_graph
+
+import matplotlib.pyplot as plt
+
+nlevels = 20
+if len(sys.argv) > 1:
+    try:
+        nlevels = int(sys.argv[1])
+    except:
+        print
+        print "usage: run dg.py in an eon akmc directory with the number of energy bins as an optional argument."
+        print
+        sys.exit()
+    
+print 'Creating a disconnectivity graph with %d energy levels.' % nlevels
+
+
+minima_energies = [float(line.strip().split()[1]) for line in open("states/state_table", 'r').readlines()]
+num_minima = len(minima_energies)
+
+db = pygmin.storage.Database(accuracy=0)
+
+print 'Loading states...'
+minima = []
+for i in range(num_minima):
+    minima.append(db.addMinimum(minima_energies[i], numpy.array([i])))
+
+print 'Loading transitions...'
+transitions = []
+for i in range(num_minima):
+    lines = open('states/%d/processtable' % i, 'r').readlines()[1:]
+    reac = minima[i]
+    for j in range(len(lines)):
+        line = lines[j].split()
+        prod = int(line[3])
+        if prod == -1:
+            continue
+        prod = minima[prod]
+        e = float(line[1])
+        proc_id = int(line[0])
+        coords = int('%d%d' % (i+1,j))    
+        if [reac,prod] not in transitions and [prod,reac] not in transitions:
+            db.addTransitionState(e, coords, reac, prod)
+            transitions.append([reac,prod])
+        
+graphwrapper = Graph(db)
+graph = graphwrapper.graph
+
+dg = disconnectivity_graph.DisconnectivityGraph(graph,subgraph_size=0,nlevels=nlevels)
+dg.calculate()
+
+print 'Plotting (this can take a while)...'
+dg.plot()
+print 'Saving tree.pdf...'
+plt.savefig("tree.pdf")    
+print 'Displaying plot...'
+plt.show()
+print 'Done.'
+
+     
