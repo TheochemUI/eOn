cdcb7c52da3f6d271df98fab8ed9cefd816e1e02
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2013-05-14 04:54:57 +0000

    finished up mcamc module, needs to be integrated with superbasin.py
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2133 9d962951-508e-48a8-94b5-974f9cfda47c

--- eon/mcamc/test.py
+++ eon/mcamc/test.py
@@ -3,6 +3,7 @@ import os
 import numpy
 import ctypes
 from time import time
+from mcamc import mcamc
 
 
 def random_chain(t,r,p):
@@ -38,56 +39,10 @@ def random_chain(t,r,p):
 #        raise RuntimeWarning("Problems is too ill-conditioned")
 #    print 'chose precision: %s' % prec
 
-def mcamc(Q,R,c,prec='dd'):
-    Qflat = list(Q.ravel())
-    Qflat = (ctypes.c_double * len(Qflat))(*Qflat)
-    Rflat = list(R.ravel())
-    Rflat = (ctypes.c_double * len(Rflat))(*Rflat)
-    cflat = list(c)
-    cflat = (ctypes.c_double * len(cflat))(*cflat)
-
-    Bflat = (ctypes.c_double * len(Rflat))()
-    tflat = (ctypes.c_double * len(cflat))()
-
-    # void solve(int Qsize, double *Qflat, int Rcols, double *Rflat, 
-    #           double *c_in, double *B, double *t)
-    mcamc_dll = ctypes.CDLL(os.path.abspath('mcamc.so'))
-
-    if prec == 'f':
-        f = mcamc_dll.solve_float
-    elif prec == 'd':
-        f = mcamc_dll.solve_double
-    elif prec == 'dd':
-        f = mcamc_dll.solve_double_double
-    elif prec == 'qd':
-        f = mcamc_dll.solve_quad_double
-    else:
-        raise ValueError('Unknown prec %s' % prec)
-    f(Q.shape[0], Qflat, R.shape[1], Rflat, cflat, Bflat, tflat)
-    B = numpy.array(list(Bflat)).reshape(R.shape)
-    t = list(tflat)
-    return t, B
-
 def main():
-    print 'PERFORMANCE TESTING'
-    print '-------------------'
-
-    print '%8s %8s %8s %8s' % ('N', 'f', 'd', 'dd')
-    for Ntrans in [10,100,200,500,800,1000,1200,1500]:
-        times = []
-        for prec in ['f','d','dd']:
-            p = 1e-5
-            Nabs = 50
-            Q,R,c = random_chain(Ntrans,Nabs,p)
-            t1 = time()
-            t, B = mcamc(Q,R,c,prec)
-            t2 = time()
-            times.append(t2-t1)
-        print '%8i %8.3f %8.3f %8.3f' % (Ntrans,times[0],times[1],times[2])
 
     Ntrans = 200
     Nabs = 50
-    print '\n'
     print 'PRECISION TESTING'
     print '-----------------'
     print 'Solving MCAMC problem with %i transient states and %i absorbing states' % (Ntrans, Nabs)
@@ -100,5 +55,21 @@ def main():
             t, B = mcamc(Q,R,c,prec)
             errors.append(abs(1.0-max(abs(numpy.sum(B,1)))))
         print '%5.0e %5.0e %5.0e %5.0e %5.0e' % (p,errors[0],errors[1],errors[2],errors[3])
+
+    print '\nPERFORMANCE TESTING'
+    print '-------------------'
+
+    print '%8s %8s %8s %8s %8s' % ('N', 'f', 'd', 'dd', 'qd')
+    for Ntrans in [10,100,200,500,800,1000,1200,1500]:
+        times = []
+        for prec in ['f','d','dd','qd']:
+            p = 1e-5
+            Nabs = 50
+            Q,R,c = random_chain(Ntrans,Nabs,p)
+            t1 = time()
+            t, B = mcamc(Q,R,c,prec)
+            t2 = time()
+            times.append(t2-t1)
+        print '%8i %8.3f %8.3f %8.3f %8.3f' % (Ntrans,times[0],times[1],times[2],times[3])
 
 main()
