895bf5665b46a525e9171ff4de4b6a2daedf9dee
chill <chill@9d962951-508e-48a8-94b5-974f9cfda47c>
2013-05-14 16:19:49 +0000

    added a routine to guess the precision needed in libmcamc
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2137 9d962951-508e-48a8-94b5-974f9cfda47c

--- eon/mcamc/mcamc.py
+++ eon/mcamc/mcamc.py
@@ -3,6 +3,35 @@ import ctypes
 import numpy as np
 from os.path import join, abspath, dirname
 
+libpath = join(dirname(abspath(__file__)), 'libmcamc.so')
+libmcamc = ctypes.CDLL(libpath)
+
+def estimate_condition(Q, R):
+    Qflat = list(Q.ravel())
+    Qflat = (ctypes.c_double * len(Qflat))(*Qflat)
+    Rflat = list(R.ravel())
+    Rflat = (ctypes.c_double * len(Rflat))(*Rflat)
+    libmcamc.estimate_condition.restype = ctypes.c_double
+
+    return libmcamc.estimate_condition(Q.shape[0], Qflat, R.shape[1], Rflat)
+
+def guess_precision(Q, R):
+    cond = estimate_condition(Q, R)
+    k = np.log10(cond)
+
+    if k < 11:
+        prec = 'd'
+    elif k < 26:
+        prec = 'dd'
+    elif k < 60:
+        prec = 'qd'
+    else:
+        #problem is too ill-conditioned to solve accurately
+        prec = '-'
+
+    return prec
+
+
 def mcamc(Q, R, c, prec='dd'):
     Qflat = list(Q.ravel())
     Qflat = (ctypes.c_double * len(Qflat))(*Qflat)
@@ -15,8 +44,6 @@ def mcamc(Q, R, c, prec='dd'):
     tflat = (ctypes.c_double * len(cflat))()
 
     residual = (ctypes.c_double * 1)()
-    libpath = join(dirname(abspath(__file__)), 'libmcamc.so')
-    libmcamc = ctypes.CDLL(libpath)
 
     if prec == 'f':
         solve = libmcamc.solve_float
