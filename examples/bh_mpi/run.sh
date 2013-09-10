00c97e852b1e4c99c947492c2abc7fbcc8fc1f03
Sam Chill <SamChill@users.noreply.github.com>
2013-09-10 12:54:18 +0000

    update basin hopping lammps mpi example

--- examples/bh_mpi/run.sh
+++ examples/bh_mpi/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export EON_NUMBER_OF_CLIENTS=7
+export EON_NUMBER_OF_CLIENTS=2
 export EON_SERVER_PATH=~/code/eon/eon/basinhopping.py
-mpirun -n 8 ~/code/eon/client/client_mpi
+mpirun -n 3 ~/code/eon/client/eonclientmpi
