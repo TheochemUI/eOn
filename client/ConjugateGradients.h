d0a74ea770a8794e02ce160aa3fe0c3d2a7a1e27
rbwilcoxon11 <rbwilcoxon11@9d962951-508e-48a8-94b5-974f9cfda47c>
2015-12-09 02:08:48 +0000

    Documentation updates thus far; .dox file tells doxygen how to run, .txt file has the doxygen mainpage, the header files are now doxygenated (a phrase I am coining, and the makefiles now support targets docsClean (removes doxygen documentation) docsTest (debugger for variable info passed to doxygen) docs (makes the docs, outputs to doxygen file in the eon home directory) and updated the targets all (now includes docs) and clean (now includes docsClean)
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2286 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/ConjugateGradients.h
+++ client/ConjugateGradients.h
@@ -17,31 +17,78 @@
 #include "Parameters.h"
 #include "HelperFunctions.h"
 
+/**
+ * @file
+ * @ingroup Optimizers
+ *
+ * \brief Direct optimization for energy minimization
+ *
+ * The conjugate gradient method is an algorithm for the numerical solution of particular
+ * stystems of linear equations, namely symetric,positive-definite ones.
+ *
+ */
+
+/**
+ * Decleration of the Conjugate Gradients optimizer
+ */
+
 class ConjugateGradients : public Optimizer
 {
     public:
-
+	//! Conjugate Gradients optimizer constructor
+	/*!
+ 	 * \param *objf an ref ObjectiveFunction that tells the optimizer how to run
+ 	 * \param *parameters defined by the config.init file
+ 	 */	 
         ConjugateGradients(ObjectiveFunction *objf, Parameters *parameters);
+	//! Conjugant Gradient deconstructor
         ~ConjugateGradients();
 
+	//! Calls the next step in the algorithm
+	/**
+ 	 * Either calls the single_step or line_search method depending on the parameters
+ 	 * \return whether or not the algorithm has converged
+ 	 */
         bool step(double maxMove);
+	//! Runs the conjugate gradient 
+	/**
+ 	 * \todo method should also return an error code and message if the algorithm errors out
+ 	 * \return algorithm convergence
+	 */
         bool run(int maxIterations, double maxMove);
+	//! Gets the direction of the next step
         VectorXd getStep();
 
     private:
-
+	//! An objective function relating a certain job method to the conjugate gradient optimizer
         ObjectiveFunction *objf;
+	//! Parameters set by the config.init file
         Parameters *parameters;
 
+	//! Current step direction of the conjugate gradient
         VectorXd direction;
+	//! Algorithms previous step direction
         VectorXd directionOld;
+	//! Normalised version of the current direction vector
         VectorXd directionNorm;
+	//! Current force vector
         VectorXd force;
+	//! Previous force vector
         VectorXd forceOld;
     
-        int cg_i;
+        //! Counts the number of descrete steps untill algorithm convergence
+	int cg_i;
     
-        bool single_step(double maxMove);
+	//! Steps the conjugate gradient
+	/**
+ 	 *  Checks for convergence based on the change in displacement or direction	
+         */
+	bool single_step(double maxMove);
+	//! Steps the conjugate gradient
+	/**
+ 	* Checks for convergence based on the ratio of the projected force along a line
+ 	* to the norm of the total force 
+ 	*/ 
         bool line_search(double maxMove);
     
 };
