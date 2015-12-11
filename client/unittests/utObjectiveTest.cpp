1e2cac91de3a4dbb65cd253e4b4d02f519e4795f
rbwilcoxon11 <rbwilcoxon11@9d962951-508e-48a8-94b5-974f9cfda47c>
2015-12-11 06:30:25 +0000

    uploading unit-testing framework and updated makefile
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2287 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ client/unittests/utObjectiveTest.cpp
@@ -0,0 +1,69 @@
+//-----------------------------------------------------------------------------------
+//// eOn is free software: you can redistribute it and/or modify
+//// it under the terms of the GNU General Public License as published by
+//// the Free Software Foundation, either version 3 of the License, or
+//// (at your option) any later version.
+////
+//// A copy of the GNU General Public License is available at
+//// http://www.gnu.org/licenses/
+////-----------------------------------------------------------------------------------
+
+#include "utObjectiveTest.h"
+#include <iostream>
+#include <fstream>
+
+#include "utHelperFunctions.h"
+
+const std::string ObjectiveTest::HELPER_FUNCTIONS = "HelperFunctionTest";
+
+ObjectiveTest *ObjectiveTest::getObjectiveTest(std::string testType) 
+{
+	ObjectiveTest *objectiveTest=NULL;
+	if(testType==ObjectiveTest::HELPER_FUNCTIONS)
+	{
+		objectiveTest = new HelperFunctionsTest();
+	
+	}
+		
+	return objectiveTest;
+};
+
+void ObjectiveTest::pushToResults(std::string update)
+{
+
+	this->testResults.push_back(update);	
+
+}
+
+void ObjectiveTest::saveTest(std::string testName){
+	
+	std::string resultsFileName(testName);
+	resultsFileName+= ".txt";
+	std::ofstream resultsFile(resultsFileName);
+
+	resultsFile << std::endl;
+
+	if(currentStatus == 0)
+	{
+
+		resultsFile << "PASSED: all " << testName << "'s ended in success" << std::endl;
+
+	} else {
+
+		resultsFile << "FAILED: some tests in " << testName << " ended in failure" << std::endl;
+
+	}
+
+	resultsFile <<  std::endl << "---------------------------------------------------------------------";
+
+	resultsFile << std::endl << std::endl << "Detailed output: " << std::endl << std::endl;
+
+	for(const auto lineOut: this->testResults)
+		resultsFile << lineOut << std::endl;
+
+	resultsFile << std::endl;
+
+	resultsFile.close();
+
+};
+
---
+++ client/unittests/utObjectiveTest.h
@@ -0,0 +1,34 @@
+//-----------------------------------------------------------------------------------
+//// eOn is free software: you can redistribute it and/or modify
+//// it under the terms of the GNU General Public License as published by
+//// the Free Software Foundation, either version 3 of the License, or
+//// (at your option) any later version.
+////
+//// A copy of the GNU General Public License is available at
+//// http://www.gnu.org/licenses/
+////-----------------------------------------------------------------------------------
+
+#ifndef OBJECTIVE_TEST_H
+#define OBJECTIVE_TEST_H
+
+#include <vector>
+#include <string>
+
+class ObjectiveTest {
+
+	public:
+		virtual ~ObjectiveTest() {};
+		virtual int test()=0;
+                
+		std::vector<std::string> testResults;
+		int currentStatus=0;
+		void pushToResults(std::string update);
+		void saveTest(std::string testType);
+		
+		static const std::string HELPER_FUNCTIONS;
+
+		static ObjectiveTest *getObjectiveTest(std::string testType);
+
+};
+
+#endif
