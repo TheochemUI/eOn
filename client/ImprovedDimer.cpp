8de120855870ec966525532a63a1ccbab096d3eb
Graeme Henkelman <iz1ng@users.noreply.github.com>
2017-04-03 02:21:12 +0000

    change gnu license from 3 to 2, at the request of UT

--- client/ImprovedDimer.cpp
+++ client/ImprovedDimer.cpp
@@ -1,13 +1,3 @@
-//-----------------------------------------------------------------------------------
-// eOn is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// A copy of the GNU General Public License is available at
-// http://www.gnu.org/licenses/
-//-----------------------------------------------------------------------------------
-
 // An implementation of Johannes Kästner and Paul Sherwood's improved dimer.
 // An attempt to keep to the variable names in their 2008 paper has been made.
 
--- eon/escaperate.py
+++ eon/escaperate.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
 
-##-----------------------------------------------------------------------------------
-## eOn is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## A copy of the GNU General Public License is available at
-## http://www.gnu.org/licenses/
-##-----------------------------------------------------------------------------------
-
 import ConfigParser
 
 from cStringIO import StringIO
