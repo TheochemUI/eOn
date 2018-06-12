d11adb5c3c27f0c365318ca0aa2dc7b7f2f6e59f
leili <leili@9d962951-508e-48a8-94b5-974f9cfda47c>
2018-06-12 05:13:48 +0000

    update eon-state-stats.py
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2364 9d962951-508e-48a8-94b5-974f9cfda47c

--- tools/eon-state-stats.py
+++ tools/eon-state-stats.py
@@ -3,31 +3,50 @@
 import sys
 import os
 import re
+import glob
+from datetime import datetime
 
+time = datetime.strptime
 states = sys.argv[1:]
 num_states = len(sys.argv)-1
 #print "num_states: ",num_states
 
+state_listdir=[]
+current_cwd=os.getcwd()
 if(len(states) == 0):
     state_main_dir = "states/"
-    full_state_listdir = os.listdir(state_main_dir)
-    state_listdir = []
-    for dir in full_state_listdir:
-        try:
-            int(dir)
-            state_listdir.append(dir)
-        except ValueError:
-            pass
-    state_listdir.sort(key=int)
+    os.chdir(state_main_dir)
+    for f in glob.glob('*'):
+       try:
+          state_listdir.append(int(f))
+       except:
+          continue
+#    state_listdir = os.listdir(state_main_dir)
+    state_listdir.sort()
     for dir in state_listdir:
-        state_dir = state_main_dir+dir
+        state_dir = str(dir)
         if(os.path.isdir(state_dir)):
-            states.append(dir)
+            states.append(str(dir))
 
+print "#{:>11s}:{:>12s}{:>12s}{:>12s}{:>12s}{:>12s}{:>12s}{:>12s}{:>20s}{:>20s}{:>20s}".format(
+                                  "state", "num_search", "num_good", "num_unique", "frac_good", "fcalls_sad",
+                                  "fcalls_min","fcalls_dyn", "fcalls_sad_total", "fcalls_sad_bad", "fcalls_sad_abort")
+t_num_search    = 0
+t_num_good      = 0
+t_num_unique    = 0
+t_frac_good     = 0
+t_fcalls_sad    = 0
+t_fcalls_min    = 0
+t_fcalls_dyn    = 0
+t_fc_sad_tot    = 0
+t_fc_sad_abort  = 0
+t_fc_sad_bad    = 0
+
+#print ""
 for state in states:
 
-    print "\nstate:",state
-    state_dir = state_main_dir+str(state)
+    #print "\nstate:",state
+    state_dir = str(state)
 
     # read the search data for the state
     if(os.path.isdir(state_dir)):
@@ -69,17 +88,45 @@ for state in states:
             fc_sad_bad += int(line_item[4])
 
     # print stats
-    print " num search :",num_search
-    print " num good   :",num_good
-    print " num unique :",num_unique
-    if(num_search>0):   
-        print " frac good  : {0:.2f}".format(float(num_good)/float(num_search))
-    if(num_good>0):
-        print " fcalls sad : {0:.2f}".format(float(fc_sad)/float(num_good))
-        print " fcalls min : {0:.2f}".format(fc_min/num_good)
-        print " fcalls dyn : {0:.2f}".format(fc_dyn/num_good)
-        print " fcalls sad total : %d" % fc_sad_tot
-        print " fcalls sad abort : %d" % fc_sad_abort
-        print " fcalls sad bad : %d" % fc_sad_bad
+    #print " num search :",num_search
+    #print " num good   :",num_good
+    #print " num unique :",num_unique
 
+    if(num_search>0):
+        frac_good = float(num_good)/float(num_search)
+        #print " frac good  : {0:.2f}".format(float(num_good)/float(num_search))
+    if(num_good>0):
+        fcalls_sad = float(fc_sad)/float(num_good)
+        fcalls_min = fc_min/num_good
+        fcalls_dyn = fc_dyn/num_good
+        #print " fcalls sad : {0:.2f}".format(float(fc_sad)/float(num_good))
+        #print " fcalls min : {0:.2f}".format(fc_min/num_good)
+        #print " fcalls dyn : {0:.2f}".format(fc_dyn/num_good)
+        #print " fcalls sad total : %d" % fc_sad_tot
+        #print " fcalls sad abort : %d" % fc_sad_abort
+        #print " fcalls sad bad : %d" % fc_sad_bad
+    print "{:>11s}:{:>12d}{:>12d}{:>12d}{:>12.2f}{:>12.2f}{:>12.2f}{:>12.2f}{:>20d}{:>20d}{:>20d}".format(
+          state, num_search, num_good, num_unique, frac_good, fcalls_sad, fcalls_min, fcalls_dyn, fc_sad_tot, fc_sad_bad, fc_sad_abort)
+    t_num_search   += num_search
+    t_num_good     += num_good
+    t_num_unique   += num_unique
+    t_frac_good    += frac_good
+    t_fcalls_sad   += fcalls_sad
+    t_fcalls_min   += fcalls_min
+    t_fcalls_dyn   += fcalls_dyn
+    t_fc_sad_tot   += fc_sad_tot
+    t_fc_sad_abort += fc_sad_abort
+    t_fc_sad_bad   += fc_sad_bad
+
+n_s = len(states)
+os.chdir(current_cwd)
+firstline= os.popen("head -1 akmc.log").readline().split()
+lastline= os.popen("tail -1 akmc.log").readline().split()
+t1 = datetime.strptime(firstline[0]+' '+firstline[1],"%Y-%m-%d %H:%M:%S")
+t2 = datetime.strptime(lastline[0]+' '+lastline[1],"%Y-%m-%d %H:%M:%S")
+totaltime = t2-t1
+print "{:>11s}:{:>12d}{:>12d}{:>12d}{:>12.2f}{:>12.2f}{:>12.2f}{:>12.2f}{:>20d}{:>20d}{:>20d}".format(
+      "#average", t_num_search/n_s, t_num_good/n_s, t_num_unique/n_s, t_frac_good/n_s, t_fcalls_sad/n_s, 
+      t_fcalls_min/n_s, t_fcalls_dyn/n_s, t_fc_sad_tot/n_s, t_fc_sad_bad/n_s, t_fc_sad_abort/n_s)
+print "states per minute:", float(n_s)*60.00/totaltime.total_seconds()
 print ""
