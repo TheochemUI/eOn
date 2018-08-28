d7b366aec6ea455698359c62ae4d551e7512da8e
graeme <graeme@9d962951-508e-48a8-94b5-974f9cfda47c>
2018-08-28 18:03:47 +0000

    remove windows include files
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2368 9d962951-508e-48a8-94b5-974f9cfda47c

--- eon/explorer.py
+++ eon/explorer.py
@@ -69,7 +69,7 @@ class MinModeExplorer(Explorer):
         self.previous_state = previous_state
         self.comm = communicator.get_communicator()
 
-        if config.recycling_on: 
+        if config.recycling_on:
             self.nrecycled = 0
             self.recycler = recycling.Recycling(self.states,
                                            self.previous_state, 
@@ -102,7 +102,9 @@ class MinModeExplorer(Explorer):
         self.displace = displace.DisplacementManager(self.reactant, moved_atoms)
 
     def explore(self):
+        print "before client reg results"
         self.register_results()
+        print "after client reg results"
         if self.state.get_confidence(self.superbasin) < config.akmc_confidence:
             self.make_jobs()
         else:
@@ -238,15 +240,27 @@ class ClientMinModeExplorer(MinModeExplorer):
         self.job_table.write()
 
     def register_results(self):
+        print "client reg results"
         logger.info("Registering results")
+        print "before time"
         t1 = time()
+        print "after time"
         if os.path.isdir(config.path_jobs_in):
+            print "into os.path.isdir"
             try:
+                print "before shutil.rmtree"
                 shutil.rmtree(config.path_jobs_in)  
+                print "after shutil.rmtree"
             except (OSError, IOError):
+                print "exception 1"
                 pass
+        print "before if not os.path.isdir"
         if not os.path.isdir(config.path_jobs_in):
+            print "before makedirs"
+            print "path_jobs_in: ",config.path_jobs_in
             os.makedirs(config.path_jobs_in)
+            print "after makedirs"
+        print "end register results"
 
         # Function used by communicator to determine whether to discard a result
         def keep_result(name):
@@ -397,6 +411,7 @@ class ServerMinModeExplorer(MinModeExplorer):
         MinModeExplorer.explore(self)
 
     def register_results(self):
+        print "server register results"
         logger.info("Registering results")
         t1 = time()
         if os.path.isdir(config.path_jobs_in):
