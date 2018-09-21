3c47e8378dff391526f796ab41795b6b8ed834ca
Graeme Henkelman <iz1ng@users.noreply.github.com>
2018-09-21 18:30:36 +0000

    slurm submit job script

--- tools/clusters/slurm/submit_job.sh
+++ tools/clusters/slurm/submit_job.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-sbatch -A A-chgh -J "$1" -D "$2" -o ll_out -t "01:00:00" ~/code/eon/client/eonclient | awk '{print $4}'
+sbatch -A A-chgh -J "$1" -D "$2" -n 48 -N 1 -p skx-dev -o ll_out -t "01:00:00" --wrap="~/code/eon/client/eonclient" > sbatch.out
+cat sbatch.out | tail -1 | awk '{print $4}'
