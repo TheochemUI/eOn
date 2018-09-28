fd54321dcbd4ae61b9dc6c3c55f273e461685b9c
graeme <graeme@9d962951-508e-48a8-94b5-974f9cfda47c>
2018-09-28 16:49:45 +0000

    akmc-vasp-slurm example
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2380 9d962951-508e-48a8-94b5-974f9cfda47c

---
+++ examples/akmc-vasp-slurm/clean.sh
@@ -0,0 +1,2 @@
+rm -r jobs  output debug_results info.txt  jobs.tbl akmc.log   prng.pkl  superbasins states lockfile
+rm -r kdb.db  kdbscratch
---
+++ examples/akmc-vasp-slurm/submit_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+sbatch \
+	--job-name="$1" \
+	--output=eonclient_stdout.log \
+	--qos=debug \
+	--nodes=1 \
+	--time=0:10:00 \
+	-C haswell \
+	--account=account_name \
+	--mail-type=ALL \
+	--mail-user=your@email \
+	--chdir="$2" \
+	client_script.sh | awk '{print $4}'
+#	~/software/eon/bin/eonclient | awk '{print $4}'
+#	module load gcc; module load vasp/5.4.1_vtst-gcc; module load ase; ~/software/eon/bin/eonclient | awk '{print $4}'
+#	module load python/2.7-anaconda-5.2 vasp/5.4.1_vtst ase
+
