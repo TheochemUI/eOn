d3f5cab98f2aaac676458300ce5d27ff14f77565
(no author) <(no author)@9d962951-508e-48a8-94b5-974f9cfda47c>
2020-10-15 14:20:54 +0000

    update pyamff potential to call prep once
    git-svn-id: http://theory.cm.utexas.edu/svn/eon@2462 9d962951-508e-48a8-94b5-974f9cfda47c

--- client/potentials/PyAMFF/PyAMFF.cpp
+++ client/potentials/PyAMFF/PyAMFF.cpp
@@ -14,15 +14,34 @@
 
 extern "C" void calc_eon(long *nAtoms, const double [], const double [], const int [], double [], double *U, int *num_elements, int []);
 
+extern "C" void read_mlffParas(long *nAtoms, int *num_elements, int *max_fps, const int [], int []);
+
+extern "C" void cleanup();
+
+extern "C" void nncleanup();
+
+extern "C" void prepfNN( long *nAtoms, int *num_elements, int *max_fps, const int [], int []);
 
 PyAMFF::PyAMFF(void)
 {
+    new_pyamff = true; 
     return;
 }
 
+PyAMFF::~PyAMFF(){
+    cleanMemory();
+}
 
 void PyAMFF::cleanMemory(void)
 {
+    if(new_pyamff != true ){
+        new_pyamff = true;
+        //also call NN cleanup
+        cleanup();
+        cout << "fp cleaned up" << endl;
+        nncleanup();
+        cout << "nn cleaned up" << endl;
+    }
     return;
 }
 
@@ -42,6 +61,10 @@ void PyAMFF::force(long N, const double *R, const int *atomicNrs, double *F,
 //        cout << symbols[i] << endl;
 //        cout << i << endl;
 //    }
+
+            
+    int max_fps;
+    max_fps = 100;
     vector<int> unique_atomicNrs;
     for (int i=0; i<N; i++)
     {
@@ -55,9 +78,19 @@ void PyAMFF::force(long N, const double *R, const int *atomicNrs, double *F,
 
     int num_elements;
     num_elements = unique_atomicNrs.size();
+    //cout << "num_unique_elements" << num_elements << endl;
     int unique[num_elements];
     copy(unique_atomicNrs.begin(),unique_atomicNrs.end(),unique);
     
+
+    if (new_pyamff == true){
+        cout << "reading mlff in c" << endl;
+        read_mlffParas(&N, &num_elements, &max_fps, atomicNrs, unique);
+        cout << "prepping fnn in c" << endl;
+        prepfNN(&N, &num_elements, &max_fps, atomicNrs, unique);
+        cout << "fnn prepped!" << endl;
+    }
+    new_pyamff = false;
     calc_eon(&N, R, box, atomicNrs, F, U, &num_elements, unique);
     
 
