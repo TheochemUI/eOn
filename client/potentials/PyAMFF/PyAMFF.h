7dd18f0ddba5dd11e8ed375e5c2b0b377549586d
(no author) <na@NODATA>
2020-11-20 21:23:00 +0000

    Update pyamff

--- client/potentials/PyAMFF/PyAMFF.h
+++ client/potentials/PyAMFF/PyAMFF.h
@@ -23,11 +23,12 @@ class PyAMFF : public Potential
 
     public:
         PyAMFF(void);
-//		~FAMFF();
+		~PyAMFF();
 		void initialize() {};
 		void cleanMemory(void);
         void force(long N, const double *R, const int *atomicNrs, 
                    double *F, double *U, const double *box);
-};
+        bool new_pyamff;
+};     
 #endif
 
