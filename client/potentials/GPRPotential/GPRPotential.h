b5f57aa4b78651e436746dafd70bedb5ddc11bae
Maxim <maxim.vadimovich@gmail.com>
2021-01-29 11:40:10 +0100

    Added a basic interface class for gpr_dimer

---
+++ client/potentials/GPRPotential/GPRPotential.h
@@ -0,0 +1,37 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//-----------------------------------------------------------------------------------
+
+#ifndef NEWPOT_INTERFACE
+#define NEWPOT_INTERFACE
+
+#include "../../Potential.h"
+#include "../../../gpr_dimer/gpr/ml/GaussianProcessRegression.h"
+
+/** Template to use if user want to provide potential. */
+class GPRPotential : public Potential{
+
+private:
+    GaussianProcessRegression *gpr_model;
+
+public:
+// Functions
+	// constructor and destructor
+    GPRPotential(Parameters *p);
+
+    void registerGPRObject(GaussianProcessRegression *_gpr_model);
+
+    // To satisfy interface
+    void initialize(void);    
+    void cleanMemory(void);    
+
+    void force(long N, const double *R, const int *atomicNrs, double *F, double *U, const double *box);
+};
+#endif
+
