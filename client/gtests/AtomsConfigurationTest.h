5727c00e84980a0f5aa11fb00a56b62dbd45db07
Rohit Goswami <rgoswami@ieee.org>
2021-02-04 10:55:56 +0000

    gtests: Init with AtomsConfiguration

---
+++ client/gtests/AtomsConfigurationTest.h
@@ -0,0 +1,14 @@
+#ifndef ATOMSCONFIGURATIONTEST_H
+#define ATOMSCONFIGURATIONTEST_H
+
+#include <gtest/gtest.h>
+
+namespace tests {
+    class AtomsConfigurationTest : public ::testing::Test {
+        public:
+            AtomsConfigurationTest();
+            virtual ~AtomsConfigurationTest();
+    };
+} /* namespace tests */
+
+#endif /* ATOMSCONFIGURATIONTEST_H */
