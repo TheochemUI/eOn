82b3b1bbd094ff19ad72917ff0f29a83a6618f02
Rohit Goswami <rgoswami@ieee.org>
2021-02-05 12:43:53 +0000

    obs: Add untested helper

---
+++ client/gtests/ObsTest.h
@@ -0,0 +1,15 @@
+#ifndef OBSTEST_H
+#define OBSTEST_H
+
+#include <gtest/gtest.h>
+
+namespace tests {
+    class ObsTest : public ::testing::Test {
+        public:
+            ObsTest();
+            virtual ~ObsTest();
+    };
+} /* namespace tests */
+
+
+#endif /* OBSTEST_H */
