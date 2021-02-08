5085d890a7205f0d38873efe17b2455773efdbcf
Rohit Goswami <rgoswami@ieee.org>
2021-02-08 08:26:54 +0000

    gtests: Kill tests, more skeletons

---
+++ client/gtests/GPRDimerTest.h
@@ -0,0 +1,15 @@
+#ifndef GPRDIMERTEST_H
+#define GPRDIMERTEST_H
+
+#include <gtest/gtest.h>
+
+namespace tests {
+    class GPRDimerTest : public ::testing::Test {
+        public:
+            GPRDimerTest();
+            virtual ~GPRDimerTest();
+    };
+} /* namespace tests */
+
+
+#endif /* GPRDIMERTEST_H */
