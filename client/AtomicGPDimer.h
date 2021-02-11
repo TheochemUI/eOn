0b429ff426f11efbf6e7886fadb2879818b6902a
Rohit Goswami <rgoswami@ieee.org>
2021-02-11 10:13:50 +0000

    atomicGPDimer: Massive refactor and clean up
    Also kill spurious image and fix the size of orient

--- client/AtomicGPDimer.h
+++ client/AtomicGPDimer.h
@@ -2,13 +2,12 @@
 #define ATOMICGPDIMER_H
 
 #include "Eigen.h"
+#include "LowestEigenmode.h"
 #include "Matter.h"
 #include "Parameters.h"
-#include "LowestEigenmode.h"
 #include <vector>
 
 #include "gprdimer/gpr/AtomicDimer.h"
-
 
 #include "gprdimer/gpr/Enums.h"
 #include "gprdimer/gpr/auxiliary/ProblemSetUp.h"
@@ -18,33 +17,32 @@
 #include "gprdimer/managers/io/FileManager.h"
 
 // dimer method to find the lowest curvature mode
-class AtomicGPDimer : public LowestEigenmode
-{
+class AtomicGPDimer : public LowestEigenmode {
 
-    public:
+public:
+  // Optimization for the dimer
+  static const char OPT_SCG[];
+  static const char OPT_LBFGS[];
 
-    // Optimization for the dimer
-    static const char OPT_SCG[];
-    static const char OPT_LBFGS[];
+  AtomicGPDimer(Matter *matter, Parameters *parameters);
+  ~AtomicGPDimer();
 
-    AtomicGPDimer(Matter *matter, Parameters *parameters);
-    ~AtomicGPDimer();
-
-    void compute(Matter *matter, AtomMatrix initialDirectionMatrix);
-    double getEigenvalue();
-    AtomMatrix getEigenvector();
+  void compute(Matter *matter, AtomMatrix initialDirectionMatrix);
+  double getEigenvalue();
+  AtomMatrix getEigenvector();
 
 private:
+  Matter *matterCenter;       // initial center of the dimer
+  AtomMatrix direction;       // direction along the dimer
+  AtomMatrix rotationalPlane; // direction normal to the plane of dimer rotation
+  Parameters *parameters;
 
-    Matter *matterCenter; // center of the dimer
-    Matter *matterDimer; //one configuration of the dimer
-    AtomMatrix direction; // direction along the dimer
-    AtomMatrix rotationalPlane; // direction normal to the plane of dimer rotation
-    Parameters *parameters;
-
-    InputParameters p;
-    atmd::AtomicDimer atomic_dimer;
-    aux::ProblemSetUp problem_setup;
+  InputParameters p;
+  atmd::AtomicDimer atomic_dimer;
+  aux::ProblemSetUp problem_setup;
+  AtomsConfiguration atoms_config;
+  Observation init_observations, init_middle_point;
+  gpr::Coord orient_init, R_init;
 };
 
 #endif
