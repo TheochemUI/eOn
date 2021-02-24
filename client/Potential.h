d28f76b76e558acde91a9f626724220181fbca1a
apenat <apenat.un@gmail.com>
2021-02-24 16:48:56 +0000

    ugly interface w/AMS

--- client/Potential.h
+++ client/Potential.h
@@ -36,6 +36,7 @@ class Potential
         static const char POT_EXT[];
         static const char POT_NEW[];
         static const char POT_PYAMFF[];
+        static const char POT_AMS[];
 
         static Potential* getPotential(Parameters *parameters);
 
