33171be98fd2c9fcb278c47f7a6259126b73a154
Rohit Goswami <rog32@hi.is>
2021-04-26 15:35:25 +0000

    tests: Try another akmc approach

--- tests/test_akmc_pt.py
+++ tests/test_akmc_pt.py
@@ -4,11 +4,11 @@ import pytest
 from pathlib import Path
 
 p = Path(str(sh.pwd())) # Hacky way to get project root
-new_env = os.environ.copy()
-new_env["PATH"] += os.pathsep + str(p).strip()+"/bin"
-new_env["PYTHONPATH"] += os.pathsep + str(p).strip()+"/eon"
+# new_env = os.environ.copy()
+# new_env["PATH"] += os.pathsep + str(p).strip()+"/bin"
+# new_env["PYTHONPATH"] += os.pathsep + str(p).strip()+"/eon"
 eonclient = sh.Command(str(p).strip()+"/client/build/eonclient")
-eon = sh.Command(str(p).strip()+"/bin/eon")
+# eon = sh.Command(str(p).strip()+"/bin/eon")
 
 def test_akmc_pt_morse_dimer(datadir,shared_datadir):
     ddir=f"{shared_datadir}/server/Pt_Heptamer_oneLayer"
@@ -17,7 +17,7 @@ def test_akmc_pt_morse_dimer(datadir,shared_datadir):
     files = sh.ls()
     diff = set(files.split()) ^ {'config.ini', 'pos.con'}
     assert not diff
-    eon(_env=new_env) # Runs eon
-    eon(_env=new_env) # Runs eon
-    eon(_env=new_env) # Runs eon
+    sh.eon()
+    sh.eon()
+    sh.eon()
     # TODO: Actually parse an output
