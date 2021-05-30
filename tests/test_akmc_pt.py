bf2a21bb7e5ef9387db9f43e8994150049f14765
Rohit Goswami <rog32@hi.is>
2021-05-30 19:55:11 +0000

    tests: Assume system-wide install

--- tests/test_akmc_pt.py
+++ tests/test_akmc_pt.py
@@ -7,7 +7,8 @@ p = Path(str(sh.pwd())) # Hacky way to get project root
 # new_env = os.environ.copy()
 # new_env["PATH"] += os.pathsep + str(p).strip()+"/bin"
 # new_env["PYTHONPATH"] += os.pathsep + str(p).strip()+"/eon"
-eonclient = sh.Command(str(p).strip()+"/client/build/eonclient")
+# eonclient = sh.Command(str(p).strip()+"/client/build/eonclient")
+eonclient = sh.eonclient
 # eon = sh.Command(str(p).strip()+"/bin/eon")
 
 def test_akmc_pt_morse_dimer(datadir,shared_datadir):
