45dd285a0e5677d341c85f1f0038e4ce6bf8f910
Rohit Goswami <rog32@hi.is>
2021-05-30 20:19:35 +0000

    version: Update

--- client/version.sh
+++ client/version.sh
@@ -1,4 +1,9 @@
+# Todo, handle this more gracefully
 VERSION=$(svnversion 2> /dev/null)
+if [ -z "$VERSION" ]; then
+    VERSION=$(git rev-parse --short HEAD 2> /dev/null)
+fi
+
 
 if [ -z "$VERSION" ]; then
     VERSION="unknown"
