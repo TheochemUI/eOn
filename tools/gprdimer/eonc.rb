e0b4c0a36881be5a2c717eea41e78b7b18d14af9
Rohit Goswami <rog32@hi.is>
2021-12-16 02:52:08 +0000

    ENH,DOC: Timestamped directories, docs [eonc.rb]

--- tools/gprdimer/eonc.rb
+++ tools/gprdimer/eonc.rb
@@ -1,5 +1,32 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby -w
 # frozen_string_literal: true
+
+# Wraps eonclient in a friendly helper. Generates timestamped output folders,
+# and a prompt driven customization routine.
+#
+# Usage examples:
+#
+# To run a standard GPRD calculation:
+#
+#   eonc.rb # Then select the right options
+#
+# Importantly, this is meant to be run with a "fat" configuration file, along with the three required files:
+# [1] post.con
+# [2] displacement.con
+# [3] direction.dat
+#
+# The "fat" configuration file essentially must be called config.ini
+# Furthermore, the following must be present **as written**!!
+# - use_dropout = false
+# - start_dropout_at
+# - ndropout_vals
+# - dropout_threshold
+#
+# This is because regular expressions are used to ensure the correct configuration is set up.
+#
+# TODO: Add a config reader, replace regexps
+# TODO: Add templates to remove the requirement for the config
+# TODO: Add a classic CLI with option parsing as well
 
 require 'tty-command' # For the Command
 require 'tty-prompt' # For the minmodemethod choice
@@ -35,38 +62,55 @@ if minmodemethod.downcase == 'gprdimer'
 end
 run_eonclient = prompt.yes?('Run eonclient?')
 
+# Prepare output string
+out_root = if minmodemethod.downcase == 'gprdimer'
+             "#{minmodemethod.downcase}_prune_#{prune_status}"
+           else
+             minmodemethod.downcase.to_s
+           end
+
+# We would like to ideally run every one of these in tandem, all at once without
+# having to worry about overwrites, this means timestamping a folder and moving
+# to it. We use the seconds since epoch for better granularity.
+# TODO: This way we can even make a pretty README
+# HACK: Might not be the most elegant approach. Consider when multiple of these
+# are started concurrently
+puts 'Moving one level down'
+mytime = Time.now.to_f
+orig_dir = Dir.pwd
+lowered_dir = "#{orig_dir}/#{mytime}_#{out_root}"
+Dir.mkdir(lowered_dir)
+FileUtils.cp %w[direction.dat displacement.con pos.con config.ini], lowered_dir.to_s
+
+# HACK: I do not like this, the actual shift is jarring
+Dir.chdir(lowered_dir)
+puts Dir.pwd
+
 # Unconditional
 replace_value_conf('min_mode_method', minmodemethod.downcase)
 replace_value_conf('use_dropout', prune_status)
 prune_sched.each { |key, value| replace_value_conf(key, value) } if prune_status == true
 
-# Generate output string
-out_root = if minmodemethod.downcase == 'gprdimer'
-             "./#{minmodemethod.downcase}_prune_#{prune_status}"
-           else
-             "./#{minmodemethod.downcase}"
-           end
-
 # Run
 if run_eonclient == false
-  puts "#{out_root}_stdout"
-  puts "#{out_root}_stderr"
-  cmd = TTY::Command.new(printer: :pretty)
-  TTY::File.tail_file('config.ini', 15) do |line|
+  puts "Writing to #{out_root}_stdout and #{out_root}_stderr"
+  cmd = TTY::Command.new(printer: :pretty, dry_run: true)
+  TTY::File.tail_file('config.ini') do |line|
     puts line
   end
   cmd.run('eonclient')
 else
+  cmd = TTY::Command.new(printer: :pretty)
   out, err = cmd.run('eonclient')
-  File.write("#{out_root}_stdout", out)
-  File.write("#{out_root}_stderr", err)
+  File.write("./#{out_root}_stdout", out)
+  File.write("./#{out_root}_stderr", err)
 
   # Cleanup
   # Move generated files around
-  File.rename 'client.log', "#{out_root}_client.log"
-  File.rename 'mode.dat', "#{out_root}_mode.dat"
-  File.rename 'results.dat', "#{out_root}_results.dat"
-  File.rename 'saddle.con', "#{out_root}_saddle.con"
+  File.rename 'client.log', "./#{out_root}_client.log"
+  File.rename 'mode.dat', "./#{out_root}_mode.dat"
+  File.rename 'results.dat', "./#{out_root}_results.dat"
+  File.rename 'saddle.con', "./#{out_root}_saddle.con"
 
   # TODO: Fixup
   File.delete('runAMS.sh') if File.exist?('runAMS.sh')
