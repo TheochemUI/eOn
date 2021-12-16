84870800f29bf043f702bbae6fda9a1706dff158
Rohit Goswami <rog32@hi.is>
2021-12-16 01:58:45 +0000

    ENH: Support more options with eonc.rb

--- tools/gprdimer/eonc.rb
+++ tools/gprdimer/eonc.rb
@@ -1,37 +1,78 @@
 #!/usr/bin/env ruby
 
 require "tty-command" # For the Command
-require "tty-prompt" # For the method choice
+require "tty-prompt" # For the minmodemethod choice
 require "tty-file" # For the replacement
+require "solid_assert" # Need assert
 
-prompt = TTY::Prompt.new
-a = prompt.select("Choose the method", %w(GPRDimer Dimer))
-
-# TODO: Super fragile, update
-re = TTY::File.replace_in_file "config.ini", /(?<=min_mode_method = ).*$/, a.downcase
-if re==false
-  # Couldn't replace, exit
-  exit
+def replace_value_conf(input_key, new_value)
+  # TODO: Super fragile, update
+  re = TTY::File.replace_in_file "config.ini", /(?<=#{input_key} = ).*$/, "#{new_value}"
+  assert re == true # Can't fail
 end
 
-# Run eonclient
-cmd = TTY::Command.new(printer: :pretty)
-out, err = cmd.run("eonclient")
-File.write("./%s_stdout" % [a.downcase], out)
-File.write("./%s_stderr" % [a.downcase], err)
+prompt = TTY::Prompt.new
 
-# Cleanup
-# Move generated files around
-File.rename "client.log", "./%s_client.log" % [a.downcase]
-File.rename "mode.dat", "./%s_mode.dat" % [a.downcase]
-File.rename "results.dat", "./%s_results.dat" % [a.downcase]
-File.rename "saddle.con", "./%s_saddle.con" % [a.downcase]
+# Populate prompt
+minmodemethod = prompt.select("Choose the minmodemethod", %w(GPRDimer Dimer))
+if minmodemethod.downcase == "gprdimer"
+  prune_status = prompt.yes?("With pruning?", default: false)
+  if prune_status == true
+    prune_defaults = prompt.yes?("Use the defaults?\n This is the removal of 4 elements greater than 0.5 starting from size 10", default: true)
+    if prune_defaults == true
+      prune_sched = {'start_dropout_at' => 10, 'ndropout_vals' => 4, 'dropout_threshold' => 0.5}
+    else # User needs to provide the values
+      prune_sched = prompt.collect do
+        key('start_dropout_at').ask("Initial dropout size at?", convert: :int)
+        key('ndropout_vals').ask("How many values are to be dropped per thinning round?", convert: :int)
+        key('dropout_threshold').ask("Initial highest allowed force value?", convert: :float)
+    end
+    end
+  end
+end
+run_eonclient = prompt.yes?("Run eonclient?")
 
-# TODO: Fixup
-File.delete("runAMS.sh") if File.exist?("runAMS.sh")
-File.delete("updCoord.sh") if File.exist?("updCoord.sh")
-File.delete("myrestart.in.sh") if File.exist?("myrestart.in.sh")
+# Unconditional
+replace_value_conf("min_mode_method", minmodemethod.downcase)
+replace_value_conf("use_dropout", prune_status)
+if prune_status == true
+  prune_sched.each {|key, value| replace_value_conf(key, value) }
+end
 
-FileUtils.remove_dir("firstRun.results") if File.directory?("firstRun.results")
-FileUtils.remove_dir("secondRun.results") if File.directory?("secondRun.results")
-FileUtils.remove_dir("ams.results") if File.directory?("ams.results")
+# Generate output string
+if  minmodemethod.downcase == "gprdimer"
+  out_root = "./#{minmodemethod.downcase}_prune_#{prune_status}"
+else
+  out_root = "./#{minmodemethod.downcase}"
+end
+
+# Run
+if run_eonclient == false
+  puts "#{out_root}_stdout"
+  puts "#{out_root}_stderr"
+  cmd = TTY::Command.new(printer: :pretty)
+  TTY::File.tail_file("config.ini", 15) do |line|
+    puts line
+  end
+else
+  out, err = cmd.run("eonclient")
+  File.write("#{out_root}_stdout", out)
+  File.write("#{out_root}_stderr", err)
+
+  # Cleanup
+  # Move generated files around
+  File.rename "client.log", "#{out_root}_client.log"
+  File.rename "mode.dat", "#{out_root}_mode.dat"
+  File.rename "results.dat", "#{out_root}_results.dat"
+  File.rename "saddle.con", "#{out_root}_saddle.con"
+
+  # TODO: Fixup
+  File.delete("runAMS.sh") if File.exist?("runAMS.sh")
+  File.delete("updCoord.sh") if File.exist?("updCoord.sh")
+  File.delete("myrestart.in.sh") if File.exist?("myrestart.in.sh")
+
+  FileUtils.remove_dir("firstRun.results") if File.directory?("firstRun.results")
+  FileUtils.remove_dir("secondRun.results") if File.directory?("secondRun.results")
+  FileUtils.remove_dir("ams.results") if File.directory?("ams.results")
+end
+
