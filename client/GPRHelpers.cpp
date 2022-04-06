e39316d71008f9eb19df70b52a3817e69ebfba74
Rohit Goswami <rog32@hi.is>
2022-04-06 11:16:33 +0000

    ENH: Handover for gpr potentials

--- client/GPRHelpers.cpp
+++ client/GPRHelpers.cpp
@@ -242,6 +242,11 @@ gpr::Observation helper_functions::eon_matter_to_init_obs(Matter *matter) {
 }
 
 std::pair<double, AtomMatrix> helper_functions::energy_and_forces(Matter *matter, Potential *pot){
+  if (pot->getName()=="gpr_pot"s) {
+    // Sanity checks
+    auto gppot = static_cast<GPRPotential*>(pot);
+    return helper_functions::gpr_energy_and_forces(matter, gppot);
+  }
   int nAtoms = matter->numberOfAtoms();
   auto posdata = matter->getPositions();
   auto celldat = matter->getCell();
