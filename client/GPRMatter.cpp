e32f616bc0e11a4debbe53f9cb2fe5ce6ce98f10
Rohit Goswami <rog32@hi.is>
2022-04-17 00:12:51 +0000

    ENH: Update to work convergence

--- client/GPRMatter.cpp
+++ client/GPRMatter.cpp
@@ -4,7 +4,7 @@ GPRMatter::~GPRMatter(){
 
 };
 
-GPRMatter::GPRMatter(Matter initMatter) : truePotMatter{initMatter} {
+GPRMatter::GPRMatter(Matter initMatter) : truePotMatter{initMatter}  {
     this->eonp = *this->truePotMatter.getParameters();
     // Setup AtomsConf
     auto conf_data = helper_functions::eon_matter_to_frozen_conf_info(&this->truePotMatter,
@@ -17,5 +17,37 @@ GPRMatter::GPRMatter(Matter initMatter) : truePotMatter{initMatter} {
     for (int i = 0; i < 9; i++) {
         potparams.cell_dimensions.value[i] = this->truePotMatter.getCell()(i);
     }
+    this->trainedGPR = std::make_unique<GPRPotential>(&this->eonp);
     gprfunc.initialize(potparams, this->atmconf);
+}
+
+void GPRMatter::trainGPR(gpr::Observation obspath){
+    this->gprfunc.setHyperparameters(obspath, this->atmconf);
+    this->gprfunc.optimize(obspath);
+    this->trainedGPR->registerGPRObject(&this->gprfunc);
+}
+
+std::pair<double, AtomMatrix> GPRMatter::gpr_energy_forces(Matter& getat){
+  int nFreeAtoms = getat.numberOfFreeAtoms();
+  auto posdata = getat.getPositionsFree();
+  auto celldat = getat.getCell();
+  AtomMatrix forces = AtomMatrix::Constant(nFreeAtoms, 3, 0);
+  double *pos = posdata.data();
+  double *frcs = forces.data();
+  double *bx = celldat.data();
+  double energy{0};
+  double *erg = &energy;
+  this->trainedGPR->force(nFreeAtoms, pos, nullptr, frcs, erg, bx, 1);
+  return std::make_pair(energy, forces);
+}
+
+std::pair<double, AtomMatrix> GPRMatter::true_free_energy_forces(Matter& getat){
+  return std::make_pair(getat.getPotentialEnergy(), getat.getForcesFree());
+}
+
+bool GPRMatter::isCloseTo(Matter& testat, double eps){
+    // TODO: Use more convergence methods
+    double convval {((gpr_energy_forces(testat)).second
+    - testat.getForcesFree()).norm()};
+    return (convval < eps);
 }
--- client/GPRMatter.h
+++ client/GPRMatter.h
@@ -16,7 +16,12 @@ class GPRMatter {
         gpr::AtomsConfiguration atmconf;
         gpr::GPRSetup gpr_parameters;
         gpr::GaussianProcessRegression gprfunc;
+        std::unique_ptr<GPRPotential> trainedGPR;
     public:
         GPRMatter(Matter initMatter);
         ~GPRMatter(); // Destructor
+        void trainGPR(gpr::Observation obspath);
+        std::pair<double, AtomMatrix> gpr_energy_forces(Matter& getat);
+        std::pair<double, AtomMatrix> true_free_energy_forces(Matter& getat);
+        bool isCloseTo(Matter& testat, double eps = 1e-6);
 };
