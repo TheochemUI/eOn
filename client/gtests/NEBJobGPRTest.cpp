12b6d3b8aadcf6044f5e2de7925c2777d523ca9e
Rohit Goswami <rog32@hi.is>
2022-06-16 10:32:28 +0000

    TST,TMP: Rework and re-enable NEB GPRPot test

--- client/gtests/NEBJobGPRTest.cpp
+++ client/gtests/NEBJobGPRTest.cpp
@@ -6,102 +6,107 @@
  *     Company: University of Iceland
  */
 
-#include <algorithm>
-
-#include "../Potential.h"
-#include "../potentials/Morse/Morse.h"
-#include "../NudgedElasticBandJob.h"
-
-#include "../HelperFunctions.h"
-#include "../GPRHelpers.h"
-#include "../Log.h"
-#include "../Matter.h"
-#include "../Parameters.h"
 #include "NEBJobGPRTest.h"
 
 namespace tests {
 
 NEBJobGPRTest::NEBJobGPRTest() {
-    reactantFilename = helper_functions::getRelevantFile("reactant.con");
-    productFilename = helper_functions::getRelevantFile("product.con");
+  this->reactantFilename = helper_functions::getRelevantFile("reactant.con");
+  this->productFilename = helper_functions::getRelevantFile("product.con");
 
-    parameters = std::make_unique<Parameters>();
-    parameters->potential = "morse_pt";
-    parameters->nebImages = 7;
-    parameters->LogPotential = false;
-    log_init(parameters.get(), (char *)"test.log");
+  this->parameters = std::make_unique<Parameters>();
+  this->parameters->potential = "morse_pt";
+  this->parameters->nebImages = 7;
+  this->parameters->LogPotential = false;
+  log_init(parameters.get(), (char *)"test.log");
 
-    gprparameon = std::make_unique<Parameters>();
-    gprparameon->potential = "gpr_pot";
-    gprparameon->nebImages = 7;
-    gprparameon->LogPotential = false;
+  this->initmatter = std::make_unique<Matter>(parameters.get());
+  this->finalmatter = std::make_unique<Matter>(parameters.get());
 
-    initmatter = std::make_unique<Matter>(parameters.get());
-    finalmatter = std::make_unique<Matter>(parameters.get());
+  this->initmatter->con2matter(reactantFilename);
+  this->finalmatter->con2matter(productFilename);
 
-    initmatter->con2matter(reactantFilename);
-    finalmatter->con2matter(productFilename);
+  this->gprfunc = std::make_unique<gpr::GaussianProcessRegression>();
 
-    gprfunc = std::make_unique<gpr::GaussianProcessRegression>();
+  // Constants
+  this->init_eref = this->initmatter.get()->getPotentialEnergy();
+  this->init_frcsref = this->initmatter.get()->getForcesFree();
+  this->config_data = helper_functions::eon_matter_to_frozen_conf_info(
+      this->initmatter.get(), 5);
+  auto atoms_config = std::get<gpr::AtomsConfiguration>(config_data);
+  auto R_init = std::get<gpr::Coord>(config_data);
+  // Setup the observations
+  this->imgArray = helper_functions::prepInitialPath(this->parameters.get());
+  this->obspath = helper_functions::prepInitialObs(imgArray);
+  // Setup GPR
+  auto eondat = std::make_pair(*this->parameters, *this->initmatter);
+  *this->gprfunc = helper_functions::initializeGPR(*this->gprfunc, atoms_config,
+                                                   obspath, eondat);
+  this->gprfunc->setHyperparameters(obspath, atoms_config);
+  this->gprfunc->optimize(obspath);
+  // Comparer
+  this->comparer = [&](const gpr::EigenMatrix &lhs,
+                       const gpr::EigenMatrix &rhs) -> bool {
+    return lhs.isApprox(rhs, 1e-2); // FIXME: lowered because of the gradients
+  };
 }
 
-NEBJobGPRTest::~NEBJobGPRTest() {
-}
+NEBJobGPRTest::~NEBJobGPRTest() {}
 
-// TEST_F(NEBJobGPRTest, TestMatter) {
-//   const auto init_eref = this->initmatter.get()->getPotentialEnergy(); // Surprisingly still needed, buggy?
-//   const auto init_frcsref = this->initmatter.get()->getForcesFree();
-//   auto config_data = helper_functions::eon_matter_to_frozen_conf_info(this->initmatter.get(),  2);
-//   auto atoms_config = std::get<gpr::AtomsConfiguration>(config_data);
-//   Parameters eonp = *this->parameters;
-//   // Setup the run
-//   auto imgArray = helper_functions::prepInitialPath(this->parameters.get());
-//   auto obspath = helper_functions::prepInitialObs(imgArray);
-//   // Setup GPR
-//   auto eondat = std::make_pair(eonp, *this->initmatter);
-//   *this->gprfunc = helper_functions::initializeGPR(*this->gprfunc, atoms_config, obspath, eondat);
-//   this->gprfunc->setHyperparameters(obspath, atoms_config);
-//   this->gprfunc->optimize(obspath);
-//   // Prepare GPR potential
-//   GPRPotential gprpot{this->gprparameon.get()};
-//   gprpot.registerGPRObject(this->gprfunc.get());
-//   // Setup matter objects
-//   auto matterOne = std::make_unique<Matter>(this->gprparameon.get());
-//   matterOne->con2matter(this->reactantFilename);
-//   matterOne->setPotential(&gprpot);
-//   auto matterFin = std::make_unique<Matter>(this->gprparameon.get());
-//   matterFin->con2matter(this->productFilename);
-//   matterFin->setPotential(&gprpot);
-//   auto matterTest = std::make_unique<Matter>(this->gprparameon.get());
-//   matterTest->con2matter(this->productFilename);
-//   matterTest->setPositions((matterOne->getPositions()*1.01+matterFin->getPositions()*0.03) / 2);
-//   matterTest->setPotential(&gprpot);
-//   double blah = matterTest->getPotentialEnergy();
-//   std::cout<<matterTest->getPotentialEnergy()<<" Matter at Test point\n";
-//   // RUN NEB!!!
-//   NudgedElasticBand *neb = new NudgedElasticBand(matterOne.get(), matterFin.get(), this->gprparameon.get());
-//   neb->compute();
-//   bool mustUpdate = helper_functions::maybeUpdateObs(*neb, obspath, eonp);
-//   this->gprfunc->setHyperparameters(obspath, atoms_config, false);
-//   this->gprfunc->optimize(obspath);
-//   gprpot.registerGPRObject(this->gprfunc.get());
-//   matterTest->setPotential(&gprpot);
-//   ASSERT_NE(blah, matterTest->getPotentialEnergy())<<"Energy not changed after updating gpr";
-//   while(mustUpdate){
-//     this->gprfunc->setHyperparameters(obspath, atoms_config, false);
-//     this->gprfunc->optimize(obspath);
-//     auto nebTwo = helper_functions::prepGPRNEBround(*this->gprfunc,
-//                                                 *matterOne, *matterFin,
-//                                                 *this->gprparameon);
-//     nebTwo->compute();
-//     mustUpdate = helper_functions::maybeUpdateObs(*nebTwo, obspath, eonp);
-//   };
-//   // Final round
-//     auto nebFin = helper_functions::prepGPRNEBround(*this->gprfunc,
-//                                                 *matterOne, *matterFin,
-//                                                 *this->gprparameon);
-//     nebFin->compute();
-//     delete neb;
-// }
+TEST_F(NEBJobGPRTest, TestMatter) {
+  // Prepare GPR potential
+  auto gprpot = new GPRPotential {this->parameters.get()};
+  gprpot->registerGPRObject(this->gprfunc.get());
+  // // Setup matter objects
+  auto matterOne = std::make_unique<Matter>(this->parameters.get());
+  matterOne->con2matter(this->reactantFilename);
+  matterOne->setPotential(gprpot);
+  auto matterFin = std::make_unique<Matter>(this->parameters.get());
+  matterFin->con2matter(this->productFilename);
+  matterFin->setPotential(gprpot);
+  auto matterTest = std::make_unique<Matter>(this->parameters.get());
+  matterTest->con2matter(this->productFilename);
+  matterTest->setPositions(
+      (matterOne->getPositions() * 1.01 + matterFin->getPositions() * 0.03) /
+      2);
+  matterTest->setPotential(gprpot);
+  double blah = matterTest->getPotentialEnergy();
+  // RUN NEB!!!
+  NudgedElasticBand *neb = new NudgedElasticBand(
+      matterOne.get(), matterFin.get(), this->parameters.get());
+  neb->compute();
+  for (long idx {0}; idx <= neb->images+1; idx++){// excludes final, initial
+    neb->image[idx]->setPotential(gprpot);
+    // IC(idx, neb->image[idx]->getPotential()->getName());
+  }
+  obspath.printSizes();
+  bool mustUpdate = helper_functions::maybeUpdateObs(*neb, obspath, *this->parameters);
+  // obspath.printSizes();
+  // this->gprfunc->setHyperparameters(obspath, atoms_conf, false);
+  this->gprfunc->optimize(obspath);
+  gprpot->registerGPRObject(this->gprfunc.get());
+  matterTest->setPotential(gprpot);
+  IC(mustUpdate, blah, matterTest->getPotentialEnergy());
+  ASSERT_NE(blah, matterTest->getPotentialEnergy())
+      << "Energy not changed after updating gpr";
+  while(mustUpdate){
+    // this->gprfunc->setHyperparameters(obspath, atoms_conf, false);
+    this->gprfunc->optimize(obspath);
+    auto nebTwo = helper_functions::prepGPRNEBround(*gprpot,
+                                                *matterOne, *matterFin,
+                                                *this->parameters);
+    nebTwo->compute();
+    mustUpdate = helper_functions::maybeUpdateObs(*nebTwo, obspath, *this->parameters);
+    gprpot->registerGPRObject(this->gprfunc.get());
+    matterTest->setPotential(gprpot);
+    IC(mustUpdate, blah, matterTest->getPotentialEnergy());
+  };
+  // // Final round
+  //   auto nebFin = helper_functions::prepGPRNEBround(*this->gprfunc,
+  //                                               *matterOne, *matterFin,
+  //                                               *this->parameters);
+  //   nebFin->compute();
+  delete neb;
+}
 
 } /* namespace tests */
--- client/gtests/NEBJobGPRTest.h
+++ client/gtests/NEBJobGPRTest.h
@@ -3,6 +3,17 @@
 
 #include <gtest/gtest.h>
 
+#include <algorithm>
+
+#include "../GPRHelpers.h"
+#include "../HelperFunctions.h"
+#include "../Log.h"
+#include "../Matter.h"
+#include "../NudgedElasticBandJob.h"
+#include "../Parameters.h"
+#include "../Potential.h"
+#include "../potentials/Morse/Morse.h"
+
 namespace tests {
     class NEBJobGPRTest : public ::testing::Test {
         public:
@@ -10,14 +21,21 @@ namespace tests {
             virtual ~NEBJobGPRTest();
 
     const long double threshold{1e-6};
-    string reactantFilename;
-    string productFilename;
+
+    std::string reactantFilename;
+    std::string productFilename;
 
     std::unique_ptr<Parameters> parameters;
-    std::unique_ptr<Parameters> gprparameon;
     std::unique_ptr<Matter> initmatter;
     std::unique_ptr<Matter> finalmatter;
     std::unique_ptr<gpr::GaussianProcessRegression> gprfunc;
+    std::vector<Matter> imgArray;
+    gpr::Observation obspath;
+    std::pair<gpr::AtomsConfiguration, gpr::Coord> config_data;
+    AtomMatrix init_frcsref;
+    double init_eref;
+    std::function<bool(const gpr::EigenMatrix &lhs, const gpr::EigenMatrix &rhs)> comparer;
+    gpr::AtomsConfiguration atoms_conf;
     };
 } /* namespace tests */
 
