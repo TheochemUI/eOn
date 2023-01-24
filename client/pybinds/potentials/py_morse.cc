de09551c173dfa0b8d5a49bbe1ff488e15c5a236
Rohit Goswami <rog32@hi.is>
2023-01-24 19:22:28 +0000

    BUG: Fix corrected energy and force calls

--- client/pybinds/potentials/py_morse.cc
+++ client/pybinds/potentials/py_morse.cc
@@ -61,7 +61,10 @@ void py_morse(py::module_ &m) {
         },
             py::arg("nAtoms"), "positions"_a, "box"_a)
         .def("ef_matter", [](Morse &mpot, Matter mat){
-            mat.setPotential(mpot.getPotential(mat.getParameters()));
+            Parameters params{mat.getParameters()};
+            params.potential = "morse_pt";
+            mpot.setParams(&params);
+            mat.setPotential(&mpot);
             return std::make_pair(mat.getPotentialEnergy(), mat.getForces());
         }, py::arg("matter"))
         .def("force",
