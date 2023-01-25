3d2432fcb02a5f8580e1b148863e843021d97aa9
Rohit Goswami <rog32@hi.is>
2023-01-25 10:26:03 +0000

    TMP: Enable buggy setting up of the potential
    lldb python -- -c 'import bbdir.pyeonclient as ec; params=ec.Parameters(); m1=ec.Matter(params); m1.con2matter("pos.con"); ec.log_init(params, "blah.log"); params.LogPotential=False; exec(open("fakepot.py").read()); afake=FakePot(params); m1.setPotential(afake); print(m1.pot_energy);'
    
    Will sort of work, but not really

--- client/pybinds/py_matter.cc
+++ client/pybinds/py_matter.cc
@@ -65,7 +65,7 @@ void py_matter(py::module_ &m) {
              "The maximum distance between two atoms in the Matter objects",
              py::arg("matter"))
         // TODO: See why this is unresolved
-        // .def("setPotential", &Matter::setPotential, "Sets the potential function")
+        .def("setPotential", &Matter::setPotential, "Sets the potential function")
         .def("resize", &Matter::resize, "Set or reset the number of atoms", py::arg("nAtoms"))
         .def("numberOfAtoms", &Matter::numberOfAtoms, "Return the number of atoms")
         .def("getCell", &Matter::getCell, "Returns the cell dimensions")
