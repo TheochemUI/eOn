1d921ef7e80b18f5f38ef7b0d10cd5e5bf3b54fc
Rohit Goswami <rog32@hi.is>
2023-01-30 14:20:18 +0000

    MAINT: Format

--- client/gtests/MatterTest.cpp
+++ client/gtests/MatterTest.cpp
@@ -7,17 +7,18 @@
  */
 
 #include "MatterTest.h"
+#include "../potentials/Morse/Morse.cpp"
 
 #include <algorithm>
 
 namespace tests {
 
 MatterTest::MatterTest() {
-    // TODO Auto-generated constructor stub
+  // TODO Auto-generated constructor stub
 }
 
 MatterTest::~MatterTest() {
-    // TODO Auto-generated destructor stub
+  // TODO Auto-generated destructor stub
 }
 
 // Kanged from https://stackoverflow.com/a/39238772/1895378
@@ -30,13 +31,13 @@ bool VectorEquality(const VectorXd &lhs, const VectorXd &rhs) {
 }
 
 TEST(MatterTest, TestCell) {
-    std::string confile("pos.con");
-    Parameters *parameters = new Parameters;
-    Matter *m1 = new Matter(parameters);
-    m1->con2matter(confile);
-    Matrix3d _cell;
-    Matrix3d _cellInverse;
-    // clang-format off
+  std::string confile("pos.con");
+  Parameters *parameters = new Parameters;
+  Matter *m1 = new Matter(parameters);
+  m1->con2matter(confile);
+  Matrix3d _cell;
+  Matrix3d _cellInverse;
+  // clang-format off
     _cell << // Comma initialized
         25.0, 0.0, 0.0,
         0.0, 25.0, 0.0,
@@ -45,11 +46,11 @@ TEST(MatterTest, TestCell) {
         0.04, 0.0, 0.0,
         0.0, 0.04, 0.0,
         0.0, 0.0, 0.04;
-    // clang-format on
-    ASSERT_PRED2(MatrixEquality, _cell, m1->getCell());
-    ASSERT_PRED2(MatrixEquality, _cellInverse, m1->getCell().inverse());
-    delete m1;
-    delete parameters;
+  // clang-format on
+  ASSERT_PRED2(MatrixEquality, _cell, m1->getCell());
+  ASSERT_PRED2(MatrixEquality, _cellInverse, m1->getCell().inverse());
+  delete m1;
+  delete parameters;
 }
 
 } /* namespace tests */
