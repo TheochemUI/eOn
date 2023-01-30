456cb5ccdffd78691e1e70121848dbc2e0a6b453
Rohit Goswami <rog32@hi.is>
2023-01-30 22:50:09 +0000

    TST: Calling potential without matter

--- client/gtests/PotTest.cpp
+++ client/gtests/PotTest.cpp
@@ -11,34 +11,97 @@
 
 #include <algorithm>
 
+using namespace std::placeholders;
+
 namespace tests {
 
-PotTest::PotTest() {
-    // TODO Auto-generated constructor stub
-}
+PotTest::PotTest()
+    : params{new Parameters}, m1{new Matter(params)}, threshold{1e-4} {}
 
 PotTest::~PotTest() {
-    // TODO Auto-generated destructor stub
+  delete params;
+  delete m1;
 }
 
-// Kanged from https://stackoverflow.com/a/39238772/1895378
-bool MatrixEquality(const MatrixXd &lhs, const MatrixXd &rhs) {
-  return lhs.isApprox(rhs, 1e-4);
+void PotTest::SetUp() {
+  std::string confile("pos.con");
+  m1->con2matter(confile);
 }
 
-bool VectorEquality(const VectorXd &lhs, const VectorXd &rhs) {
-  return lhs.isApprox(rhs, 1e-4);
+void PotTest::TearDown() {}
+
+TEST_F(PotTest, getName) {
+  Parameters *parameters = new Parameters;
+  parameters->potential = "lj";
+  Potential *pot = Potential::getPotential(parameters);
+  ASSERT_EQ(pot->getName(), "lj");
+  parameters->potential = "morse_pt";
+  Potential *pot2 = Potential::getPotential(parameters);
+  ASSERT_EQ(pot2->getName(), "morse_pt");
 }
 
-    TEST(PotTest, getName){
-        string confile("pos.con");
-        Parameters *parameters = new Parameters;
-        parameters->potential = "lj";
-        Potential* pot = Potential::getPotential(parameters);
-        ASSERT_EQ(pot->getName(), "lj");
-        parameters->potential = "morse_pt";
-        Potential* pot2 = Potential::getPotential(parameters);
-        ASSERT_EQ(pot2->getName(), "morse_pt");
-    }
+TEST_F(PotTest, callForce) {
+  auto matEq =
+      std::bind(helper_functions::eigenEquality<AtomMatrix>, _1, _2, threshold);
+  // ./eonclient -p "lj" -s pos.con
+  double energy_lj{-8.9245539406};
+  AtomMatrix forces_lj{Eigen::MatrixXd::Zero(m1->numberOfAtoms(), 3)};
+  // clang-format off
+  forces_lj << // Comma initialized
+      0.463529,   -0.24841,   -1.30892,
+      1.08313,  0.0575693,   0.886135,
+      -2.20116,   -2.23402,   -2.09064,
+      2.23797,   -2.40278,    2.50396,
+      -2.71153,   0.972603,   -2.41778,
+      2.14953,    3.62404,     2.3242,
+      0.823661,    1.02942,    1.04033,
+      -0.488582,    1.86173,   0.036945,
+      -0.234744,  -0.460397,   -1.21061,
+      -0.0650386,   -2.24842,   0.560231,
+      1.03832,  -0.512915,    1.15749,
+      -0.813122,   0.310969,   -1.78941,
+      -1.28196,   0.250611,    0.30807;
+  // clang-format on
+  // ./eonclient -p "morse" -s pos.con
+  double energy_morse{1611.8700112470};
+  AtomMatrix forces_morse{Eigen::MatrixXd::Zero(m1->numberOfAtoms(), 3)};
+  // clang-format off
+  forces_morse << // Comma initialized
+      -64.9256,  38.3478,   194.53,
+      -157.289, -7.61147, -134.612,
+      -164.748, -274.908, -285.305,
+      76.0495, -338.668,  421.113,
+      -226.069,  166.355, -318.858,
+      106.552,   385.35,  298.415,
+      364.555,  448.297,  440.502,
+      -201.26,  742.277,  84.4901,
+      -106.268, -272.671, -696.314,
+      -12.0385, -743.517,  239.385,
+      470, -229.572,  504.702,
+      -293.633,  125.068, -697.975,
+      209.073, -38.7473, -50.0734;
+  // clang-format on
+  params->potential = "lj";
+  Potential *pot = Potential::getPotential(params);
+  double e_lj{0};
+  AtomMatrix f_lj = Eigen::MatrixXd::Ones(m1->numberOfAtoms(), 3);
+  pot->force(m1->numberOfAtoms(), m1->getPositions().data(),
+             m1->getAtomicNrs().data(), f_lj.data(), &e_lj,
+             m1->getCell().data(), 1);
+  ASSERT_NEAR(e_lj, energy_lj, threshold);
+  ASSERT_PRED2(matEq, forces_lj, f_lj);
+  // Switching
+  double e_morse{0};
+  AtomMatrix f_morse = Eigen::MatrixXd::Ones(m1->numberOfAtoms(), 3);
+  params->potential = "morse_pt";
+  Potential *pot2 = Potential::getPotential(params);
+  ASSERT_NE(pot2, pot);
+  ASSERT_EQ(pot2->getName(), "morse_pt");
+  pot2->force(m1->numberOfAtoms(), m1->getPositions().data(),
+              m1->getAtomicNrs().data(), f_morse.data(), &e_morse,
+              m1->getCell().data(), 1);
+  ASSERT_NEAR(e_morse, energy_morse, threshold);
+  ASSERT_PRED2(matEq, forces_morse, f_morse);
+}
 
 } /* namespace tests */
