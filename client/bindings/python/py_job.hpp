77ea9591fdd32a89e8d4c851a5f49e9d41cce42e
Rohit Goswami <rog32@hi.is>
2023-01-31 12:35:27 +0000

    BIND: Add job and saddlesearchjob

---
+++ client/bindings/python/py_job.hpp
@@ -0,0 +1,16 @@
+#ifndef PY_JOB_H
+#define PY_JOB_H
+
+#include "../../Job.h"
+#include "py_wrapper.hpp"
+
+template <class JobBase = Job>
+class PyJob : public JobBase {
+public:
+    using JobBase::JobBase;
+    std::vector<std::string> run() override {
+        PYBIND11_OVERRIDE_PURE(std::vector<std::string>, JobBase, run, );
+    };
+};
+
+#endif /* PY_JOB_H */
--- client/bindings/python/py_wrapper.cc
+++ client/bindings/python/py_wrapper.cc
@@ -4,4 +4,7 @@ PYBIND11_MODULE(pyeonclient, m) {
     py_parameters(m);
     py_log(m);
     py_matter(m);
+    // Jobs
+    py_job(m);
+    py_saddlesearchjob(m);
 }
