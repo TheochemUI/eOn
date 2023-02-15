cde4ae976aececfab70e2ceab6dc9ffe3e32e7f6
Rohit Goswami <rog32@hi.is>
2023-02-15 20:52:51 +0000

    TST: Disable

--- client/testpot.cpp
+++ client/testpot.cpp
@@ -1,15 +1,16 @@
 #include "Matter.h"
 #include "Parameters.h"
 #include <cstdlib>
+#include <memory>
+
+using namespace std::string_literals; // For ""s
 
 int main(void){
-  string confile("pos.con");
-  Parameters *parameters = new Parameters;
-  parameters->potential = "cuh2_pot";
-  Matter *matter = new Matter(parameters);
-  matter->con2matter(confile);
-  std::cout<<matter->getPotentialEnergy()<<std::endl;
-  delete matter;
-  delete parameters;
+  // string confile("pos.con");
+  // std::unique_ptr<Parameters> params = std::make_unique<Parameters>();
+  // params ->potential = "cuh2_pot"s;
+  // std::unique_ptr<Matter> matter = std::make_unique<Matter>(params.get());
+  // matter->con2matter(confile);
+  // std::cout<<matter->getPotentialEnergy()<<std::endl;
   return EXIT_SUCCESS;
 }
