becce2659813669bdcb2a23a366e5c0e06859754
Rohit Goswami <rog32@hi.is>
2023-02-16 12:56:56 +0000

    MAINT: Minor update to fix signatures

--- client/potentials/AMS_IO/AMS_IO.cpp
+++ client/potentials/AMS_IO/AMS_IO.cpp
@@ -72,7 +72,7 @@ namespace {
      
 
 
-void AMS_IO::force(long N, const double *R, const int *atomicNrs, double *F, double *U, const double *box, int nImages=1)
+void AMS_IO::force(long N, const double *R, const int *atomicNrs, double *F, double *U, const double *box)
 {
     passToSystem(N, R, atomicNrs, box); 
     system("chmod +x run_AMS_IO.sh");
