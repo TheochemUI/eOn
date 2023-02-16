435b6c02479c9b4e55c5c0e487e39de1568cb8ba
Rohit Goswami <rog32@hi.is>
2023-02-16 03:00:56 +0000

    ENH: Rework the Morse bindings

--- client/bindings/py_srcs/potentials/py_morse.cc
+++ client/bindings/py_srcs/potentials/py_morse.cc
@@ -11,23 +11,16 @@ class PyMorse : public PyPotential<MorseBase> {
 public:
     using PyPotential<MorseBase>::PyPotential; // Inherit constructor
     // Override pure virtual with non-pure
-    void force(long nAtoms,
-               const double *positions,
-               const int *atomicNrs,
-               double *forces,
-               double *energy,
-               const double *box) override {
-        PYBIND11_OVERRIDE(void,      /* Return type */
+    std::pair<double, AtomMatrix>
+    get_ef(const AtomMatrix positions, const VectorXi atomicNrs, const Matrix3d box) override {
+        // This is needed to prevent substitution errors
+        using Return = std::pair<double, AtomMatrix>;
+        PYBIND11_OVERRIDE(Return, /* Return type */
                           MorseBase, /* Parent class */
-                          force,     /* Name of function in C++ (must match Python name) */
-                          nAtoms,    /* Argument(s) */
-                          positions,
-                          atomicNrs,
-                          forces,
-                          energy,
-                          box
-                          );
-    };
+                          get_ef, /* Function name (C++) */
+                          positions, /* Arg(s) */
+                          atomicNrs, box);
+    }
 };
 
 void py_morse(py::module_ &m) {
@@ -46,34 +39,8 @@ void py_morse(py::module_ &m) {
         */
         .def("cleanMemory", &Morse::cleanMemory)
         // /** TODO: These force calls should be part of Potential, no need to make separate bindings **/
-        .def(
-            "force",
-            [](Morse &pot, Matter &mat) {
-                // Store and restore
-                Potential* oldPot = mat.getPotential();
-                mat.setPotential(&pot);
-                double e_pot{mat.getPotentialEnergy()};
-                AtomMatrix f_pot {mat.getForces()};
-                mat.setPotential(oldPot);
-                return std::make_pair(e_pot, f_pot);
-            },
-            py::arg("matter"))
         /* Technically incorrect, but might be useful, does not account for the fixed / free atoms */
-        .def(
-            "force",
-            [](Morse &pot, size_t natoms, AtomMatrix pos, VectorXi atmnrs, Matrix3d box) {
-                double e_pot{0};
-                AtomMatrix f_pot = Eigen::MatrixXd::Ones(natoms, 3);
-                pot.force(natoms, pos.data(), atmnrs.data(), f_pot.data(), &e_pot, box.data());
-                return std::make_pair(e_pot, f_pot);
-            },
-            py::arg("natoms"),
-            "pos"_a,
-            "atmnrs"_a,
-            "box"_a)
-        /*
-        ** Parameters
-        */
+        .def("get_ef", &Morse::get_ef)
 
         /*
         ** Python helpers
