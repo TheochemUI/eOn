defd23f8a01e360c96b89dd330bf44a3ba174794
Rohit Goswami <rog32@hi.is>
2023-05-26 10:45:38 +0000

    MAINT: Add all jobs again

--- client/Job.cpp
+++ client/Job.cpp
@@ -26,6 +26,70 @@ std::unique_ptr<Job> makeJob(std::unique_ptr<Parameters> params) {
           return (std::make_unique<ProcessSearchJob>(std::move(params)));
           break;
       }
+      case JobType::SaddleSearch: {
+          return (std::make_unique<SaddleSearchJob>(std::move(params)));
+          break;
+      }
+      case JobType::Minimization: {
+          return (std::make_unique<MinimizationJob>(std::move(params)));
+          break;
+      }
+      case JobType::Point: {
+          return (std::make_unique<PointJob>(std::move(params)));
+          break;
+      }
+      case JobType::ParallelReplica: {
+          return (std::make_unique<ParallelReplicaJob>(std::move(params)));
+          break;
+      }
+      case JobType::SafeHyperdynamics: {
+          return (std::make_unique<SafeHyperJob>(std::move(params)));
+          break;
+      }
+      case JobType::TAD: {
+          return (std::make_unique<TADJob>(std::move(params)));
+          break;
+      }
+      case JobType::ReplicaExchange: {
+          return (std::make_unique<ReplicaExchangeJob>(std::move(params)));
+          break;
+      }
+      case JobType::BasinHopping: {
+          return (std::make_unique<BasinHoppingJob>(std::move(params)));
+          break;
+      }
+      case JobType::Hessian: {
+          return (std::make_unique<HessianJob>(std::move(params)));
+          break;
+      }
+      case JobType::FiniteDifference: {
+          return (std::make_unique<FiniteDifferenceJob>(std::move(params)));
+          break;
+      }
+      case JobType::NEB: {
+          return (std::make_unique<NudgedElasticBandJob>(std::move(params)));
+          break;
+      }
+      case JobType::Dynamics: {
+          return (std::make_unique<DynamicsJob>(std::move(params)));
+          break;
+      }
+      case JobType::Prefactor: {
+          return (std::make_unique<PrefactorJob>(std::move(params)));
+          break;
+      }
+      case JobType::GlobalOptimization: {
+          return (std::make_unique<GlobalOptimizationJob>(std::move(params)));
+          break;
+      }
+      case JobType::StructureComparison: {
+          return (std::make_unique<StructureComparisonJob>(std::move(params)));
+          break;
+      }
+      case JobType::MonteCarlo: {
+          return (std::make_unique<MonteCarloJob>(std::move(params)));
+          break;
+      }
       default:
           throw std::runtime_error("No known job could be constructed");
         break;
