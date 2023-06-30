885adecabd10913823d2e447e5de32ab00d3c032
Rohit Goswami <rog32@hi.is>
2023-06-30 17:58:28 +0000

    MAINT: Log cleanup and also clang-format
    With the slow:
    find . \( -name '*.cpp' -o -name '*.h' -o -name '*.hpp' -o -name '*.cc' \) -exec clang-format -i {} \;

--- client/Prefactor.cpp
+++ client/Prefactor.cpp
@@ -1,8 +1,5 @@
-
 #include "Prefactor.h"
 #include "Hessian.h"
-#include "Log.h"
-#include <math.h>
 
 int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
                              Matter *saddle, Matter *min2, double &pref1,
@@ -25,7 +22,7 @@ int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
   Hessian hessian(parameters, min1);
   min1Freqs = hessian.getFreqs(min1, atoms);
   if (min1Freqs.size() == 0) {
-    log("[Prefactor] Bad hessian: min1\n");
+    SPDLOG_ERROR("[Prefactor] Bad hessian: min1");
     return -1;
   }
   // remove zero modes
@@ -36,7 +33,7 @@ int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
   // calculate saddle frequencies
   saddleFreqs = hessian.getFreqs(saddle, atoms);
   if (saddleFreqs.size() == 0) {
-    log("[Prefactor] Bad hessian: saddle\n");
+    SPDLOG_ERROR("[Prefactor] Bad hessian: saddle");
     return -1;
   }
   // remove zero modes
@@ -48,7 +45,7 @@ int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
   min2Freqs = hessian.getFreqs(min2, atoms);
   if (min2Freqs.size() == 0) {
     if (!parameters->quiet) {
-      log("[Prefactor] Bad hessian: min2\n");
+      SPDLOG_ERROR("[Prefactor] Bad hessian: min2");
     }
     return -1;
   }
@@ -61,7 +58,7 @@ int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
   if ((min1Freqs.size() != saddleFreqs.size()) ||
       (min1Freqs.size() != saddleFreqs.size())) {
     if (!parameters->quiet) {
-      log("[Prefactor] Bad prefactor: Hessian sizes do not match\n");
+      SPDLOG_ERROR("[Prefactor] Bad prefactor: Hessian sizes do not match");
     }
     return -1;
   }
@@ -74,12 +71,12 @@ int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
   int i, numNegFreq = 0;
   for (i = 0; i < size; i++) {
     if (min1Freqs(i) < 0) {
-      log("[Prefactor] min1 had negative mode of %e\n", min1Freqs(i));
+      SPDLOG_DEBUG("[Prefactor] min1 had negative mode of {}", min1Freqs(i));
       numNegFreq++;
     }
   }
   if (numNegFreq != 0) {
-    log("[Prefactor] Error: %i negative modes at min1\n", numNegFreq);
+    SPDLOG_DEBUG("[Prefactor] Error: {} negative modes at min1", numNegFreq);
     return -1;
   }
 
@@ -90,7 +87,7 @@ int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
     }
   }
   if (numNegFreq != 1) {
-    log("Error: %i negative modes at saddle\n", numNegFreq);
+    SPDLOG_DEBUG("Error: {} negative modes at saddle", numNegFreq);
     return -1;
   }
 
@@ -101,7 +98,7 @@ int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
     }
   }
   if (numNegFreq != 0) {
-    log("Error: %i negative modes at min2\n", numNegFreq);
+    SPDLOG_DEBUG("Error: {} negative modes at min2", numNegFreq);
     return -1;
   }
 
@@ -140,21 +137,21 @@ int Prefactor::getPrefactors(Parameters *parameters, Matter *min1,
     pref1 = 2. * kB_T / (h)*pref1;
     pref2 = 2. * kB_T / (h)*pref2;
   }
-  log("[Prefactor] reactant to product prefactor: %.3e\n", pref1);
-  log("[Prefactor] product to reactant prefactor: %.3e\n", pref2);
+  SPDLOG_DEBUG("[Prefactor] reactant to product prefactor: %.3e", pref1);
+  SPDLOG_DEBUG("[Prefactor] product to reactant prefactor: %.3e", pref2);
   return 0;
 }
 
 void Prefactor::logFreqs(VectorXd freqs, char *name) {
-  log_file("[Prefactor] Frequencies at %s\n", name);
+  log_file("[Prefactor] Frequencies at %s", name);
   int i;
   for (i = 0; i < freqs.size(); i++) {
     log_file("%10.6f ", freqs(i));
     if ((i + 1) % 5 == 0) {
-      log_file("\n");
+      log_file("");
     }
   }
-  log_file("\n");
+  log_file("");
 }
 
 VectorXi Prefactor::movedAtoms(Parameters *parameters, Matter *min1,
@@ -215,7 +212,8 @@ VectorXi Prefactor::movedAtomsPct(Parameters *parameters, Matter *min1,
   VectorXd diff(nAtoms);
   diff.setConstant(0.0);
 
-  log("[Prefactor] including all atoms that make up %.3f%% of the motion\n",
+  SPDLOG_DEBUG(
+      "[Prefactor] including all atoms that make up %.3f%% of the motion",
       parameters->prefactorFilterFraction * 100);
   double sum = 0.0;
   int mini = 0;
@@ -227,8 +225,8 @@ VectorXi Prefactor::movedAtomsPct(Parameters *parameters, Matter *min1,
     }
   }
 
-  log("[Prefactor] sum of atom distances moved %.4f\n", sum);
-  log("[Prefactor] max moved atom distance: %.4f\n", diff.maxCoeff());
+  SPDLOG_DEBUG("[Prefactor] sum of atom distances moved %.4f", sum);
+  SPDLOG_DEBUG("[Prefactor] max moved atom distance: %.4f", diff.maxCoeff());
 
   int nMoved = 0;
   double d = 0.0;
@@ -263,9 +261,9 @@ VectorXi Prefactor::movedAtomsPct(Parameters *parameters, Matter *min1,
       }
     }
   }
-  log("[Prefactor] including %i atoms in the hessian (%i moved + %i "
-      "neighbors)\n",
-      totalAtoms, nMoved, totalAtoms - nMoved);
+  SPDLOG_DEBUG("[Prefactor] including {} atoms in the hessian ({} moved + {} "
+               "neighbors)",
+               totalAtoms, nMoved, totalAtoms - nMoved);
   return (VectorXi)moved.block(0, 0, totalAtoms, 1);
 }
 
