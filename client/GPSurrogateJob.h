1ba56681b13147620fa1f25e5c4b6fc6d7a0bf15
Rohit Goswami <rog32@hi.is>
2023-07-01 14:30:56 +0000

    MAINT: Clang format and massive changes
    Also rework the uncertainity point choice and many more changes

--- client/GPSurrogateJob.h
+++ client/GPSurrogateJob.h
@@ -1,14 +1,14 @@
 #pragma once
 
+#include "HelperFunctions.h"
 #include "Job.h"
 #include "Parameters.h"
-#include "HelperFunctions.h"
 
-#include <pybind11/embed.h>
-#include <pybind11/eigen.h>
+#include "potentials/PySurrogate/PySurrogate.h"
 #include <fmt/format.h>
 #include <fmt/ostream.h>
-#include "potentials/PySurrogate/PySurrogate.h"
+#include <pybind11/eigen.h>
+#include <pybind11/embed.h>
 
 #include "NudgedElasticBand.h"
 
@@ -16,37 +16,43 @@ class GPSurrogateJob : public Job {
 public:
   GPSurrogateJob(std::unique_ptr<Parameters> parameters)
       : Job(std::move(parameters)) {
-  // debugging
+    // debugging
 #ifndef NDEBUG
-  py::module_ sys_mod = py::module_::import("sys");
-  py::module_ ipdb_mod = py::module_::import("ipdb");
-  sys_mod.attr("breakpointhook") = ipdb_mod.attr("set_trace");
+    py::module_ sys_mod = py::module_::import("sys");
+    py::module_ ipdb_mod = py::module_::import("ipdb");
+    sys_mod.attr("breakpointhook") = ipdb_mod.attr("set_trace");
 #endif // NDEBUG
   }
   ~GPSurrogateJob(void) = default;
   std::vector<std::string> run(void) override;
 
 private:
-    void saveData(NudgedElasticBand::NEBStatus status,
-                  std::shared_ptr<Potential> true_pot,
-                  std::unique_ptr<NudgedElasticBand> neb);
-    std::vector<std::string> returnFiles;
-    pybind11::scoped_interpreter guard{};
+  void saveData(NudgedElasticBand::NEBStatus status,
+                std::shared_ptr<Potential> true_pot,
+                std::unique_ptr<NudgedElasticBand> neb);
+  std::vector<std::string> returnFiles;
+  pybind11::scoped_interpreter guard{};
 };
 
 namespace helper_functions::surrogate {
-  Eigen::MatrixXd get_features(const std::vector<Matter>& matobjs);
-  Eigen::MatrixXd get_features(const std::vector<std::shared_ptr<Matter>>& matobjs);
-  Eigen::MatrixXd get_targets(std::vector<std::shared_ptr<Matter>>& matobjs, std::shared_ptr<Potential> true_pot);
-  Eigen::MatrixXd get_targets(std::vector<Matter>& matobjs, std::shared_ptr<Potential> true_pot);
-  Eigen::VectorXd make_target(Matter &m1, std::shared_ptr<Potential> true_pot);
-  std::pair<Eigen::VectorXd, Eigen::VectorXd>
-  getNewDataPoint(const std::vector<std::shared_ptr<Matter>> &matobjs, std::shared_ptr<Potential> true_pot);
-  std::vector<Matter> getMidSlice(const std::vector<Matter>& matobjs);
-}
+Eigen::MatrixXd get_features(const std::vector<Matter> &matobjs);
+Eigen::MatrixXd
+get_features(const std::vector<std::shared_ptr<Matter>> &matobjs);
+Eigen::MatrixXd get_targets(std::vector<std::shared_ptr<Matter>> &matobjs,
+                            std::shared_ptr<Potential> true_pot);
+Eigen::MatrixXd get_targets(std::vector<Matter> &matobjs,
+                            std::shared_ptr<Potential> true_pot);
+Eigen::VectorXd make_target(Matter &m1, std::shared_ptr<Potential> true_pot);
+std::pair<Eigen::VectorXd, Eigen::VectorXd>
+getNewDataPoint(const std::vector<std::shared_ptr<Matter>> &matobjs,
+                std::shared_ptr<Potential> true_pot);
+std::vector<Matter> getMidSlice(const std::vector<Matter> &matobjs);
+bool accuratePES(std::vector<std::shared_ptr<Matter>> &matobjs,
+                 std::shared_ptr<Potential> true_pot);
+} // namespace helper_functions::surrogate
 
 namespace helper_functions::eigen {
-  Eigen::MatrixXd vertCat(const Eigen::MatrixXd& m1, const Eigen::MatrixXd& m2);
-  void addVectorRow(Eigen::MatrixXd& data, const Eigen::VectorXd& newrow);
-  // Modifies data
-}
+Eigen::MatrixXd vertCat(const Eigen::MatrixXd &m1, const Eigen::MatrixXd &m2);
+void addVectorRow(Eigen::MatrixXd &data, const Eigen::VectorXd &newrow);
+// Modifies data
+} // namespace helper_functions::eigen
