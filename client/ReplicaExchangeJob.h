c9dafd0c0b59dcfc83a2c3fe64b9582cc1187063
Rohit Goswami <rog32@hi.is>
2023-07-03 12:09:47 +0000

    MAINT: [log] replica exchange job

--- client/ReplicaExchangeJob.h
+++ client/ReplicaExchangeJob.h
@@ -1,16 +1,16 @@
-
 #ifndef REPLICAEXCHANGEJOB_H
 #define REPLICAEXCHANGEJOB_H
 
 #include "Dynamics.h"
-#include "Eigen.h"
 #include "Job.h"
 #include "Parameters.h"
 
 class ReplicaExchangeJob : public Job {
 public:
   ReplicaExchangeJob(std::unique_ptr<Parameters> parameters)
-      : Job(std::move(parameters)), forceCalls{0} {}
+      : Job(std::move(parameters)), forceCalls{0} {
+    log = spdlog::get("combi");
+  }
   ~ReplicaExchangeJob(void) = default;
   std::vector<std::string> run(void);
 
@@ -23,6 +23,7 @@ private:
   //        Dynamics **replicaDynamics;
   //        double *replicaTemperature;
   std::vector<std::string> returnFiles;
+  shared_ptr<spdlog::logger> log;
 };
 
 #endif
