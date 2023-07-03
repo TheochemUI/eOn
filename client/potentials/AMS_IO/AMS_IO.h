8972a89b818004f9949e14c5f2b26cc552770f9d
Rohit Goswami <rog32@hi.is>
2023-07-03 04:06:26 +0000

    MAINT: Use the pre-commit hook

--- client/potentials/AMS_IO/AMS_IO.h
+++ client/potentials/AMS_IO/AMS_IO.h
@@ -23,6 +23,7 @@ public:
   void cleanMemory(void);
   void force(long N, const double *R, const int *atomicNrs, double *F,
              double *U, double *variance, const double *box);
+
 private:
   void passToSystem(long N, const double *R, const int *atomicNrs,
                     const double *box);
