1e3f4ee3a2cfe86d5c8fb1ee2ab133dad863054c
Rohit Goswami <rog32@hi.is>
2023-07-04 09:34:25 +0000

    ENH: Conform [fire] to new Optimizer mechanism
    Also introduce the new naming convention
    _m, _a

--- client/FIRE.h
+++ client/FIRE.h
@@ -8,27 +8,47 @@
 class FIRE : public Optimizer {
 
 public:
-  double dt;
-
-  FIRE(ObjectiveFunction *objf, Parameters *parameters);
+  FIRE(std::shared_ptr<ObjectiveFunction> a_objf,
+       std::shared_ptr<Parameters> a_params)
+      : m_objf{a_objf},
+        m_params{a_params},
+        m_dt{a_params->optTimeStep},
+        m_dt_max{a_params->optMaxTimeStep},
+        m_max_move{a_params->optMaxMove},
+        m_N_min{5},
+        m_N{0},
+        m_v{Eigen::VectorXd::Zero(a_objf->degreesOfFreedom())},
+        m_alpha_start{0.1},
+        m_alpha{m_alpha_start},
+        m_f_inc{1.1},
+        m_f_dec{0.5},
+        m_f_a{0.99},
+        m_iteration{0} {
+    if (spdlog::get("fire")) {
+      m_log = spdlog::get("fire");
+    } else {
+      m_log = spdlog::basic_logger_st("fire", "_fire.log", true);
+    }
+    m_log->set_pattern("[%l] [FIRE] %v");
+  }
   ~FIRE() = default;
 
-  int step(double maxMove);
-  int run(int maxIterations, double maxMove);
+  int step(double a_maxMove);
+  int run(int a_maxIterations, double a_maxMove);
 
 private:
-  ObjectiveFunction *objf;
-  Parameters *parameters;
-  VectorXd v;
-  double alpha;
-  double alpha_start;
-  int N, N_min;
-  double f_inc;
-  double f_dec;
-  double f_a;
-  double dt_max;
-  int iteration;
-  shared_ptr<spdlog::logger> log;
+  std::shared_ptr<ObjectiveFunction> m_objf;
+  std::shared_ptr<Parameters> m_params;
+  double m_dt, m_dt_max, m_max_move;
+  size_t m_N_min, m_N;
+  Eigen::VectorXd m_v;
+  double m_alpha_start;
+  double m_alpha;
+  double m_f_inc;
+  double m_f_dec;
+  double m_f_a;
+  size_t m_iteration;
+  shared_ptr<spdlog::logger> m_log;
 };
 
 #endif
