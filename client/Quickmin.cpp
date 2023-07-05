1ae32851cef6a1b622ea3a87e011e8eb50a38f9f
Rohit Goswami <rog32@hi.is>
2023-07-05 10:06:23 +0000

    MAINT: Rework QM for new Optimizer interface

--- client/Quickmin.cpp
+++ client/Quickmin.cpp
@@ -1,50 +1,32 @@
 #include "Quickmin.h"
 #include "HelperFunctions.h"
 
-Quickmin::Quickmin(ObjectiveFunction *objfPassed,
-                   Parameters *parametersPassed) {
-  objf = objfPassed;
-  parameters = parametersPassed;
-  dt = parametersPassed->optTimeStep;
-  velocity.resize(objf->degreesOfFreedom());
-  velocity.setZero();
-  iteration = 0;
-  if (spdlog::get("qm")) {
-    log = spdlog::get("qm");
+int Quickmin::step(double m_maxMove) {
+  Eigen::VectorXd force = -m_objf->getGradient();
+  if (m_params->optQMSteepestDecent) {
+    m_vel.setZero();
   } else {
-    log = spdlog::basic_logger_st("qm", "_qm.log", true);
-  }
-  log->set_pattern("[%l] [QM] %v");
-}
-
-Quickmin::~Quickmin() { return; }
-
-int Quickmin::step(double maxMove) {
-  VectorXd force = -objf->getGradient();
-  if (parameters->optQMSteepestDecent) {
-    velocity.setZero();
-  } else {
-    if (velocity.dot(force) < 0) {
-      velocity.setZero();
+    if (m_vel.dot(force) < 0) {
+      m_vel.setZero();
     } else {
-      VectorXd f_unit = force / force.norm();
-      velocity = velocity.dot(f_unit) * f_unit;
+      Eigen::VectorXd f_unit = force / force.norm();
+      m_vel = m_vel.dot(f_unit) * f_unit;
     }
   }
 
-  velocity += force * dt;
-  VectorXd dr = helper_functions::maxAtomMotionAppliedV(velocity * dt,
-                                                        parameters->optMaxMove);
-  // SPDLOG_LOGGER_INFO(log, "{} Velocity is {}", iteration,
-  // fmt::streamed(velocity));
-  objf->setPositions(objf->getPositions() + dr);
-  iteration++;
-  return objf->isConverged();
+  m_vel += force * m_dt;
+  Eigen::VectorXd dr = helper_functions::maxAtomMotionAppliedV(
+      m_vel * m_dt, m_maxMove); // used to be m_params->optMaxTimeStep
+  SPDLOG_LOGGER_INFO(m_log, "{} M_Vel is {}", m_iteration,
+                     fmt::streamed(m_vel));
+  m_objf->setPositions(m_objf->getPositions() + dr);
+  m_iteration++;
+  return m_objf->isConverged();
 }
 
-int Quickmin::run(int maxSteps, double maxMove) {
-  while (!objf->isConverged() && iteration < maxSteps) {
-    step(maxMove);
+int Quickmin::run(size_t m_maxSteps, double m_maxMove) {
+  while (!m_objf->isConverged() && m_iteration < m_maxSteps) {
+    step(m_maxMove);
   }
-  return objf->isConverged();
+  return m_objf->isConverged();
 }
