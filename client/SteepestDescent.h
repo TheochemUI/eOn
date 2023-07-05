60475b49725ac39464bcabd15f99995342a8589f
Rohit Goswami <rog32@hi.is>
2023-07-05 10:43:13 +0000

    MAINT: Rework [sd] for optimizer interface

--- client/SteepestDescent.h
+++ client/SteepestDescent.h
@@ -8,25 +8,30 @@
 #include "Optimizer.h"
 #include "Parameters.h"
 
-class SteepestDescent : public Optimizer {
-
+class SteepestDescent final : public Optimizer {
 public:
-  SteepestDescent(ObjectiveFunction *objf, Parameters *parameters);
+  SteepestDescent(std::shared_ptr<ObjectiveFunction> a_objf,
+                  std::shared_ptr<Parameters> a_params)
+      : Optimizer(a_objf, OptType::SteepestDescent, a_params),
+        iteration{0} {
+    if (spdlog::get("sd")) {
+      m_log = spdlog::get("sd");
+    } else {
+      m_log = spdlog::basic_logger_st("sd", "_sd.log", true);
+    }
+    m_log->set_pattern("[%l] [SD] %v");
+  }
   ~SteepestDescent() = default;
 
-  int step(double maxMove);
-  int run(int maxIterations, double maxMove);
+  int step(double a_maxMove) override;
+  int run(size_t a_maxIterations, double a_maxMove) override;
 
 private:
-  shared_ptr<spdlog::logger> log;
-  VectorXd getStep(VectorXd f);
-  Parameters *parameters;
-  ObjectiveFunction *objf;
-
-  int iteration;
-
-  VectorXd rPrev;
-  VectorXd fPrev;
+  shared_ptr<spdlog::logger> m_log;
+  Eigen::VectorXd getStep(Eigen::VectorXd a_f);
+  size_t iteration;
+  Eigen::VectorXd m_rPrev;
+  Eigen::VectorXd m_fPrev;
 };
 
 #endif
