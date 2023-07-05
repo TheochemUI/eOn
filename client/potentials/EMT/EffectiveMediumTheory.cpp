8470bf8dd11dfc07059b09877380277479d900cc
Rohit Goswami <rog32@hi.is>
2023-07-05 10:59:16 +0000

    BUG: Use parameters correctly in potentials

--- client/potentials/EMT/EffectiveMediumTheory.cpp
+++ client/potentials/EMT/EffectiveMediumTheory.cpp
@@ -71,7 +71,7 @@ void EffectiveMediumTheory::force(long N, const double *R, const int *atomicNrs,
 
     AtomsObj->SetAtomicNumbers(atomicNrsTemp);
 
-    if (params->EMTRasmussen) {
+    if (m_params->EMTRasmussen) {
       EMTParameterObj = new EMTRasmussenParameterProvider();
       EMTObj = new EMT(EMTParameterObj);
     } else
