425660f4323961581e57d9b3473fd2a9bf5d1441
Rohit Goswami <rog32@hi.is>
2023-07-10 22:00:03 +0000

    ENH: Initialize SurrogatePotential

--- client/SurrogatePotential.h
+++ client/SurrogatePotential.h
@@ -2,12 +2,17 @@
 
 #include "Potential.h"
 
+#include <any>
+
 class SurrogatePotential : public Potential {
 
 public:
-  SurrogatePotential(std::shared_ptr<Parameters> paramsIn)
-      : Potential(paramsIn) {}
+  SurrogatePotential(PotType a_ptype, std::shared_ptr<Parameters> a_params)
+      : Potential(a_ptype, a_params) {}
   virtual ~SurrogatePotential() = default;
-  std::tuple<double, AtomMatrix, Eigen::MatrixXd>
+  std::tuple<double, AtomMatrix, double> // energy, forces, energy variance
   get_ef_var(const AtomMatrix pos, const VectorXi atmnrs, const Matrix3d box);
+  virtual void train_optimize(Eigen::MatrixXd a_features,
+                              Eigen::MatrixXd a_targets) = 0;
+  virtual void prepare(const std::any &data) = 0;
 };
