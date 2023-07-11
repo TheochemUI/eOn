bb174f143366cf6bfe47a99d34cb7ba5f2ad985a
Rohit Goswami <rog32@hi.is>
2023-07-11 10:40:11 +0000

    MAINT: Cleanup SurrogatePotential
    Use constructors

--- client/SurrogatePotential.h
+++ client/SurrogatePotential.h
@@ -2,8 +2,6 @@
 
 #include "Potential.h"
 
-#include <any>
-
 class SurrogatePotential : public Potential {
 
 public:
@@ -14,5 +12,4 @@ public:
   get_ef_var(const AtomMatrix pos, const VectorXi atmnrs, const Matrix3d box);
   virtual void train_optimize(Eigen::MatrixXd a_features,
                               Eigen::MatrixXd a_targets) = 0;
-  virtual void prepare(const std::any &data) = 0;
 };
