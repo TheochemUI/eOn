e43ba08d59df73174783160ce418ef0af41d1363
Rohit Goswami <rog32@hi.is>
2023-11-16 04:55:26 +0000

    ENH: Add the ASE_ORCA interface
    Works in parallel, easy to extend, gets the right units

---
+++ client/potentials/ASE_ORCA/ASE_ORCA.cpp
@@ -0,0 +1,61 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//-----------------------------------------------------------------------------------
+
+#include "ASE_ORCA.h"
+#include "Eigen/src/Core/Matrix.h"
+
+ASEOrcaPot::ASEOrcaPot(shared_ptr<Parameters> a_params)
+    : Potential(PotType::ASE_ORCA, a_params) {
+  py::module_ sys = py::module_::import("sys");
+  ase = py::module_::import("ase");
+  py::module_ ase_orca = py::module_::import("ase.calculators.orca");
+  py::module_ psutil = py::module_::import("psutil");
+
+  // Set up ORCA profile and calculator
+  py::object OrcaProfile = ase_orca.attr("OrcaProfile");
+  py::object ORCA = ase_orca.attr("ORCA");
+
+  py::list orca_paths;
+  orca_paths.append(py::str(a_params->orca_path));
+  this->calc = ORCA("profile"_a = OrcaProfile(orca_paths),
+                    // TODO: These can all be user parameters, except ENGRAD, which is needed for energy + forces
+                    "orcasimpleinput"_a = "ENGRAD B3LYP def2-SVP DefGrid2", // DefGrid2 is for ORCA > 5.x
+                    "orcablocks"_a = py::str(fmt::format(
+                        "%pal nprocs {} end",
+                        py::cast<int>(psutil.attr("cpu_count")(false)))),
+                    "directory"_a = ".");
+};
+
+void ASEOrcaPot::force(long nAtoms, const double *R, const int *atomicNrs,
+                       double *F, double *U, double *variance,
+                       const double *box) {
+  variance = nullptr;
+  Eigen::MatrixXd positions =
+      Eigen::Map<Eigen::MatrixXd>(const_cast<double *>(R), nAtoms, 3);
+  Eigen::MatrixXd boxx =
+      Eigen::Map<Eigen::MatrixXd>(const_cast<double *>(box), 3, 3);
+  Eigen::VectorXi atmnmrs =
+      Eigen::Map<Eigen::VectorXi>(const_cast<int *>(atomicNrs), nAtoms);
+  py::object atoms = this->ase.attr("Atoms")(
+      "symbols"_a = atmnmrs, "positions"_a = positions, "cell"_a = boxx);
+  atoms.attr("set_calculator")(this->calc);
+  double py_e = py::cast<double>(atoms.attr("get_potential_energy")());
+  Eigen::MatrixXd py_force =
+      py::cast<Eigen::MatrixXd>(atoms.attr("get_forces")());
+
+  // Populate the output parameters
+  *U = py_e;
+  for (long i = 0; i < nAtoms; ++i) {
+    F[3 * i] = py_force(i, 0);
+    F[3 * i + 1] = py_force(i, 1);
+    F[3 * i + 2] = py_force(i, 2);
+  }
+  return;
+}
