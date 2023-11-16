0465f580c494cd211822b702f7ae2f534fd62ff8
Rohit Goswami <rog32@hi.is>
2023-11-16 04:55:26 +0000

    ENH: Add the ASE_ORCA interface
    Works in parallel, easy to extend, gets the right units

---
+++ client/potentials/ASE_ORCA/ASE_ORCA.h
@@ -0,0 +1,36 @@
+//-----------------------------------------------------------------------------------
+// eOn is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// A copy of the GNU General Public License is available at
+// http://www.gnu.org/licenses/
+//-----------------------------------------------------------------------------------
+
+#pragma once
+#define PYBIND11_DETAILED_ERROR_MESSAGES
+
+#include "../../Potential.h"
+
+#include <pybind11/eigen.h>
+#include <pybind11/embed.h>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+using namespace pybind11::literals; // to bring in the `_a` literal
+
+class ASEOrcaPot : public Potential {
+
+private:
+  py::object calc;
+  py::object ase;
+
+public:
+  ASEOrcaPot(shared_ptr<Parameters> a_params);
+
+  // Functions
+  void force(long nAtoms, const double *R, const int *atomicNrs,
+             double *F, double *U, double *variance,
+             const double *box) override;
+};
