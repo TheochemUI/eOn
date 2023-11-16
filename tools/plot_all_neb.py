91fd2d2674ff944a5dfb6c3dbb203a2e8763577a
Rohit Goswami <rog32@hi.is>
2023-11-16 20:39:53 +0000

    MAINT: Minor cleanup, take all data paths

--- tools/plot_all_neb.py
+++ tools/plot_all_neb.py
@@ -7,7 +7,7 @@ from cmcrameri import cm
 plt.style.use("bmh")
 
 # List all files matching the pattern 'neb_***.dat', excluding the last one
-file_paths = glob.glob("neb_*.dat")[:-1]
+file_paths = glob.glob("neb_*.dat")
 file_paths.sort()  # Sort the file paths to ensure correct order
 
 # Set up your figure and axes
@@ -26,7 +26,7 @@ for idx, file_path in enumerate(file_paths):
 
     # Cubic spline interpolation
     rc_fine = np.linspace(rc.min(), rc.max(), num=100)
-    spl = splrep(rc, energy, k = 3)
+    spl = splrep(rc, energy, k=3)
     spl_y = splev(rc_fine, spl)
 
     # Color specification using colormap
@@ -38,8 +38,12 @@ for idx, file_path in enumerate(file_paths):
     alpha = 1 if idx == 0 or idx == num_files - 1 else 0.5
 
     # Plotting
-    ax.plot(rc_fine, spl_y, color=color, alpha=alpha)
-    ax.plot(rc, energy, linestyle="", marker="o", color=color, alpha=alpha)
+    if idx == num_files - 1:
+        ax.plot(rc_fine, spl_y, color="red", alpha=alpha)
+        ax.plot(rc, energy, linestyle="", marker="o", color="red", alpha=alpha)
+    else:
+        ax.plot(rc_fine, spl_y, color=color, alpha=alpha)
+        ax.plot(rc, energy, linestyle="", marker="o", color=color, alpha=alpha)
 
 # Labels and adjustments
 ax.set_xlabel("Reaction Coordinate, ($\AA$)")
@@ -49,7 +53,7 @@ ax.set_ylim(0, ax.get_ylim()[1])
 ax.set_xlim(0, ax.get_xlim()[1])
 ax.set_facecolor("gray")
 plt.title("NEB paths over optimization steps")
-plt.tight_layout(pad=0.1)
+plt.tight_layout(pad=0.2)
 plt.subplots_adjust(left=0.20)
 
 # Adding colorbar
