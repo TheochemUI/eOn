f7e14d6c3707446c8353030bf64b1cfc2ceb35e3
Rohit Goswami <rog32@hi.is>
2023-11-19 12:27:41 +0000

    ENH: Add more plotting scripts

---
+++ tools/plot_hist_opttime.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+# Initialize variables
+scg_counts = []
+current_count = 0
+in_section = False
+
+# Read and process the file
+with open('eon_orca_b3lyp_def2_svp_gpneb_retry', 'r') as file:
+    for line in file:
+        if "optimize time:" in line:
+            if in_section:
+                # End of current section, save count
+                scg_counts.append(current_count)
+                current_count = 0
+            in_section = True
+        elif "SCG iteration:" in line and in_section:
+            current_count += 1
+
+# Add the last section if file doesn't end with "optimize time:"
+if current_count > 0:
+    scg_counts.append(current_count)
+
+# Create histogram
+plt.hist(scg_counts, bins=len(set(scg_counts)), edgecolor='black')
+plt.xlabel('SCG Iteration Count')
+plt.ylabel('Frequency')
+plt.title('Histogram of SCG Iterations per Optimization Time Section')
+plt.show()
+
---
+++ tools/plot_opt_time.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import re
+from matplotlib import offsetbox
+
+# Step 1: Read the file and extract time values
+time_values = []
+with open("eon_orca_b3lyp_def2_svp_gpneb_retry", "r") as file:
+    for line in file:
+        match = re.search(r"optimize time: (\d+\.\d+)s", line)
+        if match:
+            time_values.append(float(match.group(1)))
+
+# Step 2: Prepare data for plotting
+cumulative_time = sum(time_values)
+occurrences = list(range(1, len(time_values) + 1))
+
+# Step 3: Plot the data and add annotations
+plt.plot(occurrences, time_values, marker='o')
+# Step 4: Add annotations and adjust positions
+# for i, time in enumerate(time_values):
+#     annotation = plt.annotate(f'{time}s',
+#                               (occurrences[i], time_values[i]),
+#                               xytext=(5, 5),  # Slight offset
+#                               textcoords='offset points')
+#     # Adjust the position of the annotation to avoid overlap
+#     offsetbox.AnnotationBbox(annotation, (occurrences[i], time_values[i]))
+
+plt.xlabel('Occurrence')
+plt.ylabel('Time (s)')
+plt.title(f'Optimize Time per Occurrence - Cumulative Time: {cumulative_time:.2f}s')
+plt.grid(True)
+plt.show()
