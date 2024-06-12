f24ef9d2b3324f0810b68dc287a186142a5777aa
Rohit Goswami <rog32@hi.is>
2024-06-12 02:20:18 +0000

    MAINT: Be more unixy about envvars

--- client/EnvHelpers.hpp
+++ client/EnvHelpers.hpp
@@ -4,8 +4,8 @@
 #include <string>
 
 namespace helper_functions {
-std::string get_value_from_param_or_env(const std::string &param_value,
-                                        const char *env_variable,
+std::string get_value_from_env_or_param(const char *env_variable,
+                                        const std::string &param_value,
                                         const std::string &default_value = "",
                                         const std::string &warning_message = "",
                                         const bool is_mandatory = false);
