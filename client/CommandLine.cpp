9912d72569fba6c3b9e9e8a1095089c95264de31
Rohit Goswami <rog32@hi.is>
2024-06-12 03:11:04 +0000

    ENH: Rework to a newer CLI
    On the plus side, should now work on Windows...

--- client/CommandLine.cpp
+++ client/CommandLine.cpp
@@ -1,162 +1,163 @@
 #include "CommandLine.h"
-#include "ConjugateGradients.h"
 #include "Matter.h"
 #include "Parameters.h"
 #include "Potential.h"
 #include "version.h"
 
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <memory>
 #include <string>
-#include <unistd.h>
 
 using namespace std;
 
 void singlePoint(std::unique_ptr<Matter> matter) {
-  printf("Energy:         %.10f\n", matter->getPotentialEnergy());
+  std::cout << "Energy:         " << std::fixed << std::setprecision(10)
+            << matter->getPotentialEnergy() << std::endl;
   std::cout << "(free) Forces:         \n" << matter->getForcesFree() << "\n";
-  printf("Max atom force: %.10g\n", matter->maxForce());
+  std::cout << "Max atom force: " << std::scientific << matter->maxForce()
+            << std::endl;
 }
 
-void minimize(std::unique_ptr<Matter> matter, string confileout) {
+void minimize(std::unique_ptr<Matter> matter, const string &confileout) {
   matter->relax(false, false);
-  if (confileout.length() > 0) {
-    printf("saving relaxed structure to %s\n", confileout.c_str());
+  if (!confileout.empty()) {
+    std::cout << "Saving relaxed structure to " << confileout << std::endl;
   } else {
-    printf("no output file specified, not saving\n");
+    std::cout << "No output file specified, not saving" << std::endl;
   }
   matter->matter2con(confileout);
 }
 
-void usage(void) {
-  fprintf(stderr, "Usage: eonclient [options] inputConfile [outputConfile]\n");
-  char fmtStr[] = "  -%-2s %s\n";
-
-  fprintf(stderr, "Job Type:\n");
-  fprintf(stderr, fmtStr, "v", "print version information");
-  fprintf(stderr, fmtStr, "m",
-          "Minimization of inputConfile saves to outputConfile");
-  fprintf(stderr, fmtStr, "s", "Single point energy of inputConfile");
-  fprintf(stderr, fmtStr, "c",
-          "Compare structures of inputConfile to outputConfile");
-  fprintf(stderr, fmtStr, "o", "Optimization method [default: qm]");
-  fprintf(stderr, fmtStr, "f", "Convergence force [default: 0.001]");
-  fprintf(stderr, fmtStr, "t", "Distance tolerance [default: 0.1]");
-
-  fprintf(stderr, "Required Options:\n");
-  fprintf(stderr, fmtStr, "p", "The potential (e.g. qsc, lj, eam_al)");
-}
-
 void commandLine(int argc, char **argv) {
-// no getopt on windows
-#ifndef WIN32
-  int c;
   bool sflag = false, mflag = false, pflag = false, cflag = false;
   double optConvergedForce = 0.001;
-
   string potential;
   string confile;
   string optimizer("cg");
 
   auto params = std::make_shared<Parameters>();
 
-  while ((c = getopt(argc, argv, "chsmp:f:o:t:v")) != -1) {
-    switch (c) {
-    case 'c':
-      cflag = true;
-      break;
-    case 's':
-      sflag = true;
-      break;
-    case 'm':
+  cxxopts::Options options("eonclient", "The eON client");
+  options.add_options()("v,version", "Print version information")(
+      "m,minimize", "Minimization of inputConfile saves to outputConfile")(
+      "s,single", "Single point energy of inputConfile")(
+      "c,compare", "Compare structures of inputConfile to outputConfile")(
+      "o,optimizer", "Optimization method",
+      cxxopts::value<std::string>()->default_value("cg"))(
+      "f,force", "Convergence force",
+      cxxopts::value<double>()->default_value("0.001"))(
+      "t,tolerance", "Distance tolerance",
+      cxxopts::value<double>()->default_value("0.1"))(
+      "p,potential", "The potential (e.g. qsc, lj, eam_al)",
+      cxxopts::value<std::string>())("h,help", "Print usage");
+
+  try {
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help")) {
+      std::cout << options.help() << std::endl;
+      exit(0);
+    }
+
+    if (result.count("version")) {
+      std::cout << "eonclient version r" << VERSION << std::endl;
+      std::cout << "          compiled " << BUILD_DATE << std::endl;
+      exit(0);
+    }
+
+    if (result.count("minimize")) {
       mflag = true;
-      break;
-    case 'p':
+    }
+
+    if (result.count("single")) {
+      sflag = true;
+    }
+
+    if (result.count("compare")) {
+      cflag = true;
+    }
+
+    if (result.count("potential")) {
       pflag = true;
-      potential = optarg;
-      break;
-    case 't':
-      params->distanceDifference = atof(optarg);
-      break;
-    case 'o':
-      optimizer = optarg;
-      break;
-    case 'f':
-      cout << optarg << endl;
-      optConvergedForce = atof(optarg);
-      break;
-    case 'h':
-      usage();
-      exit(0);
-    case 'v':
-      printf("eonclient version r%s\n", VERSION);
-      printf("          compiled %s\n", BUILD_DATE);
-      exit(0);
-    case '?':
-      if (optopt == 'p')
-        fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-      else
-        fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-      usage();
+      potential = result["potential"].as<std::string>();
+    }
+
+    if (result.count("optimizer")) {
+      optimizer = result["optimizer"].as<std::string>();
+    }
+
+    if (result.count("force")) {
+      optConvergedForce = result["force"].as<double>();
+    }
+
+    if (result.count("tolerance")) {
+      params->distanceDifference = result["tolerance"].as<double>();
+    }
+
+    if (sflag && mflag) {
+      std::cerr << "Cannot specify both minimization and single point"
+                << std::endl;
       exit(2);
     }
-  }
 
-  if (sflag && mflag) {
-    fprintf(stderr, "Cannot specify both minimization and single point\n");
-    exit(2);
-  }
-
-  if (!pflag && (sflag || mflag)) {
-    fprintf(stderr, "Must specify a potential\n");
-    exit(2);
-  } else if (!cflag) {
-    for (string::size_type i = 0; i < potential.length(); ++i) {
-      potential[i] = tolower(potential[i]);
+    if (!pflag && (sflag || mflag)) {
+      std::cerr << "Must specify a potential" << std::endl;
+      exit(2);
     }
-  }
 
-  int extraArgs = argc - optind;
+    if (!cflag) {
+      for (auto &ch : potential) {
+        ch = tolower(ch);
+      }
+    }
 
-  if (extraArgs < 1) {
-    fprintf(stderr, "Only one non-option argument is allowed: the con file\n");
-    exit(2);
-  } else {
-    confile = argv[optind];
-  }
-
-  if (!cflag) {
-    params->potential = helper_functions::getPotentialType(potential);
-  }
-
-  if (!sflag) {
-    params->optMethod = helper_functions::getOptType(optimizer);
-    params->optConvergedForce = optConvergedForce;
-  }
-
-  auto pot = helper_functions::makePotential(params);
-  auto matter = std::make_unique<Matter>(pot, params);
-  auto matter2 = std::make_unique<Matter>(pot, params);
-  matter->con2matter(confile);
-
-  string confileout;
-  if (extraArgs == 2) {
-    confileout = argv[optind + 1];
-    if (cflag)
-      matter2->con2matter(confileout);
-  }
-
-  if (sflag) {
-    singlePoint(std::move(matter));
-  } else if (mflag) {
-    minimize(std::move(matter), confileout);
-  } else if (cflag) {
-    params->checkRotation = true;
-    if (matter->compare(*matter2, true)) {
-      printf("structures match\n");
+    auto unmatched = result.unmatched();
+    if (unmatched.size() < 1) {
+      std::cerr << "At least one non-option argument is required: the con file"
+                << std::endl;
+      exit(2);
     } else {
-      printf("structures do not match\n");
+      confile = unmatched[0];
     }
-  }
 
-#endif
+    if (!cflag) {
+      params->potential = helper_functions::getPotentialType(potential);
+    }
+
+    if (!sflag) {
+      params->optMethod = helper_functions::getOptType(optimizer);
+      params->optConvergedForce = optConvergedForce;
+    }
+
+    auto pot = helper_functions::makePotential(params);
+    auto matter = std::make_unique<Matter>(pot, params);
+    auto matter2 = std::make_unique<Matter>(pot, params);
+    matter->con2matter(confile);
+
+    string confileout;
+    if (unmatched.size() == 2) {
+      confileout = unmatched[1];
+      if (cflag)
+        matter2->con2matter(confileout);
+    }
+
+    if (sflag) {
+      singlePoint(std::move(matter));
+    } else if (mflag) {
+      minimize(std::move(matter), confileout);
+    } else if (cflag) {
+      params->checkRotation = true;
+      if (matter->compare(*matter2, true)) {
+        std::cout << "Structures match" << std::endl;
+      } else {
+        std::cout << "Structures do not match" << std::endl;
+      }
+    }
+  } catch (const cxxopts::exceptions::exception &e) {
+    std::cerr << "Error parsing options: " << e.what() << std::endl;
+    std::cerr << options.help() << std::endl;
+    exit(1);
+  }
 }
