fb8df6d36777131828277bfa359f055f42c9565e
Rohit Goswami <rog32@hi.is>
2024-06-18 01:40:59 +0000

    MAINT: Cleanup LJ a bit more

--- client/potentials/LJ/LJ.h
+++ client/potentials/LJ/LJ.h
@@ -18,32 +18,28 @@
 
 /** Lennard Jones potential.*/
 class LJ : public Potential {
-
 private:
-  //	Variables
   double u0;
   double cuttOffR;
   double psi;
-
   double cuttOffU;
 
 public:
-  // Functions
-  // constructor
   LJ(std::shared_ptr<Parameters> params)
-      : Potential(PotType::LJ, params),
-        u0{1.0},
-        cuttOffR{15.0},
-        psi{1.0} {};
-  // LJ(double r0Recieved, double u0Recieved, double psiRecieved);
+      : LJ(PotType::LJ, params, 1.0, 15.0, 1.0) {}
 
-  ~LJ();
+  LJ(PotType ptype, std::shared_ptr<Parameters> params, double u0,
+     double cuttOffR, double psi)
+      : Potential(ptype, params),
+        u0{u0},
+        cuttOffR{cuttOffR},
+        psi{psi} {}
 
-  // Just to satisfy interface
-  void cleanMemory();
+  ~LJ() = default;
 
   void force(long N, const double *R, const int *atomicNrs, double *F,
              double *U, double *variance, const double *box) override;
+
   void setParameters(double r0Recieved, double u0Recieved, double psiRecieved);
 };
 #endif
