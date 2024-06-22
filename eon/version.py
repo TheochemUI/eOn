de37622e80f6c7f2ff7378c85e391fbfb08a9239
Rohit Goswami <rog32@hi.is>
2024-06-22 01:57:06 +0000

    MAINT: No need for svn...

--- eon/version.py
+++ eon/version.py
@@ -4,11 +4,11 @@ from os.path import abspath, dirname
 
 def version():
     path = dirname(abspath( __file__ ))
-    try:
-        output = subprocess.check_output(["svnversion",str(path)], stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as grepexc:
-        return 'unknown'
-    return 'svn revision %s' % output.decode('ascii')
+    # try:
+    #     output = subprocess.check_output(["svnversion",str(path)], stderr=subprocess.STDOUT)
+    # except subprocess.CalledProcessError as grepexc:
+    #     return 'unknown'
+    # return 'svn revision %s' % output.decode('ascii')
 
 
 if __name__ == '__main__':
