33a92409feb9e1201369a426695403c373d46a5b
Rohit Goswami <rog32@hi.is>
2024-06-22 02:12:22 +0000

    MAINT: Simplify and fix fpe_handler
    For M1 macs

--- client/fpe_handler.cpp
+++ client/fpe_handler.cpp
@@ -13,13 +13,8 @@ void enableFPE() {
   _controlfp_s(nullptr, 0, _MCW_EM);
   _controlfp_s(nullptr, ~(_EM_ZERODIVIDE | _EM_INVALID | _EM_OVERFLOW),
                _MCW_EM);
-#elif defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
-#if defined(__linux__)
-  // Enable floating-point exceptions on Linux
-  feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
-#elif defined(__APPLE__)
-#ifdef __arm__
-  // Enable floating-point exceptions on ARM macOS (M1/M2)
+#elif defined(__unix__) || (defined(__APPLE__) && defined(__aarch64__))
+  // Enable floating-point exceptions on Unixes and M1 MacOS
   feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
 #elif defined(__x86_64__)
   // Enable floating-point exceptions on Intel macOS
@@ -27,14 +22,6 @@ void enableFPE() {
   fegetenv(&fenv);
   fenv.__mxcsr &= ~(_MM_MASK_INVALID | _MM_MASK_DIV_ZERO | _MM_MASK_OVERFLOW);
   fesetenv(&fenv);
-#else
-  std::cerr << "FPE trapping not supported on this Apple platform."
-            << std::endl;
-#endif
-#else
-  std::cerr << "FPE trapping not supported on this Unix-like platform."
-            << std::endl;
-#endif
 #else
   std::cerr << "FPE trapping not supported on this platform." << std::endl;
 #endif
