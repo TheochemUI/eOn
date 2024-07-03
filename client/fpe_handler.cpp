fe94a821a1066ea779270be39c0ad7313fc20c7b
Rohit Goswami <rog32@hi.is>
2024-07-03 09:40:26 +0000

    MAINT: Switch to fpe_handler

--- client/fpe_handler.cpp
+++ client/fpe_handler.cpp
@@ -7,6 +7,10 @@
 #include <float.h>
 #endif
 
+#if defined(__APPLE__) && defined(__x86_64__)
+#include <xmmintrin.h>
+#endif
+
 void enableFPE() {
 #ifdef _WIN32
   // Enable floating-point exceptions on Windows
@@ -14,14 +18,13 @@ void enableFPE() {
   _controlfp_s(nullptr, ~(_EM_ZERODIVIDE | _EM_INVALID | _EM_OVERFLOW),
                _MCW_EM);
 #elif defined(__unix__) || (defined(__APPLE__) && defined(__aarch64__))
-  // Enable floating-point exceptions on Unixes and M1 MacOS
+  // Enable floating-point exceptions on Unix and ARM macOS
   feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
-#elif defined(__x86_64__)
+#elif defined(__APPLE__) && defined(__x86_64__)
   // Enable floating-point exceptions on Intel macOS
-  fenv_t fenv;
-  fegetenv(&fenv);
-  fenv.__mxcsr &= ~(_MM_MASK_INVALID | _MM_MASK_DIV_ZERO | _MM_MASK_OVERFLOW);
-  fesetenv(&fenv);
+  _MM_SET_EXCEPTION_MASK(
+      _MM_MASK_MASK &
+      ~(_MM_MASK_INVALID | _MM_MASK_DIV_ZERO | _MM_MASK_OVERFLOW));
 #else
   std::cerr << "FPE trapping not supported on this platform." << std::endl;
 #endif
