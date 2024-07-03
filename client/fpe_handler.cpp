722840c20be145578f021a50703a55af97e49f8f
Rohit Goswami <rog32@hi.is>
2024-07-03 09:53:42 +0000

    MAINT: Try to handle things consistently

--- client/fpe_handler.cpp
+++ client/fpe_handler.cpp
@@ -1,6 +1,8 @@
 #include "fpe_handler.h"
 
 #include <cfenv>
+#include <cmath>
+#include <csignal>
 #include <iostream>
 
 #ifdef _WIN32
@@ -11,15 +13,37 @@
 #include <xmmintrin.h>
 #endif
 
+static void fpe_signal_handler(int sig, siginfo_t *sip, void *scp) {
+  int fe_code = sip->si_code;
+  std::cerr << "Floating point exception: ";
+
+  if (fe_code == FPE_FLTDIV)
+    std::cerr << "Division by zero" << std::endl;
+  else if (fe_code == FPE_FLTINV)
+    std::cerr << "Invalid operation" << std::endl;
+  else if (fe_code == FPE_FLTOVF)
+    std::cerr << "Overflow" << std::endl;
+  else
+    std::cerr << "Code detected: " << fe_code << std::endl;
+
+  std::abort();
+}
+
 void enableFPE() {
 #ifdef _WIN32
   // Enable floating-point exceptions on Windows
   _controlfp_s(nullptr, 0, _MCW_EM);
   _controlfp_s(nullptr, ~(_EM_ZERODIVIDE | _EM_INVALID | _EM_OVERFLOW),
                _MCW_EM);
-#elif defined(__unix__) || (defined(__APPLE__) && defined(__aarch64__))
-  // Enable floating-point exceptions on Unix and ARM macOS
+#elif defined(__unix__)
+  // Enable floating-point exceptions on Unix
   feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+#elif defined(__APPLE__) && defined(__aarch64__)
+  // Enable floating-point exceptions on ARM macOS
+  fenv_t env;
+  fegetenv(&env);
+  env.__fpsr &= ~(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+  fesetenv(&env);
 #elif defined(__APPLE__) && defined(__x86_64__)
   // Enable floating-point exceptions on Intel macOS
   _MM_SET_EXCEPTION_MASK(
@@ -28,4 +52,11 @@ void enableFPE() {
 #else
   std::cerr << "FPE trapping not supported on this platform." << std::endl;
 #endif
+
+  // Register signal handler
+  struct sigaction act;
+  act.sa_sigaction = fpe_signal_handler;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = SA_SIGINFO;
+  sigaction(SIGFPE, &act, nullptr);
 }
