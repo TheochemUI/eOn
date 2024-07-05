d0507b26b74fe7cbe31c699837034ef2b266053e
Rohit Goswami <rog32@hi.is>
2024-07-05 13:20:04 +0000

    MAINT: Speed up build by a bit

---
+++ client/toml.cpp
@@ -0,0 +1,4 @@
+#define TOML_IMPLEMENTATION
+#include "thirdparty/toml.hpp"
+// Kanged from Seldon-code
+// Speeding up compilation following: https://marzer.github.io/tomlplusplus/#mainpage-example-speed-up-compilation
