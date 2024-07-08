5e66a66466f1437bf155ed8c38f382cf360bfb84
Rohit Goswami <rog32@hi.is>
2024-07-08 20:21:08 +0000

    MAINT: Add license to Fortran files

--- client/potentials/Aluminum/fofrhoDblexp.f
+++ client/potentials/Aluminum/fofrhoDblexp.f
@@ -1,3 +1,13 @@
+C This file is part of eON.
+C
+C SPDX-License-Identifier: BSD-3-Clause
+C
+C Copyright (c) 2010--present, eON Development Team
+C All rights reserved.
+C
+C Repo:
+C https://github.com/TheochemUI/eON
+
 c   Dblexp:    June 92
 c  This routine calculates FOFRHO
 c  Generalized to arbitrary order
--- client/potentials/Aluminum/gagafeDblexp.f
+++ client/potentials/Aluminum/gagafeDblexp.f
@@ -1,3 +1,13 @@
+C This file is part of eON.
+C
+C SPDX-License-Identifier: BSD-3-Clause
+C
+C Copyright (c) 2010--present, eON Development Team
+C All rights reserved.
+C
+C Repo:
+C https://github.com/TheochemUI/eON
+
 c   EAM version e:
 c   EAM version d:    Greg found bug in interactions between FPI chains.
 c   EAM version c:    tags 100-199 for fixed-endpoint FPI
