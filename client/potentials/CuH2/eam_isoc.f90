90e19e36dbf4cad53ec0fdf1f788d28f2f93b36f
Rohit Goswami <rog32@hi.is>
2024-07-08 20:21:08 +0000

    MAINT: Add license to Fortran files

--- client/potentials/CuH2/eam_isoc.f90
+++ client/potentials/CuH2/eam_isoc.f90
@@ -1,3 +1,12 @@
+! This file is part of eON.
+!
+! SPDX-License-Identifier: BSD-3-Clause
+!
+! Copyright (c) 2010--present, eON Development Team
+! All rights reserved.
+!
+! Repo:
+! https://github.com/TheochemUI/eON
   module eam_wrap
     use iso_c_binding, only: c_double, c_int
     use iso_fortran_env, only: real64
