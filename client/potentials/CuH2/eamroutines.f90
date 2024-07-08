dac4801a47229e7bfa5c27960526b1a74824b496
Rohit Goswami <rog32@hi.is>
2024-07-08 20:21:08 +0000

    MAINT: Add license to Fortran files

--- client/potentials/CuH2/eamroutines.f90
+++ client/potentials/CuH2/eamroutines.f90
@@ -1,3 +1,13 @@
+! This file is part of eON.
+!
+! SPDX-License-Identifier: BSD-3-Clause
+!
+! Copyright (c) 2010--present, eON Development Team
+! All rights reserved.
+!
+! Repo:
+! https://github.com/TheochemUI/eON
+
       SUBROUTINE force_eam(natms,ndim,box,R,F,U)
 
         IMPLICIT NONE
