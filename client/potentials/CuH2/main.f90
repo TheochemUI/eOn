6f2b62f71d3647ffa8188304b5144f0c99095dd2
Rohit Goswami <rog32@hi.is>
2024-07-08 20:21:08 +0000

    MAINT: Add license to Fortran files

--- client/potentials/CuH2/main.f90
+++ client/potentials/CuH2/main.f90
@@ -1,3 +1,12 @@
+! This file is part of eON.
+!
+! SPDX-License-Identifier: BSD-3-Clause
+!
+! Copyright (c) 2010--present, eON Development Team
+! All rights reserved.
+!
+! Repo:
+! https://github.com/TheochemUI/eON
 
       program readcon
 ! This program has been edited for matlab
