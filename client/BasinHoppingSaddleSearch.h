0487a6156b95a489b7430b1ba917cbb7957c6634
Rohit Goswami <rog32@hi.is>
2024-07-08 20:40:02 +0000

    MAINT: Add licenses to remaining files

--- client/BasinHoppingSaddleSearch.h
+++ client/BasinHoppingSaddleSearch.h
@@ -1,3 +1,14 @@
+/*
+** This file is part of eON.
+**
+** SPDX-License-Identifier: BSD-3-Clause
+**
+** Copyright (c) 2010--present, eON Development Team
+** All rights reserved.
+**
+** Repo:
+** https://github.com/TheochemUI/eON
+*/
 #pragma once
 #include "Eigen.h"
 #include "HelperFunctions.h"
