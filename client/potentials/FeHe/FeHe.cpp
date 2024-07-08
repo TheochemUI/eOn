60cd7f24ea1b35c8798a66094fc51e418267faa0
Rohit Goswami <rog32@hi.is>
2024-07-08 20:36:34 +0000

    MAINT: Update all GNU licenses to BSD-3

--- client/potentials/FeHe/FeHe.cpp
+++ client/potentials/FeHe/FeHe.cpp
@@ -1,12 +1,14 @@
-//-----------------------------------------------------------------------------------
-// eOn is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// A copy of the GNU General Public License is available at
-// http://www.gnu.org/licenses/
-//
+/*
+** This file is part of eON.
+**
+** SPDX-License-Identifier: BSD-3-Clause
+**
+** Copyright (c) 2010--present, eON Development Team
+** All rights reserved.
+**
+** Repo:
+** https://github.com/TheochemUI/eON
+*/
 //-----------------------------------------------------------------------------------
 #include "FeHe.h"
 #include <assert.h>
