e6db9446e82dce296586491b14ca337e10310348
Rohit Goswami <rog32@hi.is>
2024-07-08 20:40:02 +0000

    MAINT: Add licenses to remaining files

--- client/potentials/Water_Pt/Tip4p_Pt.cpp
+++ client/potentials/Water_Pt/Tip4p_Pt.cpp
@@ -1,3 +1,14 @@
+/*
+** This file is part of eON.
+**
+** SPDX-License-Identifier: BSD-3-Clause
+**
+** Copyright (c) 2010--present, eON Development Team
+** All rights reserved.
+**
+** Repo:
+** https://github.com/TheochemUI/eON
+*/
 #include "Tip4p_Pt.hpp"
 
 void Tip4p_Pt::force(long N, const double *R, const int *atomicNrs, double *F,
