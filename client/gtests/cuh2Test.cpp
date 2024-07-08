723257653a814dcc39a6ec1fd7fbda6a2a6033da
Rohit Goswami <rog32@hi.is>
2024-07-08 20:40:02 +0000

    MAINT: Add licenses to remaining files

--- client/gtests/cuh2Test.cpp
+++ client/gtests/cuh2Test.cpp
@@ -1,4 +1,15 @@
 /*
+** This file is part of eON.
+**
+** SPDX-License-Identifier: BSD-3-Clause
+**
+** Copyright (c) 2010--present, eON Development Team
+** All rights reserved.
+**
+** Repo:
+** https://github.com/TheochemUI/eON
+*/
+/*
  * CuH2Test.cpp
  *
  *  Created on: 15 Nov 2022
