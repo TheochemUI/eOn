cf1faa45751dc863314fa5be36ac531e6f5aa6c2
Rohit Goswami <rog32@hi.is>
2024-07-10 17:22:12 +0000

    MAINT,TMP: Minimial compilation set

--- client/PointJob.cpp
+++ client/PointJob.cpp
@@ -11,26 +11,27 @@
 */
 #include "PointJob.h"
 #include "Matter.h"
+#include "Potential.h"
 namespace eonc {
 std::vector<std::string> PointJob::run(void) {
   std::vector<std::string> returnFiles;
   std::string posInFilename("pos.con");
   std::string resultsFilename("results.dat");
   returnFiles.push_back(resultsFilename);
+  auto pot = helper_functions::makePotential(params);
 
-  auto pos = std::make_unique<Matter>(pot, params);
-  pos->con2matter(posInFilename);
+  Matter pos{Matter(pot, &params)};
+  pos.con2matter(posInFilename);
 
-  SPDLOG_LOGGER_DEBUG(log, "Energy:         {:.12f}",
-                      pos->getPotentialEnergy());
-  SPDLOG_LOGGER_DEBUG(log, "Max atom force: {:.12f}", pos->maxForce());
+  SPDLOG_LOGGER_DEBUG(log, "Energy:         {:.12f}", pos.getPotentialEnergy());
+  SPDLOG_LOGGER_DEBUG(log, "Max atom force: {:.12f}", pos.maxForce());
 
   std::shared_ptr<spdlog::logger> fileLogger;
   fileLogger = spdlog::basic_logger_mt("point", "results.dat", true);
 
   fileLogger->set_pattern("%v");
-  fileLogger->info("{:.12f} Energy", pos->getPotentialEnergy());
-  fileLogger->info("{:.12f} Max_Force", pos->maxForce());
+  fileLogger->info("{:.12f} Energy", pos.getPotentialEnergy());
+  fileLogger->info("{:.12f} Max_Force", pos.maxForce());
 
   spdlog::drop("point");
   fileLogger.reset();
