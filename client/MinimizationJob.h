c5a461589a00160767156b85716dbfe4e8abedb8
Rohit Goswami <rog32@hi.is>
2024-07-10 16:25:04 +0000

    ENH,MAINT: Move, refactor matterobjectivefunc

--- client/MinimizationJob.h
+++ client/MinimizationJob.h
@@ -12,8 +12,36 @@
 #pragma once
 
 #include "Job.h"
-#include "Parameters.h"
+#include "Matter.h"
+#include "ObjectiveFunction.h"
 namespace eonc {
+
+class MinObjF : public ObjectiveFunction {
+private:
+  Matter &_mat;
+  ObjectiveFunction::Conv _metric{ObjectiveFunction::Conv::NORM};
+  double _convForce = 1e-6;
+
+public:
+  MinObjF(Matter &mat_a)
+      : ObjectiveFunction(),
+        _mat{mat_a} {}
+  MinObjF(Matter &mat_a, ObjectiveFunction::Conv metric_a, double cforce_a)
+      : ObjectiveFunction(),
+        _mat{mat_a},
+        _metric{metric_a},
+        _convForce{cforce_a} {}
+  ~MinObjF() = default;
+  double getEnergy() const override;
+  VectorType getGradient(bool fdstep = false) const override;
+  VectorType getPositions() const override;
+  int degreesOfFreedom() const override;
+  bool isConverged() const override;
+  double getConvergence() const override;
+  void setPositions(VectorType x) override;
+  VectorType difference(VectorType a, VectorType b) override;
+};
+
 class MinimizationJob : public Job {
 public:
   MinimizationJob(std::unique_ptr<Parameters> parameters)
