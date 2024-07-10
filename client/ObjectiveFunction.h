65630589aafcb96ad0b948dd5a58cb552ab49030
Rohit Goswami <rog32@hi.is>
2024-07-10 15:40:52 +0000

    MAINT: Streamline ObjectiveFunction

--- client/ObjectiveFunction.h
+++ client/ObjectiveFunction.h
@@ -11,30 +11,20 @@
 */
 #pragma once
 
-#include "Matter.h"
+#include "Eigen.h"
 
-#ifdef WITH_GPRD
-#include "AtomicGPDimer.h"
-#endif
 namespace eonc {
 class ObjectiveFunction {
-protected:
-  std::shared_ptr<Matter> matter;
-  std::shared_ptr<Parameters> params;
-
 public:
-  ObjectiveFunction(std::shared_ptr<Matter> matterPassed,
-                    std::shared_ptr<Parameters> paramsPassed)
-      : matter{matterPassed},
-        params{paramsPassed} {}
+  ObjectiveFunction() {}
   virtual ~ObjectiveFunction() {}
-  virtual double getEnergy() = 0;
-  virtual VectorType getGradient(bool fdstep = false) = 0;
+  virtual double getEnergy() const = 0;
+  virtual VectorType getGradient(bool fdstep = false) const = 0;
   virtual void setPositions(VectorType x) = 0;
-  virtual VectorType getPositions() = 0;
-  virtual int degreesOfFreedom() = 0;
-  virtual bool isConverged() = 0;
-  virtual double getConvergence() = 0;
+  virtual VectorType getPositions() const = 0;
+  virtual int degreesOfFreedom() const = 0;
+  virtual bool isConverged() const = 0;
+  virtual double getConvergence() const = 0;
   virtual VectorType difference(VectorType a, VectorType b) = 0;
 };
 
