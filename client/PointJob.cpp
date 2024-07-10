a561029fa482773d01d33f7f5c81bcdc4dfd4778
Rohit Goswami <rog32@hi.is>
2024-07-10 23:35:49 +0000

    ENH: Rework to take a matter object in Job

--- client/PointJob.cpp
+++ client/PointJob.cpp
@@ -11,29 +11,24 @@
 */
 #include "PointJob.h"
 #include "Matter.h"
-#include "Potential.h"
 namespace eonc {
 std::vector<std::string> PointJob::run(void) {
   std::vector<std::string> returnFiles;
   std::string resultsFilename("results.dat");
   returnFiles.push_back(resultsFilename);
-  auto pot = helper_functions::makePotential(params);
-
-  // TODO(rg):: Use parameters
-  Matter pos{Matter(pot)};
-  pos.con2matter(params.main.conFilename);
 
   // SPDLOG_LOGGER_DEBUG(log, "(free) Forces:         {:.12f}",
-  //                     fmt::streamed(pos.getForcesFree()));
-  SPDLOG_LOGGER_DEBUG(log, "Energy:         {:.12f}", pos.getPotentialEnergy());
-  SPDLOG_LOGGER_DEBUG(log, "Max atom force: {:.12e}", pos.maxForce());
+  //                     fmt::streamed(_mat.getForcesFree()));
+  SPDLOG_LOGGER_DEBUG(log, "Energy:         {:.12f}",
+                      _mat.getPotentialEnergy());
+  SPDLOG_LOGGER_DEBUG(log, "Max atom force: {:.12e}", _mat.maxForce());
 
   std::shared_ptr<spdlog::logger> fileLogger;
   fileLogger = spdlog::basic_logger_mt("point", "results.dat", true);
 
   fileLogger->set_pattern("%v");
-  fileLogger->info("{:.12f} Energy", pos.getPotentialEnergy());
-  fileLogger->info("{:.12f} Max_Force", pos.maxForce());
+  fileLogger->info("{:.12f} Energy", _mat.getPotentialEnergy());
+  fileLogger->info("{:.12f} Max_Force", _mat.maxForce());
 
   spdlog::drop("point");
   fileLogger.reset();
