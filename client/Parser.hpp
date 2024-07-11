76534f8585616a868eac47a24c2aaa1ee64da8d9
Rohit Goswami <rog32@hi.is>
2024-07-11 02:28:54 +0000

    MAINT: Add release conditionals

--- client/Parser.hpp
+++ client/Parser.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#ifndef NDEBUG
 #include "fmt/core.h"
+#endif
+
 #include "magic_enum/magic_enum.hpp"
 #include "thirdparty/toml.hpp"
 #include <string>
@@ -11,17 +14,21 @@ toml::table loadTOML(const std::string &fname);
 void config_section(const toml::table &conf, const std::string_view key);
 
 template <typename T> T get_enum_toml(const toml::node_view<toml::node> &conf) {
+#ifndef NDEBUG
   if (!conf.is_string()) {
     throw std::logic_error("Node not found in TOML");
   }
+#endif
 
   auto cval = conf.value<std::string>();
   auto enum_value =
       magic_enum::enum_cast<T>(cval.value(), magic_enum::case_insensitive);
+#ifndef NDEBUG
   if (!enum_value.has_value()) {
     throw std::runtime_error(
         fmt::format("{} does not map to a valid option", cval.value()));
   }
+#endif
 
   return enum_value.value();
 }
