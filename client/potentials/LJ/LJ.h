a2356c683277c59d32c47e4612b7fe39a22a86ea
Rohit Goswami <rog32@hi.is>
2024-07-12 00:15:44 +0000

    ENH: Refactor with structs to be more explicit

--- client/potentials/LJ/LJ.h
+++ client/potentials/LJ/LJ.h
@@ -45,8 +45,7 @@ public:
 
   ~LJ() = default;
 
-  void force(long N, const double *R, const int *atomicNrs, double *F,
-             double *U, double *variance, const double *box) override;
+  void force(const ForceInput &params, ForceOut *efvdat) override;
 
   void setParameters(double r0Recieved, double u0Recieved, double psiRecieved);
 };
