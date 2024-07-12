68eab6489607db4d72ca865a4c48ce594445dfd0
Rohit Goswami <rog32@hi.is>
2024-07-12 18:09:09 +0000

    ENH: Rework Morse for new structure

--- client/potentials/Morse/Morse.cpp
+++ client/potentials/Morse/Morse.cpp
@@ -11,6 +11,7 @@
 */
 
 #include "Morse.h"
+#include "../../PotHelpers.hpp"
 #include <cassert>
 #include <math.h>
 namespace eonc {
@@ -29,56 +30,43 @@ namespace eonc {
       http://en.wikipedia.org/wiki/Morse_potential .
       */
 
-void Morse::setParameters(double De, double a, double re, double cutoff) {
-  re_ = re;
-  De_ = De;
-  a_ = a;
-  cutoff_ = cutoff;
-  double f;
-  // TODO(rg): Why call the energy and forces here?
-  morse(cutoff, energyCutoff_, f);
+void Morse::setParameters(const Params &mpar) {
+  re_ = mpar.re;
+  De_ = mpar.De;
+  a_ = mpar.a;
+  cutoff_ = mpar.cutoff;
 }
 
-// pointer to number of atoms, pointer to array of positions
-// pointer to array of forces, pointer to internal energy
-// address to supercell size
-void Morse::force(long N, const double *R, const int *, double *F, double *U,
-                  double *variance, const double *box) {
-  variance = nullptr;
-  assert((box[0] > 0) and (box[4] > 0) and (box[8] > 0));
+void Morse::forceImpl(const ForceInput &params, ForceOut *efvd) {
+#ifdef EON_CHECKS
+  eonc::pot::checkParams(params);
+  eonc::pot::zeroForceOut(params.nAtoms, efvd);
+#endif
   double diffR = 0, diffRX{0}, diffRY{0}, diffRZ{0};
-  *U = 0;
-  for (int i = 0; i < N; i++) {
-    F[3 * i] = 0;
-    F[3 * i + 1] = 0;
-    F[3 * i + 2] = 0;
-  };
+
   // Initializing en
-  for (int i = 0; i < N; i++) {
-    for (int j = i + 1; j < N; j++) {
-      diffRX = R[3 * i] - R[3 * j];
-      diffRY = R[3 * i + 1] - R[3 * j + 1];
-      diffRZ = R[3 * i + 2] - R[3 * j + 2];
-      diffRX =
-          diffRX -
-          box[0] *
-              floor(diffRX / box[0] +
-                    0.5); // floor = largest integer value less than argument
-      diffRY = diffRY - box[4] * floor(diffRY / box[4] + 0.5);
-      diffRZ = diffRZ - box[8] * floor(diffRZ / box[8] + 0.5);
+  for (size_t i = 0; i < params.nAtoms; i++) {
+    for (size_t j = i + 1; j < params.nAtoms; j++) {
+      diffRX = params.pos[3 * i] - params.pos[3 * j];
+      diffRY = params.pos[3 * i + 1] - params.pos[3 * j + 1];
+      diffRZ = params.pos[3 * i + 2] - params.pos[3 * j + 2];
+      // floor = largest integer value less than argument
+      diffRX = diffRX - params.box[0] * floor(diffRX / params.box[0] + 0.5);
+      diffRY = diffRY - params.box[4] * floor(diffRY / params.box[4] + 0.5);
+      diffRZ = diffRZ - params.box[8] * floor(diffRZ / params.box[8] + 0.5);
       diffR = sqrt(diffRX * diffRX + diffRY * diffRY + diffRZ * diffRZ);
       assert(std::isnormal(diffR));
       if (diffR < cutoff_) {
         double force{0}, energy{0};
         morse(diffR, energy, force);
-        *U += energy;
-        F[3 * i] += force * diffRX / diffR;
-        F[3 * i + 1] += force * diffRY / diffR;
-        F[3 * i + 2] += force * diffRZ / diffR;
-        F[3 * j] -= force * diffRX / diffR;
-        F[3 * j + 1] -= force * diffRY / diffR;
-        F[3 * j + 2] -= force * diffRZ / diffR;
-        *U -= energyCutoff_;
+        efvd->energy += energy;
+        efvd->F[3 * i] += force * diffRX / diffR;
+        efvd->F[3 * i + 1] += force * diffRY / diffR;
+        efvd->F[3 * i + 2] += force * diffRZ / diffR;
+        efvd->F[3 * j] -= force * diffRX / diffR;
+        efvd->F[3 * j + 1] -= force * diffRY / diffR;
+        efvd->F[3 * j + 2] -= force * diffRZ / diffR;
+        efvd->energy -= energyCutoff_;
       }
     }
   }
