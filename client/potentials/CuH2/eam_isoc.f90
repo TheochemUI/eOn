32d234397568f35b23e8544e84cde4bd6d405407
Rohit Goswami <rog32@hi.is>
2024-07-13 00:26:28 +0000

    ENH: Rewrite CuH2 into more modern Fortran

--- client/potentials/CuH2/eam_isoc.f90
+++ client/potentials/CuH2/eam_isoc.f90
@@ -10,14 +10,15 @@
   module eam_wrap
     use iso_c_binding, only: c_double, c_int
     use iso_fortran_env, only: real64
-      implicit none
+    use eam_routines
+    implicit none
   contains
 
     subroutine c_force_eam (natms, ndim, box, R, F, U) bind(c)
       integer(c_int), intent(in) :: natms(2)
       integer(c_int), value, intent(in) :: ndim
-      real(c_double), intent(in) :: box(3)
-      real(c_double), intent(inout) :: U(1), R(ndim), F(ndim)
+      real(c_double), intent(in) :: box(3), R(ndim)
+      real(c_double), intent(out) :: U(1), F(ndim)
 
       ! write(*,*)"natms", natms
       ! write(*,*)"ndim", ndim
