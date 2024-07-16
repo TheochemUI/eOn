aa6e55a0527cf7549ea0dd099ed3599872123083
Rohit Goswami <rog32@hi.is>
2024-07-16 16:12:38 +0000

    MAINT: Rework to new toml free potential form

--- client/potentials/ParseTOML.hpp
+++ client/potentials/ParseTOML.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
-#include "LJ/LJ.h"
+#include "client/potentials/LJ/LJ.h"
+#include "client/potentials/Morse/Morse.h"
+
+#ifdef WITH_ASE_ORCA
+#include "client/potentials/ASE_ORCA/ASE_ORCA.h"
+#endif
 
 namespace eonc::pot {
-void from_toml(LJ::Params &p_a, const toml::node_view<const toml::node> &tbl_a);
-}
+void from_toml(LJ::Params &, const toml::node_view<const toml::node> &);
+void from_toml(Morse::Params &, const toml::node_view<const toml::node> &);
+
+#ifdef WITH_ASE_ORCA
+void from_toml(ASEOrcaPot::Params &, const toml::node_view<const toml::node> &);
+#endif
+} // namespace eonc::pot
