b16e4bba631b6be91ce80de7b29e2ea390159705
Rohit Goswami <rog32@hi.is>
2024-07-18 21:12:03 +0000

    BUG: Rework eOn LAMMPS to the newer interface

--- client/potentials/LAMMPS/LAMMPSPot.cpp
+++ client/potentials/LAMMPS/LAMMPSPot.cpp
@@ -1,17 +1,21 @@
 #include "LAMMPSPot.h"
-#include "library.h"
 #include <map>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+// LAMMPS library inclusion
+#ifdef EONMPI
+#define LAMMPS_LIB_MPI
+#endif
+#include "library.h"
+
 LAMMPSPot::LAMMPSPot(std::shared_ptr<Parameters> p)
     : Potential(p) {
   numberOfAtoms = 0;
   LAMMPSObj = nullptr;
-  for (int i = 0; i < 9; i++)
-    oldBox[i] = 0.0;
+  std::fill_n(oldBox, 9, 0.0);
 }
 
 LAMMPSPot::~LAMMPSPot() { cleanMemory(); }
@@ -38,8 +42,11 @@ void LAMMPSPot::force(long N, const double *R, const int *atomicNrs, double *F,
   if (newLammps) {
     makeNewLAMMPS(N, R, atomicNrs, box);
   }
+  if (!LAMMPSObj) {
+    throw std::runtime_error("Should have a LAMMPS instance by now");
+  }
 
-  lammps_scatter_atoms(LAMMPSObj, "x", 1, 3, R);
+  lammps_scatter_atoms(LAMMPSObj, "x", 1, 3, const_cast<double *>(R));
   lammps_command(LAMMPSObj, "run 1 pre no post no");
 
   double *pe = (double *)lammps_extract_variable(LAMMPSObj, "pe", nullptr);
@@ -89,41 +96,27 @@ void LAMMPSPot::makeNewLAMMPS(long N, const double *R, const int *atomicNrs,
     }
   }
 
-  char *lammps_argv[9];
-  int nargs = 7;
-  lammps_argv[0] = "";
-  lammps_argv[1] = "-log";
-  if (m_params->LAMMPSLogging) {
-    lammps_argv[2] = "log.lammps";
-  } else {
-    lammps_argv[2] = "none";
-  }
-  lammps_argv[3] = "-echo";
-  lammps_argv[4] = "log";
-  lammps_argv[5] = "-screen";
-  lammps_argv[6] = "none";
 
-  if (m_params->LAMMPSThreads > 0) {
-    lammps_argv[7] = "-suffix";
-    lammps_argv[8] = "omp";
-    nargs += 2;
-  }
 #ifdef EONMPI
-  lammps_open(nargs, lammps_argv, m_params->MPIClientComm, &LAMMPSObj);
+  // TODO(rg):  Suffix should be a parameter
+  const char *lmpargv[] = { "liblammps", "-log", "none", "-echo", "log", "-screen", "none", "-suffix", "omp"};
+  int lmpargc = sizeof(lmpargv)/sizeof(const char *);
+  LAMMPSObj = lammps_open(lmpargc, (char **)lmpargv, m_params->MPIClientComm, nullptr);
 #else
-  lammps_open_no_mpi(nargs, lammps_argv, &LAMMPSObj);
+  const char *lmpargv[] = { "liblammps", "-log", "none", "-echo", "log", "-screen", "none"};
+  int lmpargc = sizeof(lmpargv)/sizeof(const char *);
+  LAMMPSObj = lammps_open_no_mpi(lmpargc, (char **)lmpargv, NULL);
 #endif
-  void *ptr = LAMMPSObj;
 
   char cmd[200];
 
   if (m_params->LAMMPSThreads > 0) {
     snprintf(cmd, 200, "package omp %i force/neigh", m_params->LAMMPSThreads);
-    lammps_command(ptr, cmd);
+    lammps_command(LAMMPSObj, cmd);
   }
 
   // Gives units in Angstoms and eV
-  //    lammps_command(ptr, "units metal");
+  //    lammps_command(LAMMPSObj, "units metal");
 
   // We need to allow for 'real' units for reaxff
   realunits = false;
@@ -142,18 +135,18 @@ void LAMMPSPot::makeNewLAMMPS(long N, const double *R, const int *atomicNrs,
   fclose(file);
 
   if (realunits) {
-    lammps_command(ptr, "units real");
+    lammps_command(LAMMPSObj, "units real");
   } else {
-    lammps_command(ptr, "units metal");
+    lammps_command(LAMMPSObj, "units metal");
   }
 
-  lammps_command(ptr, "atom_style	charge");
+  lammps_command(LAMMPSObj, "atom_style	charge");
 
   // Preserves atomic index ordering
-  lammps_command(ptr, "atom_modify map array sort 0 0");
+  lammps_command(LAMMPSObj, "atom_modify map array sort 0 0");
 
   // Always check to see if the neighbor list must be updated
-  lammps_command(ptr, "neigh_modify delay 1");
+  lammps_command(LAMMPSObj, "neigh_modify delay 1");
 
   // Define periodic cell
   //     prism args = xlo xhi ylo yhi zlo zhi xy xz yz
@@ -164,19 +157,19 @@ void LAMMPSPot::makeNewLAMMPS(long N, const double *R, const int *atomicNrs,
   snprintf(cmd, 200, "region cell prism 0 %f 0 %f 0 %f %f %f %f units box",
            box[0], box[4], box[8], box[3], box[6], box[7]);
 
-  lammps_command(ptr, cmd);
+  lammps_command(LAMMPSObj, cmd);
   snprintf(cmd, 200, "create_box %i cell", ntypes);
-  lammps_command(ptr, cmd);
+  lammps_command(LAMMPSObj, cmd);
 
   // Initialize the atoms and their types
   for (int i = 0; i < N; i++) {
     snprintf(cmd, 200, "create_atoms %i single %f %f %f units box",
              type_map[atomicNrs[i]], 0.0, 0.0, 0.0);
-    lammps_command(ptr, cmd);
+    lammps_command(LAMMPSObj, cmd);
   }
 
   // We don't care about mass but have to set it
-  lammps_command(ptr, "mass * 1.0");
+  lammps_command(LAMMPSObj, "mass * 1.0");
 
   // Read in user commands from in.lammps files
   struct stat buffer;
@@ -184,12 +177,12 @@ void LAMMPSPot::makeNewLAMMPS(long N, const double *R, const int *atomicNrs,
     fprintf(stderr, "couldn't open in.lammps: %s\n", strerror(errno));
     exit(1);
   } else {
-    lammps_file(ptr, "in.lammps");
+    lammps_file(LAMMPSObj, "in.lammps");
   }
 
   // Define variables for force and energy so they can be extracted
-  lammps_command(ptr, "variable fx atom fx");
-  lammps_command(ptr, "variable fy atom fy");
-  lammps_command(ptr, "variable fz atom fz");
-  lammps_command(ptr, "variable pe equal pe");
+  lammps_command(LAMMPSObj, "variable fx atom fx");
+  lammps_command(LAMMPSObj, "variable fy atom fy");
+  lammps_command(LAMMPSObj, "variable fz atom fz");
+  lammps_command(LAMMPSObj, "variable pe equal pe");
 }
