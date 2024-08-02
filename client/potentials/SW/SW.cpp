70ee386656e46e971c75405f63bfb1b42d10e98a
Rohit Goswami <rog32@hi.is>
2024-08-02 20:40:47 +0000

    ENH: Rework SW_SI at v3

--- client/potentials/SW/SW.cpp
+++ client/potentials/SW/SW.cpp
@@ -12,17 +12,14 @@
 
 #include "SW.h"
 namespace eonc {
-void SW::initialize(void) { return; }
-
-void SW::cleanMemory(void) { return; }
-
-// pointer to number of atoms, pointer to array of positions
-// pointer to array of forces, pointer to internal energy
-// address to supercell size
-void SW::force(long N, const double *R, const int *atomicNrs, double *F,
-               double *U, double *variance, const double *box) {
-  variance = nullptr;
-  sw_(&N, R, F, U, &box[0], &box[4], &box[8]);
+void SW::forceImpl(const ForceInput &fip, ForceOut *efvd) {
+#ifdef EON_CHECKS
+  eonc::pot::checkParams(fip);
+  eonc::pot::zeroForceOut(fip.nAtoms, efvd);
+#endif
+  const long int N = fip.nAtoms;
+  sw_(&N, fip.pos, efvd->F, &efvd->energy, &fip.box[0], &fip.box[4],
+      &fip.box[8]);
   return;
 }
 
