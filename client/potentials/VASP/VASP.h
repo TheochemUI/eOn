acba7f2318dd9250f6031f13ac0712b879b8c1d3
Rohit Goswami <rog32@hi.is>
2024-08-03 18:41:34 +0000

    ENH: Rework VASP into v3 minimally

--- client/potentials/VASP/VASP.h
+++ client/potentials/VASP/VASP.h
@@ -12,13 +12,13 @@
 
 #pragma once
 #include "../../Potential.h"
+
 namespace eonc {
 
-class VASP : public Potential {
+class VASP : public Potential<VASP> {
 
 public:
-  VASP()
-      : Potential(PotType::VASP) {
+  VASP() {
     vaspRunCount++;
     // deleting leftovers from previous run
     system("rm -f TMPCAR");
@@ -39,13 +39,11 @@ public:
     system("rm -f XDATCAR");
   }
   ~VASP() { cleanMemory(); }
-  void initialize() {};
   void cleanMemory(void);
-  void force(long N, const double *R, const int *atomicNrs, double *F,
-             double *U, double *variance, const double *box);
+  void forceImpl(const ForceInput &, ForceOut *) override final;
 
 private:
-  void writePOSCAR(long N, const double *R, const int *atomicNrs,
+  void writePOSCAR(long N, const double *R, const size_t *atomicNrs,
                    const double *box);
   void readFU(long N, double *F, double *U);
   void spawnVASP();
