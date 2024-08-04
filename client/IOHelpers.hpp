7433a0b0528147447fab30c8361a1affbb1f6268
Rohit Goswami <rog32@hi.is>
2024-08-04 00:47:55 +0000

    ENH: Add IO Helpers

---
+++ client/IOHelpers.hpp
@@ -0,0 +1,17 @@
+/*
+** This file is part of eOn.
+**
+** SPDX-License-Identifier: BSD-3-Clause
+**
+** Copyright (c) 2010--present, eOn Development Team
+** All rights reserved.
+**
+** Repo:
+** https://github.com/TheochemUI/eOn
+*/
+#pragma once
+#include "client/Eigen.h"
+
+namespace eonc::io {
+VectorType getUniqueElements(const VectorType &vec);
+}
