8f241d66feebcfcdb89754081abf683517aaecda
Rohit Goswami <rog32@hi.is>
2024-08-04 14:43:44 +0000

    ENH: Use a creator for Writers

---
+++ client/io/WriteCreator.cc
@@ -0,0 +1,37 @@
+#include "client/Parser.hpp"
+
+#include "client/io/ConWriter.hpp"
+#include "client/io/ConvelWriter.hpp"
+#include "client/io/TibbleWriter.hpp"
+#include "client/io/WriteCreator.hpp"
+#include "client/io/XYZWriter.hpp"
+
+namespace eonc::io {
+std::unique_ptr<WriteBase> mkWriter(const toml::table &config) {
+  config_section(config, "Main");
+  auto wtype = get_enum_toml<WriteType>(config["Main"]["write"]);
+  switch (wtype) {
+  case WriteType::CON: {
+    return (std::make_unique<ConWriter>());
+    break;
+  }
+  case WriteType::CONVEL: {
+    return (std::make_unique<ConvelWriter>());
+    break;
+  }
+  case WriteType::XYZ: {
+    return (std::make_unique<XYZWriter>());
+    break;
+  }
+  case WriteType::TIBBLE: {
+    return (std::make_unique<TibbleWriter>());
+    break;
+  }
+  default:
+    SPDLOG_ERROR("No known writer could be constructed from {}",
+                 magic_enum::enum_name(wtype));
+    throw std::runtime_error("Terminating");
+    break;
+  }
+}
+} // namespace eonc::io
