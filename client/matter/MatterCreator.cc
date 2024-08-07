fca5fb7f44a2c8fe7ffadec8b8cbaed4986c3f67
Rohit Goswami <rog32@hi.is>
2024-08-07 01:20:14 +0000

    ENH: Modernize conreader in place

--- client/matter/MatterCreator.cc
+++ client/matter/MatterCreator.cc
@@ -56,78 +56,91 @@ void from_con(Matter &mat, const std::string &fname) {
     throw std::runtime_error("Failed to open file: " + fname);
   }
 
-  char line[255];
-  std::string headerCon1, headerCon2, headerCon4, headerCon5, headerCon6;
+  auto readLine = [&file]() -> std::optional<std::string> {
+    std::string line;
+    if (std::getline(file, line)) {
+      return line;
+    }
+    return std::nullopt;
+  };
 
-  file.getline(line, sizeof(line));
-  headerCon1 = line;
-  file.getline(line, sizeof(line));
-  headerCon2 = line;
+  auto readArray = [](const std::string &line) -> std::array<double, 3> {
+    std::array<double, 3> arr;
+    std::istringstream iss(line);
+    iss >> arr[0] >> arr[1] >> arr[2];
+    return arr;
+  };
 
-  std::array<double, 3> lengths;
-  file.getline(line, sizeof(line));
-  sscanf(line, "%lf %lf %lf", &lengths[0], &lengths[1], &lengths[2]);
-
-  std::array<double, 3> angles;
-  file.getline(line, sizeof(line));
-  headerCon4 = line;
-  sscanf(headerCon4.c_str(), "%lf %lf %lf", &angles[0], &angles[1], &angles[2]);
+  std::string headerCon1 = *readLine();
+  std::string headerCon2 = *readLine();
+  auto lengths = readArray(*readLine());
+  auto angles = readArray(*readLine());
+  std::string headerCon4 = *readLine();
+  std::string headerCon5 = *readLine();
 
   conCell(mat, lengths, angles);
 
-  file.getline(line, sizeof(line));
-  headerCon5 = line;
-  file.getline(line, sizeof(line));
-  headerCon6 = line;
-
-  file.getline(line, sizeof(line));
-  size_t Ncomponent;
-  if (sscanf(line, "%zu", &Ncomponent) != 1) {
-    SPDLOG_INFO("The number of components cannot be read. One "
-                "component is assumed instead");
-    Ncomponent = 1;
+  size_t Ncomponent{0};
+  if (auto line = readLine()) {
+    std::istringstream iss(*line);
+    if (!(iss >> Ncomponent)) {
+      SPDLOG_INFO("The number of components cannot be read. One component is "
+                  "assumed instead");
+      Ncomponent = 1;
+    }
   }
 
-  size_t first[101];
-  size_t Natoms = 0;
-  first[0] = 0;
+  SPDLOG_INFO("Got {} components", Ncomponent);
 
-  file.getline(line, sizeof(line));
-  char *split = strtok(line, " \t");
-  for (size_t j = 0; j < Ncomponent; j++) {
-    if (!split || sscanf(split, "%zu", &Natoms) != 1) {
-      throw std::runtime_error("Invalid component count");
+  if (Ncomponent < 1) {
+    throw std::runtime_error("Unsupported number of components");
+  }
+
+  std::vector<size_t> first(Ncomponent + 1, 0);
+  if (auto line = readLine()) {
+    std::istringstream iss(*line);
+    for (size_t j = 0; j < Ncomponent; ++j) {
+      size_t Natoms;
+      if (!(iss >> Natoms)) {
+        throw std::runtime_error("Invalid component count");
+      }
+      first[j + 1] = Natoms + first[j];
     }
-    first[j + 1] = Natoms + first[j];
-    split = strtok(NULL, " \t");
   }
 
   mat.resize(first[Ncomponent]);
 
-  double mass[100];
-  file.getline(line, sizeof(line));
-  split = strtok(line, " \t");
-  for (size_t j = 0; j < Ncomponent; j++) {
-    if (!split || sscanf(split, "%lf", &mass[j]) != 1) {
-      throw std::runtime_error("Invalid mass entry");
+  std::vector<double> mass(Ncomponent);
+  if (auto line = readLine()) {
+    std::istringstream iss(*line);
+    for (size_t j = 0; j < Ncomponent; ++j) {
+      if (!(iss >> mass[j])) {
+        throw std::runtime_error("Invalid mass entry");
+      }
     }
-    split = strtok(NULL, " \t");
   }
 
-  for (size_t j = 0; j < Ncomponent; j++) {
-    char symbol[3];
-    file.getline(line, sizeof(line));
-    sscanf(line, "%2s", symbol);
-    int atomicNr = symbol2atomicNumber(symbol);
-    file.getline(line, sizeof(line)); // skip one line
-    for (size_t i = first[j]; i < first[j + 1]; i++) {
-      mat.setMass(i, mass[j]);
-      mat.setAtomicNr(i, atomicNr);
-      file.getline(line, sizeof(line));
-      if (sscanf(line, "%lf %lf %lf %d", &mat.positions(i, 0),
-                 &mat.positions(i, 1), &mat.positions(i, 2),
-                 &mat.isFixed[i]) != 4) {
-        throw std::runtime_error("Error parsing atom data");
+  for (size_t j = 0; j < Ncomponent; ++j) {
+    if (auto line = readLine()) {
+      std::istringstream iss(*line);
+      std::string symbol;
+      iss >> symbol;
+      int atomicNr = symbol2atomicNumber(symbol);
+
+      // Skip:
+      // Coordinates of component blah
+      readLine();
+
+      for (size_t i = first[j]; i < first[j + 1]; ++i) {
+        mat.setMass(i, mass[j]);
+        mat.setAtomicNr(i, atomicNr);
+        if (auto line = readLine()) {
+          std::istringstream iss(*line);
+          if (!(iss >> mat.positions(i, 0) >> mat.positions(i, 1) >>
+                mat.positions(i, 2) >> mat.isFixed[i])) {
+            throw std::runtime_error("Error parsing atom data");
+          }
+        }
       }
     }
   }
