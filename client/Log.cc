b7e410369d019ec16ea9aa116968730ee8751955
Rohit Goswami <rog32@hi.is>
2024-08-14 08:03:40 -0700

    ENH: Rework LogManager to be threadsafe

--- client/Log.cc
+++ client/Log.cc
@@ -10,13 +10,33 @@
 ** https://github.com/TheochemUI/eOn
 */
 #include "client/Log.hpp"
+#include <atomic>
+#include <mutex>
 
 namespace eonc {
 
+// BEGIN_STATIC
+std::atomic<LogManager *> LogManager::instance = nullptr;
+std::mutex LogManager::lmMutex;
+// END_STATIC
+
+LogManager *LogManager::getInstance() {
+  LogManager *lm = instance.load(std::memory_order_acquire);
+  if (!lm) {
+    std::lock_guard<std::mutex> myLock(lmMutex);
+    lm = instance.load(std::memory_order_relaxed);
+    if (!lm) {
+      // Removed on exit
+      lm = new LogManager();
+      instance.store(lm, std::memory_order_release);
+    }
+  }
+  [[maybe_unused]] volatile bool dummy{}; // Prevent being optimized away
+  return lm;
+}
+
 void LogManager::setup_logger() {
   spdlog::cfg::load_env_levels();
-  // Sinks
-  spdlog::flush_every(std::chrono::seconds(3));
   auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
   auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
       "client_spdlog.log", true); // Overwrite existing
@@ -25,8 +45,9 @@ void LogManager::setup_logger() {
   spdlog::register_logger(logger);
   logger->set_pattern("%v");
   spdlog::set_default_logger(logger);
-  // Traceback logger
   spdlog::set_level(spdlog::level::trace);
+
+  // Traceback logger
   auto trace_csink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
   auto trace_fsink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
       "client_traceback.log", true); // Overwrite existing
