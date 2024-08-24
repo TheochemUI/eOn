a235ec8b976419b60bf55c4a50e585b10d097d90
Rohit Goswami <rog32@hi.is>
2024-08-24 12:17:43 +0000

    MAINT: Try a more delegating approach

--- client/parsers/ParseOptim.cc
+++ client/parsers/ParseOptim.cc
@@ -11,23 +11,29 @@
 */
 #include "client/parsers/ParseOptim.hpp"
 #include "client/BaseStructures.h"
+#include "client/Parser.hpp"
 #include "magic_enum/magic_enum.hpp"
+#include <iostream>
 
 namespace eonc::opt {
 void from_toml(OptimBase::Params &params,
                const toml::node_view<const toml::node> &tbl) {
-  const auto &config = tbl.at_path("Optimizer");
-  params.optM =
-      magic_enum::enum_cast<OptType>(config["method"].value_or("none"s),
-                                     magic_enum::case_insensitive)
-          .value_or(params.optM);
+  params.optM = get_enum_toml<OptType>(tbl["method"]).value_or(params.optM);
   params.optCM = magic_enum::enum_cast<ConvergenceMeasure>(
-                     config["convergence_metric"].value_or("none"s),
+                     tbl["convergence_metric"].value_or("none"s),
                      magic_enum::case_insensitive)
                      .value_or(params.optCM);
   params.optConvergedForce =
-      config["converged_force"].value_or(params.optConvergedForce);
-  params.optMaxIter = config["max_iterations"].value_or(params.optMaxIter);
-  params.optMaxMove = config["max_move"].value_or(params.optMaxMove);
+      tbl["converged_force"].value_or(params.optConvergedForce);
+  params.optMaxIter = tbl["max_iterations"].value_or(params.optMaxIter);
+  params.optMaxMove = tbl["max_move"].value_or(params.optMaxMove);
+}
+
+void from_toml(ConjugateGradients::Params &params,
+               const toml::node_view<const toml::node> &tbl) {
+  from_toml(*static_cast<OptimBase::Params *>(&params), tbl); // OptimBase
+  const auto &config = tbl.at_path("CG");
+  params.max_iter_before_reset =
+      config["max_iter_before_reset"].value_or(params.max_iter_before_reset);
 }
 } // namespace eonc::opt
