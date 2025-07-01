0eab352e05b2e2bb131a2231fa87681646371ef1
Rohit Goswami <rog32@hi.is>
2025-07-01 16:19:28 +0200

    ENH: Setup scaffolding for rate based superbasins

--- eon/akmc.py
+++ eon/akmc.py
@@ -176,6 +176,8 @@ def get_superbasin_scheme(states, config):
         superbasining = superbasinscheme.TransitionCounting(config.sb_path, states, config.main_temperature / 11604.5, config.sb_tc_ntrans)
     elif config.sb_scheme == 'energy_level':
         superbasining = superbasinscheme.EnergyLevel(config.sb_path, states, config.main_temperature / 11604.5, config.sb_el_energy_increment)
+    elif config.sb_scheme == 'rate':
+        superbasining = superbasinscheme.EnergyLevel(config.sb_path, states, config.main_temperature / 11604.5, config.sb_rt_rate_threshold)
     return superbasining
 
 
