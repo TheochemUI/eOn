3c9f8f87b17de7b9af43421d4b77130f2c11fe20
Rohit Goswami <rog32@hi.is>
2025-08-14 05:02:45 +0200

    MAINT: Slowly fix licenses

--- client/potentials/ASE_NWCHEM/ASE_NWCHEM.cpp
+++ client/potentials/ASE_NWCHEM/ASE_NWCHEM.cpp
@@ -1,13 +1,3 @@
-//-----------------------------------------------------------------------------------
-// eOn is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// A copy of the GNU General Public License is available at
-// http://www.gnu.org/licenses/
-//-----------------------------------------------------------------------------------
-
 #include "ASE_NWCHEM.h"
 #include "../../EnvHelpers.hpp"
 #include "../../fpe_handler.h"
