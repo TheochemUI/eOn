b4fd53e3c97ce7a87e44302745d5cdbfbe5a2b56
Rohit Goswami <rgoswami@ieee.org>
2025-12-22 08:34:18 +0100

    chore(schema): update xtb paramset

--- eon/schema.py
+++ eon/schema.py
@@ -599,7 +599,7 @@ class PotentialConfig(BaseModel):
 class XTBPot(BaseModel):
     model_config = ConfigDict(use_attribute_docstrings=True)
 
-    paramset: str = Field(
+    paramset: Literal["GFNFF", "GFN0xTB", "GFN1xTB", "GFN2xTB"] = Field(
         default="GFNFF", description="Parameter set for XTB potential."
     )
     accuracy: float = Field(default=1.0, description="Accuracy of the XTB calculation.")
@@ -672,7 +672,7 @@ class Metatomic(BaseModel):
         description="Whether to check consistency of the Metatomic model.",
     )
     uncertainty_threshold: float = Field(
-        default=0.1, # 100meV/atom
+        default=0.1,  # 100meV/atom
         description="Threshold to report per-atom uncertainties, if positive. Also used to populate the variance.",
     )
 
