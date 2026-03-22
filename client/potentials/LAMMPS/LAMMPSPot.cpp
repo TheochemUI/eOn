247b9dad37d5012bde07c6618f0e9530f9ae3ecb
Rohit Goswami <rgoswami@ieee.org>
2026-03-22 19:00:36 +0100

    feat: runtime LAMMPS loading via dlopen (no compile-time dependency)
    LammpsLoader singleton loads liblammps at runtime via dlopen/LoadLibrary.
    Cross-platform DynLib.h abstraction (Linux/macOS/Windows). LAMMPS
    potential always compiled; clear error if library not installed.
    Removes with_lammps meson option and -DLAMMPS_POT compile flag.

--- client/potentials/LAMMPS/LAMMPSPot.cpp
+++ client/potentials/LAMMPS/LAMMPSPot.cpp
@@ -9,43 +9,46 @@
 ** Repo:
 ** https://github.com/TheochemUI/eOn
 */
-#include "LAMMPS.h"
-#include "library.h"
-#include <map>
-#include <string.h>
-#ifndef _WIN32
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#endif
+#include "LAMMPSPot.h"
+#include "LammpsLoader.h"
 
-// LAMMPS library inclusion
+#include <cstring>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <map>
+#include <string>
+
 #ifdef EONMPI
 #define LAMMPS_LIB_MPI
 #endif
-#include "library.h"
 
 LAMMPSPot::LAMMPSPot(const Parameters &p)
-    : Potential(p) {
-  numberOfAtoms = 0;
-  LAMMPSObj = nullptr;
-  std::fill_n(oldBox, 9, 0.0);
+    : Potential(p),
+      lammpsThr{p.potential_options.LAMMPSThreads}
+#ifdef EONMPI
+      ,
+      mpiComm{p.potential_options.MPIClientComm}
+#endif
+{
+  // Fail fast if LAMMPS library not available
+  eonc::LammpsLoader::instance().require_loaded();
 }
 
 LAMMPSPot::~LAMMPSPot() { cleanMemory(); }
 
-void LAMMPSPot::cleanMemory(void) {
+void LAMMPSPot::cleanMemory() {
   if (LAMMPSObj != nullptr) {
-    lammps_close(LAMMPSObj);
+    eonc::LammpsLoader::instance().close(LAMMPSObj);
     LAMMPSObj = nullptr;
   }
-  return;
 }
 
 void LAMMPSPot::force(long N, const double *R, const int *atomicNrs, double *F,
                       double *U, double *variance, const double *box) {
   variance = nullptr;
-  int i;
+  auto &lmp = eonc::LammpsLoader::instance();
+
   bool newLammps = false;
   for (int i = 0; i < 9; i++) {
     if (oldBox[i] != box[i])
@@ -60,29 +63,33 @@ void LAMMPSPot::force(long N, const double *R, const int *atomicNrs, double *F,
     throw std::runtime_error("Should have a LAMMPS instance by now");
   }
 
-  lammps_scatter_atoms(LAMMPSObj, "x", 1, 3, const_cast<double *>(R));
-  lammps_command(LAMMPSObj, "run 1 pre no post no");
+  lmp.scatter_atoms(LAMMPSObj, "x", 1, 3, const_cast<double *>(R));
+  lmp.command(LAMMPSObj, "run 1 pre no post no");
 
-  double *pe = (double *)lammps_extract_variable(LAMMPSObj, "pe", nullptr);
+  auto *pe =
+      static_cast<double *>(lmp.extract_variable(LAMMPSObj, "pe", nullptr));
   *U = *pe;
   free(pe);
 
-  double *fx = (double *)lammps_extract_variable(LAMMPSObj, "fx", "all");
-  double *fy = (double *)lammps_extract_variable(LAMMPSObj, "fy", "all");
-  double *fz = (double *)lammps_extract_variable(LAMMPSObj, "fz", "all");
+  auto *fx =
+      static_cast<double *>(lmp.extract_variable(LAMMPSObj, "fx", "all"));
+  auto *fy =
+      static_cast<double *>(lmp.extract_variable(LAMMPSObj, "fy", "all"));
+  auto *fz =
+      static_cast<double *>(lmp.extract_variable(LAMMPSObj, "fz", "all"));
 
-  for (i = 0; i < N; i++) {
+  for (long i = 0; i < N; i++) {
     F[3 * i + 0] = fx[i];
     F[3 * i + 1] = fy[i];
     F[3 * i + 2] = fz[i];
   }
 
-  // convert from kCal/mol -> eV if LAMMPS is using real units
-
+  // Convert kCal/mol -> eV if LAMMPS is using real units
   if (realunits) {
-    *U = *U / 23.0609;
-    for (i = 0; i < 3 * N; i++) {
-      F[i] = F[i] / 23.0609;
+    constexpr double kcalPerEv = 23.0609;
+    *U /= kcalPerEv;
+    for (long i = 0; i < 3 * N; i++) {
+      F[i] /= kcalPerEv;
     }
   }
 
@@ -93,113 +100,98 @@ void LAMMPSPot::force(long N, const double *R, const int *atomicNrs, double *F,
 
 void LAMMPSPot::makeNewLAMMPS(long N, const double *R, const int *atomicNrs,
                               const double *box) {
+  auto &lmp = eonc::LammpsLoader::instance();
+
   numberOfAtoms = N;
-  for (int i = 0; i < 9; i++)
-    oldBox[i] = box[i];
+  std::memcpy(oldBox, box, 9 * sizeof(double));
 
   if (LAMMPSObj != nullptr) {
     cleanMemory();
   }
 
+  // Map atomic numbers to LAMMPS type indices (1-based)
   std::map<int, int> type_map;
   int ntypes = 0;
-  for (int i = 0; i < N; i++) {
+  for (long i = 0; i < N; i++) {
     if (type_map.count(atomicNrs[i]) == 0) {
-      ntypes += 1;
-      type_map.insert(std::pair<int, int>(atomicNrs[i], ntypes));
+      type_map.insert({atomicNrs[i], ++ntypes});
     }
   }
 
 #ifdef EONMPI
-  // TODO(rg):  Suffix should be a parameter
   const char *lmpargv[] = {"liblammps", "-log", "none",    "-echo", "log",
                            "-screen",   "none", "-suffix", "omp"};
   int lmpargc = sizeof(lmpargv) / sizeof(const char *);
-  LAMMPSObj = lammps_open(lmpargc, (char **)lmpargv,
-                          m_params.potential_options.MPIClientComm, nullptr);
+  if (!lmp.open_mpi) {
+    throw std::runtime_error(
+        "LAMMPS library found but lacks MPI support (lammps_open not found).\n"
+        "Install an MPI-enabled LAMMPS build.");
+  }
+  LAMMPSObj = lmp.open_mpi(lmpargc, const_cast<char **>(lmpargv), mpiComm,
+                           nullptr);
 #else
   const char *lmpargv[] = {"liblammps", "-log",    "none", "-echo",
                            "log",       "-screen", "none"};
   int lmpargc = sizeof(lmpargv) / sizeof(const char *);
-  LAMMPSObj = lammps_open_no_mpi(lmpargc, (char **)lmpargv, NULL);
+  LAMMPSObj = lmp.open_no_mpi(lmpargc, const_cast<char **>(lmpargv), nullptr);
 #endif
 
-  char cmd[200];
-
-  if (m_params.potential_options.LAMMPSThreads > 0) {
-    snprintf(cmd, 200, "package omp %i force/neigh",
-             m_params.potential_options.LAMMPSThreads);
-    lammps_command(LAMMPSObj, cmd);
+  if (lammpsThr > 0) {
+    std::string cmd = std::format("package omp {} force/neigh", lammpsThr);
+    lmp.command(LAMMPSObj, cmd.c_str());
   }
 
-  // Gives units in Angstoms and eV
-  //    lammps_command(LAMMPSObj, "units metal");
-
-  // We need to allow for 'real' units for reaxff
+  // Detect units from in.lammps: look for "#!units real" marker
   realunits = false;
-  FILE *file;
-  file = fopen("in.lammps", "r");
-  if (!file) {
-    fprintf(stderr, "couldn't open in.lammps: %s\n", strerror(errno));
+  if (std::filesystem::exists("in.lammps")) {
+    std::ifstream infile("in.lammps");
+    std::string line;
+    while (std::getline(infile, line)) {
+      if (line == "#!units real") {
+        realunits = true;
+        break;
+      }
+    }
+  } else {
+    EONC_LOG_ERROR("[LAMMPS] in.lammps not found in working directory");
     return;
   }
-  char line[256];
-  while (fgets(line, sizeof(line), file)) {
-    if (strcmp(line, "#!units real\n") == 0) {
-      realunits = true;
-    }
-  }
-  fclose(file);
 
   if (realunits) {
-    lammps_command(LAMMPSObj, "units real");
+    lmp.command(LAMMPSObj, "units real");
   } else {
-    lammps_command(LAMMPSObj, "units metal");
+    lmp.command(LAMMPSObj, "units metal");
   }
 
-  lammps_command(LAMMPSObj, "atom_style	charge");
+  lmp.command(LAMMPSObj, "atom_style charge");
+  lmp.command(LAMMPSObj, "atom_modify map array sort 0 0");
+  lmp.command(LAMMPSObj, "neigh_modify delay 1");
 
-  // Preserves atomic index ordering
-  lammps_command(LAMMPSObj, "atom_modify map array sort 0 0");
+  // Define periodic cell (prism for non-orthorhombic)
+  std::string region_cmd = std::format(
+      "region cell prism 0 {} 0 {} 0 {} {} {} {} units box",
+      box[0], box[4], box[8], box[3], box[6], box[7]);
+  lmp.command(LAMMPSObj, region_cmd.c_str());
 
-  // Always check to see if the neighbor list must be updated
-  lammps_command(LAMMPSObj, "neigh_modify delay 1");
+  std::string create_box_cmd = std::format("create_box {} cell", ntypes);
+  lmp.command(LAMMPSObj, create_box_cmd.c_str());
 
-  // Define periodic cell
-  //     prism args = xlo xhi ylo yhi zlo zhi xy xz yz
-  //         xlo,xhi,ylo,yhi,zlo,zhi = bounds of untilted prism
-  //         xy = distance to tilt y in x direction
-  //         xz = distance to tilt z in x direction
-  //         yz = distance to tilt z in y direction
-  snprintf(cmd, 200, "region cell prism 0 %f 0 %f 0 %f %f %f %f units box",
-           box[0], box[4], box[8], box[3], box[6], box[7]);
-
-  lammps_command(LAMMPSObj, cmd);
-  snprintf(cmd, 200, "create_box %i cell", ntypes);
-  lammps_command(LAMMPSObj, cmd);
-
-  // Initialize the atoms and their types
-  for (int i = 0; i < N; i++) {
-    snprintf(cmd, 200, "create_atoms %i single %f %f %f units box",
-             type_map[atomicNrs[i]], 0.0, 0.0, 0.0);
-    lammps_command(LAMMPSObj, cmd);
+  // Initialize atoms
+  for (long i = 0; i < N; i++) {
+    std::string atom_cmd = std::format(
+        "create_atoms {} single {} {} {} units box",
+        type_map[atomicNrs[i]], 0.0, 0.0, 0.0);
+    lmp.command(LAMMPSObj, atom_cmd.c_str());
   }
 
-  // We don't care about mass but have to set it
-  lammps_command(LAMMPSObj, "mass * 1.0");
+  lmp.command(LAMMPSObj, "mass * 1.0");
 
-  // Read in user commands from in.lammps files
-  struct stat buffer;
-  if (stat("in.lammps", &buffer) == -1) {
-    fprintf(stderr, "couldn't open in.lammps: %s\n", strerror(errno));
-    exit(1);
-  } else {
-    lammps_file(LAMMPSObj, "in.lammps");
-  }
+  // Load user LAMMPS input script
+  lmp.file(LAMMPSObj, "in.lammps");
 
-  // Define variables for force and energy so they can be extracted
-  lammps_command(LAMMPSObj, "variable fx atom fx");
-  lammps_command(LAMMPSObj, "variable fy atom fy");
-  lammps_command(LAMMPSObj, "variable fz atom fz");
-  lammps_command(LAMMPSObj, "variable pe equal pe");
+  // Define variables for force/energy extraction
+  lmp.command(LAMMPSObj, "variable fx atom fx");
+  lmp.command(LAMMPSObj, "variable fy atom fy");
+  lmp.command(LAMMPSObj, "variable fz atom fz");
+  lmp.command(LAMMPSObj, "variable pe equal pe");
 }
---
+++ client/potentials/LAMMPS/LammpsLoader.h
@@ -0,0 +1,84 @@
+/*
+** This file is part of eOn.
+**
+** SPDX-License-Identifier: BSD-3-Clause
+**
+** Copyright (c) 2010--present, eOn Development Team
+** All rights reserved.
+**
+** Repo:
+** https://github.com/TheochemUI/eOn
+*/
+#pragma once
+
+/// Runtime loader for the LAMMPS C library.
+///
+/// Uses dlopen (POSIX) or LoadLibrary (Windows) to load liblammps at runtime
+/// rather than requiring it at compile time. This allows a single eOn binary
+/// to optionally use LAMMPS potentials if the library is installed.
+
+#include "../../DynLib.h"
+
+#include <stdexcept>
+#include <string>
+
+#ifdef EONMPI
+#include <mpi.h>
+#endif
+
+namespace eonc {
+
+// ---------------------------------------------------------------------------
+// LammpsLoader: singleton that loads liblammps function pointers at runtime
+// ---------------------------------------------------------------------------
+class LammpsLoader {
+public:
+  // LAMMPS C API function pointer types (from library.h)
+  using open_no_mpi_fn  = void *(*)(int, char **, void **);
+  using close_fn        = void (*)(void *);
+  using command_fn      = char *(*)(void *, const char *);
+  using file_fn         = void (*)(void *, const char *);
+  using scatter_atoms_fn = void (*)(void *, const char *, int, int, void *);
+  using extract_var_fn  = void *(*)(void *, const char *, const char *);
+#ifdef EONMPI
+  using open_mpi_fn     = void *(*)(int, char **, MPI_Comm, void **);
+#endif
+
+  /// Thread-safe singleton accessor (Meyer's pattern).
+  static LammpsLoader &instance();
+
+  // Loaded function pointers (null if library not found)
+  open_no_mpi_fn  open_no_mpi{nullptr};
+  close_fn        close{nullptr};
+  command_fn      command{nullptr};
+  file_fn         file{nullptr};
+  scatter_atoms_fn scatter_atoms{nullptr};
+  extract_var_fn  extract_variable{nullptr};
+#ifdef EONMPI
+  open_mpi_fn     open_mpi{nullptr};
+#endif
+
+  /// True if liblammps was successfully loaded.
+  [[nodiscard]] bool is_loaded() const noexcept { return m_loaded; }
+
+  /// Throws std::runtime_error if liblammps is not available.
+  void require_loaded() const;
+
+  LammpsLoader(const LammpsLoader &) = delete;
+  LammpsLoader &operator=(const LammpsLoader &) = delete;
+
+private:
+  LammpsLoader();
+  ~LammpsLoader();
+
+  bool m_loaded{false};
+  dynlib::Handle m_handle{};
+
+  /// Try to load a symbol; returns nullptr on failure.
+  template <typename Fn>
+  Fn load_sym(const char *name) {
+    return reinterpret_cast<Fn>(dynlib::sym(m_handle, name));
+  }
+};
+
+} // namespace eonc
