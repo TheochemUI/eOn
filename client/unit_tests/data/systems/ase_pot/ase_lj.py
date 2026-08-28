d76c671bc4ac2a1693a23472f569d6d4f31f0e0e
Tobias Dijkhuis <tobiasdijkhuis@hotmail.com>
2026-08-28 18:29:26 +0200

    Make ASE calculators optionally batch calculateable, by adding a 'batch_calculate' function into the 'ase_calc.py' file.

--- client/unit_tests/data/systems/ase_pot/ase_lj.py
+++ client/unit_tests/data/systems/ase_pot/ase_lj.py
@@ -1,10 +1,25 @@
 """ASE LJ calculator for eOn test suite."""
+
 from ase import Atoms
 from ase.calculators.lj import LennardJones
 
 
 def ase_calc():
     return LennardJones(epsilon=1.0, sigma=1.0, rc=10.0, ro=0.0, smooth=False)
+
+
+def batch_calculate(Rs, atomicNrs, boxes, calc):
+    num_structures = np.shape(Rs)[0]
+    energies = np.empty(num_structures)
+    forces = np.empty((num_structures, atomicNrs, 3))
+    for structure_idx in range(num_structures):
+        energies[structure_idx], forces[structure_idx, :] = _calculate(
+            Rs[structure_idx, :, :],
+            atomicNrs[structure_idx, :],
+            boxes[structure_idx, :, :],
+            calc,
+        )
+    return energies, forces
 
 
 def _calculate(R, atomicNrs, box, calc):
