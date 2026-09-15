42760adb5c5a4d163098d45ec765662180ebc7fa
Rohit Goswami <rog32@hi.is>
2026-09-15 08:40:05 -0500

    style: clang-format ParametersOptions.h after NEB default change

--- include/eon/ParametersOptions.h
+++ include/eon/ParametersOptions.h
@@ -473,8 +473,7 @@ struct neb_options_t {
   long image_count{5};
   long max_iterations{1000};
   OptType opt_method{OptType::LBFGS};
-  double force_tolerance{
-      0.01}; // resolved to optimizer_options.converged_force
+  double force_tolerance{0.01}; // resolved to optimizer_options.converged_force
   struct mmf_peak_options_t {
     bool enabled{true};
     double tolerance{0.05};
@@ -546,7 +545,6 @@ struct neb_options_t {
     bool minimize{false};
     bool use_path_file{false};
   } endpoints;
-
 };
 
 // [Molecular Dynamics] //
