4afb5c17e5b48606ae093e871b13b1b2330d7be9
Rohit Goswami <rog32@hi.is>
2026-09-15 12:47:56 -0500

    fix(test): alias EigenmodeStrategy and remaining eonc test types
    ConFileIOTest spells EigenmodeStrategy unqualified after n2nm dropped
    global aliases. Shared test helpers now include the strategy header and
    the other core types the suite still names without eonc::.

--- client/unit_tests/eonc_test_aliases.hpp
+++ client/unit_tests/eonc_test_aliases.hpp
@@ -12,10 +12,17 @@
 #pragma once
 
 #include "eon/BaseStructures.h"
+#include "eon/BondBoost.h"
+#include "eon/ConjugateGradients.h"
 #include "eon/Davidson.h"
 #include "eon/Dimer.h"
+#include "eon/Dynamics.h"
+#include "eon/EigenmodeStrategy.h"
+#include "eon/FIRE.h"
+#include "eon/Hessian.h"
 #include "eon/IDPPObjectiveFunction.hpp"
 #include "eon/ImprovedDimer.h"
+#include "eon/LBFGS.h"
 #include "eon/LORRotation.h"
 #include "eon/Lanczos.h"
 #include "eon/LowestEigenmode.h"
@@ -23,14 +30,22 @@
 #include "eon/MinModeSaddleSearch.h"
 #include "eon/MonteCarlo.h"
 #include "eon/NudgedElasticBand.h"
+#include "eon/ObjectiveFunction.h"
+#include "eon/Optimizer.h"
 #include "eon/Parameters.h"
 #include "eon/PotRegistry.h"
 #include "eon/Potential.h"
+#include "eon/Quickmin.h"
+#include "eon/SteepestDescent.h"
 
+// Test TUs sit in namespace tests and still spell eonc types unqualified.
+// Do not include optional headers here (AtomicGPDimer, ARTnSaddleSearch).
+using namespace eonc;
 using eonc::CollectiveIDPPObjectiveFunction;
 using eonc::Davidson;
 using eonc::Dimer;
 using eonc::DimerRotationBackend;
+using eonc::EigenmodeStrategy;
 using eonc::ImprovedDimer;
 using eonc::JobType;
 using eonc::Lanczos;
