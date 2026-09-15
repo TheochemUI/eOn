391628279b35553c9b17e3e443efec576aefe9e1
Rohit Goswami <rog32@hi.is>
2026-09-15 11:14:35 -0500

    fix(test): alias Dimer, Lanczos, Davidson, and saddle-search types

--- client/unit_tests/eonc_test_aliases.hpp
+++ client/unit_tests/eonc_test_aliases.hpp
@@ -11,11 +11,18 @@
 */
 #pragma once
 
+#include "eon/ARTnSaddleSearch.h"
+#include "eon/AtomicGPDimer.h"
 #include "eon/BaseStructures.h"
+#include "eon/Davidson.h"
+#include "eon/Dimer.h"
 #include "eon/IDPPObjectiveFunction.hpp"
 #include "eon/ImprovedDimer.h"
+#include "eon/LORRotation.h"
+#include "eon/Lanczos.h"
 #include "eon/LowestEigenmode.h"
 #include "eon/Matter.h"
+#include "eon/MinModeSaddleSearch.h"
 #include "eon/MonteCarlo.h"
 #include "eon/NudgedElasticBand.h"
 #include "eon/Parameters.h"
@@ -23,11 +30,16 @@
 #include "eon/Potential.h"
 
 using eonc::CollectiveIDPPObjectiveFunction;
+using eonc::Davidson;
+using eonc::Dimer;
 using eonc::DimerRotationBackend;
 using eonc::ImprovedDimer;
 using eonc::JobType;
+using eonc::Lanczos;
+using eonc::LORRotation;
 using eonc::LowestEigenmode;
 using eonc::Matter;
+using eonc::MinModeSaddleSearch;
 using eonc::MonteCarlo;
 using eonc::NEBInit;
 using eonc::NEBObjectiveFunction;
